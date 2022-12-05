3e8afdfc3ba4817f11928794c2334ec6ead00c1c
Farrukh Ayub <farrukh.ayub@gmail.com>
2022-12-05 11:49:08 +0000

    test api integrated

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,34 +1,40 @@
 import Plot from "react-plotly.js";
 import React from "react";
-
 class Graph extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("/testdata/")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        items: result
+                    })
+                },
+                (error) => {
+                    //TODO: handle ajax error gracefully
+                    console.log(error.toString());
+                }
+            );
+    }
+
+
+
     render() {
+        const { items } = this.state;
+        items.forEach(element => {
+            element.mode = 'line';
+        });
         return (
-            <Plot
-
-                data={[
-
-                    {
-
-                        x: [1, 2, 3],
-
-                        y: [2, 6, 3],
-
-                        type: "scatter",
-
-                        mode: "lines+markers",
-
-                        marker: { color: "red" },
-
-                    },
-
-                    { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
-
-                ]}
-
-                layout={{ width: 500, height: 440, title: "Graph Example" }}
-
-            />);
+            <Plot data={items} layout={{ width: 800, height: 600, title: "Winter Pressures" }} />
+        );
     }
 }
 
