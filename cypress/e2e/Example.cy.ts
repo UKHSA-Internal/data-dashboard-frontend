3a2e8a9f4440906cfe14e11a1e4800c8d07aafce
Tom  Christian <tctc91@gmail.com>
2023-02-28 13:52:44 +0000

    feat: added gov uk react package

--- cypress/e2e/Example.cy.ts
+++ cypress/e2e/Example.cy.ts
@@ -2,6 +2,6 @@
 describe('Example', () => {
   it('loads the page and displays some text', () => {
     cy.visit('http://localhost:3000/')
-    cy.findByText('Get started by editing').should('exist')
+    cy.findByText('Respiratory viruses in England')
   })
 })
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
+import { GlobalStyle } from 'govuk-react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
 }
