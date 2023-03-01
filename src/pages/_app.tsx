db09bb2a554429af92860cfb16bc64f04d7c80bd
Tom  Christian <tctc91@gmail.com>
2023-03-01 15:09:06 +0000

    feat: use next layout + add dynamic route example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
+import Layout from '@/components/Layout/Layout'
 import { GlobalStyle } from 'govuk-react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Component {...pageProps} />
       <GlobalStyle />
-    </>
+    </Layout>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,11 @@
-import { Footer, H1, Main, Page, PhaseBanner, TopNav } from 'govuk-react'
+import { H1 } from 'govuk-react'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
-      <Page
-        beforeChildren={
-          <PhaseBanner level="alpha">
-            This is a new service – your feedback will help us to improve it.
-          </PhaseBanner>
-        }
-        header={<TopNav serviceTitle={'UKHSA Dashboard'} />}
-      >
-        <Main>
-          <H1>Respiratory viruses in England</H1>
-        </Main>
-      </Page>
-      <Footer
-        copyright={{
-          image: {
-            height: 102,
-            src: 'govuk-crest-2x.png',
-            width: 125,
-          },
-          link: 'https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/',
-          text: 'Crown copyright',
-        }}
-      />
+      <H1>Respiratory viruses in England</H1>
+      <Link href="viruses/sars-cov-2">SARS-CoV-2</Link>
     </>
   )
 }
