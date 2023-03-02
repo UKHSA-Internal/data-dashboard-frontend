535459945368a0336ef19487bb8661df257abf4a
Tom  Christian <tctc91@gmail.com>
2023-03-02 14:53:35 +0000

    feat: setup msw with dummy api mocks in place

---
+++ src/mocks/api/viruses/handlers.ts
@@ -0,0 +1,8 @@
+import { rest } from 'msw'
+import { data } from './data'
+
+export const handlers = [
+  rest.get(`${process.env.NEXT_PUBLIC_API_URL}/viruses`, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(data))
+  ),
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,46 @@
-import { H1 } from 'govuk-react'
+import { initMocks } from '@/mocks'
+import { VirusesResponse } from '@/mocks/api/viruses'
+import { GridCol, GridRow, H1, H3, Paragraph } from 'govuk-react'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Link from 'next/link'
 
-export default function Home() {
+type HomeProps = InferGetStaticPropsType<typeof getStaticProps>
+
+export default function Home({ viruses: { viruses } }: HomeProps) {
   return (
     <>
       <H1>Respiratory viruses in England</H1>
-      <Link href="viruses/sars-cov-2">SARS-CoV-2</Link>
+
+      <GridRow>
+        {viruses.map(({ name, description }) => {
+          return (
+            <GridCol key={name}>
+              <Link href={`viruses/${name.toLowerCase()}`}>
+                <H3>{name}</H3>
+              </Link>
+              <Paragraph>{description}</Paragraph>
+            </GridCol>
+          )
+        })}
+      </GridRow>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps<{
+  viruses: VirusesResponse
+}> = async () => {
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+    await initMocks()
+  }
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/viruses`)
+  const viruses: VirusesResponse = await res.json()
+
+  return {
+    props: {
+      viruses,
+    },
+    revalidate: 10,
+  }
 }
