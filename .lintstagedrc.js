0bef10faae5169bd0365a5b9ea3893b3f4bd154b
Tom  Christian <tctc91@gmail.com>
2023-03-09 14:51:49 +0000

    fix: eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,10 @@ module.exports = {
   '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
   // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx|js)': (filenames) => [`eslint --fix ${filenames.join(' ')}`],
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `eslint --fix ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
+  ],
 
   // Prettify only Markdown and JSON files
   '**/*.(md|json)': (filenames) =>
