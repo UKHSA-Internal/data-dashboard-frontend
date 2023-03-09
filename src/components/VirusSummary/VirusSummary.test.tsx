cd5e0cac316d2daaf6ebd7f9b8a38612268c499a
Rhys <rhys.howard@burendo.com>
2023-03-09 08:53:19 +0000

    Sorting tests

--- src/components/VirusSummary/VirusSummary.test.tsx
+++ src/components/VirusSummary/VirusSummary.test.tsx
@@ -3,29 +3,32 @@ import VirusSummary from "./VirusSummary";
 
 jest.mock('next/router', () => require('next-router-mock'))
 
-test('Name', () => {
+test('Basic content check', () => {
     const mockData = {
         virus: "Test virus",
         description: "description for test virus",
         points: [],
     };
 
-    // render(<VirusSummary virus={mockData.virus} descrpition={mockData.description} points={mockData.points} />)
+    render(<VirusSummary virus={mockData.virus} descrpition={mockData.description} points={mockData.points} />)
 
-    // // // Title
-    // const title = screen.getByTestId("ukhsa-title")
-    // expect(within(title).getByText("Test virus")).toBeInTheDocument();
+    // Title
+    const title = screen.getByTestId("ukhsa-title")
+    expect(within(title).getByText("Test virus")).toBeInTheDocument();
 
-    // // // Download button
-    // const downloadButton = screen.getByTestId("ukhsa-downloadButton")
-    // expect(within(downloadButton).getByText("Download")).toBeInTheDocument();
+    // Download button
+    const downloadButton = screen.getByTestId("ukhsa-downloadButton")
+    expect(within(downloadButton).getByText("Download")).toBeInTheDocument();
     
-    // // // Description
-    // const description = screen.getByTestId("ukhsa-description")
-    // expect(within(description).getByText("description for test virus")).toBeInTheDocument();
+    // Description
+    const description = screen.getByTestId("ukhsa-description")
+    expect(description).toBeInTheDocument();
 
-    
-    // On click dropdown, see if items exist
+    // Dropdown button
+    const dropdown = screen.getByTestId("ukhsa-tabledropdown")
+    expect(within(dropdown).getByText("View data in a tabular format")).toBeInTheDocument();
 
-    // matches snapshot
+    // Dropdown heading
+    const dropdownHeading = screen.getByTestId("ukhsa-dropdownheading");
+    expect(within(dropdownHeading).getByAltText("Monthly Test virus cases")).toBeInTheDocument();
 })
