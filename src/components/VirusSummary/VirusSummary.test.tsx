91c06d58e3c655b696a1fdac65c496270913d071
Tom  Christian <tctc91@gmail.com>
2023-03-10 09:27:12 +0000

    fix: table a11y & test update

--- src/components/VirusSummary/VirusSummary.test.tsx
+++ src/components/VirusSummary/VirusSummary.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import VirusSummary from './VirusSummary'
 
@@ -7,7 +7,16 @@ jest.mock('next/router', () => require('next-router-mock'))
 const mockData = {
   virus: 'TestVirus',
   description: 'description for test virus',
-  points: [],
+  points: [
+    {
+      date: '2022-02-10',
+      value: 33388,
+    },
+    {
+      date: '2022-01-17',
+      value: 29214,
+    },
+  ],
 }
 
 test('Displays the title, description, shows a download button, and table dropdown button ', () => {
@@ -34,19 +43,7 @@ test('Displays the title, description, shows a download button, and table dropdo
   expect(screen.getByText('View data in a tabular format')).toBeInTheDocument()
 })
 
-test("The table dropdown content doesn't show initially", () => {
-  render(
-    <VirusSummary
-      virus={mockData.virus}
-      description={mockData.description}
-      points={mockData.points}
-    />
-  )
-
-  expect(screen.getByText('Monthly TestVirus cases')).not.toBeVisible()
-})
-
-test('Click button, and dropdown content to be shown', async () => {
+test('Clicking the "View data in a tabular format" button, shows the graph in a tabular format', async () => {
   const user = userEvent.setup()
 
   render(
@@ -57,8 +54,26 @@ test('Click button, and dropdown content to be shown', async () => {
     />
   )
 
+  // Table is not visible by default
+  expect(screen.getByText('Monthly TestVirus cases')).not.toBeVisible()
+
   const dropdownButton = screen.getByText('View data in a tabular format')
   await user.click(dropdownButton)
 
-  expect(screen.getByText('Monthly TestVirus cases')).toBeVisible()
+  const table = screen.getByRole('table', { name: 'Monthly TestVirus cases' })
+  expect(table).toBeVisible()
+
+  // Table column headers
+  const headers = within(table).getAllByRole('columnheader')
+  expect(within(headers[0]).getByText('Month'))
+  expect(within(headers[1]).getByText('Amount'))
+
+  // Table rows
+  const rows = within(table).getAllByRole('row')
+  expect(rows).toHaveLength(3) // 1 header row, 2 body rows
+
+  expect(within(rows[1]).getByText('2022-02-10'))
+  expect(within(rows[1]).getByText('33388'))
+  expect(within(rows[2]).getByText('2022-01-17'))
+  expect(within(rows[2]).getByText('29214'))
 })
--- src/components/VirusSummary/VirusSummaryTable.tsx
+++ src/components/VirusSummary/VirusSummaryTable.tsx
@@ -1,24 +1,25 @@
-import { Table } from 'govuk-react';
+import { Table } from './VirusSummaryTable.styles'
 
 interface VirusSummaryTableProps {
-    data: Array<{ date: string, value: number }>;
+  caption: string
+  data: Array<{ date: string; value: number }>
 }
 
-const VirusSummaryTable = ({ data }: VirusSummaryTableProps) => {
-    return (
-        <Table>
-            <Table.Row>
-                <Table.CellHeader>Month</Table.CellHeader>
-                <Table.CellHeader>Amount</Table.CellHeader>
-            </Table.Row>
-            {data.map((point, key) => (
-                <Table.Row key={key}>
-                    <Table.CellHeader>{point.date}</Table.CellHeader>
-                    <Table.Cell>{point.value}</Table.Cell>
-                </Table.Row>
-            ))}
-        </Table>
-    )
-};
+const VirusSummaryTable = ({ caption, data }: VirusSummaryTableProps) => {
+  return (
+    <Table caption={caption}>
+      <Table.Row>
+        <Table.CellHeader>Month</Table.CellHeader>
+        <Table.CellHeader>Amount</Table.CellHeader>
+      </Table.Row>
+      {data.map((point, key) => (
+        <Table.Row key={key}>
+          <Table.CellHeader>{point.date}</Table.CellHeader>
+          <Table.Cell>{point.value}</Table.Cell>
+        </Table.Row>
+      ))}
+    </Table>
+  )
+}
 
-export default VirusSummaryTable;
+export default VirusSummaryTable
