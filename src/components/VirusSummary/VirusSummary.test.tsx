94bfae997f62d4647c124ec3a454f725c7fc052f
Rhys <rhys.howard@burendo.com>
2023-03-10 08:31:17 +0000

    Adding additional tests for dropdown

--- src/components/VirusSummary/VirusSummary.test.tsx
+++ src/components/VirusSummary/VirusSummary.test.tsx
@@ -1,28 +1,64 @@
-import { render, screen } from '@testing-library/react';
-import VirusSummary from "./VirusSummary";
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import VirusSummary from './VirusSummary'
 
 jest.mock('next/router', () => require('next-router-mock'))
 
+const mockData = {
+  virus: 'TestVirus',
+  description: 'description for test virus',
+  points: [],
+}
+
 test('Displays the title, description, shows a download button, and table dropdown button ', () => {
-    const mockData = {
-        virus: "TestVirus",
-        description: "description for test virus",
-        points: [],
-    };
+  render(
+    <VirusSummary
+      virus={mockData.virus}
+      description={mockData.description}
+      points={mockData.points}
+    />
+  )
 
-    render(<VirusSummary virus={mockData.virus} description={mockData.description} points={mockData.points} />)
+  // Title
+  const title = screen.getByText('TestVirus')
+  expect(title).toBeInTheDocument()
+  expect(title).toHaveAttribute('href', '/viruses/TestVirus')
 
-    // Title
-    const title = screen.getByText("TestVirus")
-    expect(title).toBeInTheDocument()
-    expect(title).toHaveAttribute('href', '/viruses/TestVirus')
+  // Download button
+  expect(screen.getByRole('button', { name: 'Download' })).toBeInTheDocument()
 
-    // Download button
-    expect(screen.getByRole("button", { name: "Download" })).toBeInTheDocument()
-    
-    // Description
-    expect(screen.getByText("description for test virus")).toBeInTheDocument()
+  // Description
+  expect(screen.getByText('description for test virus')).toBeInTheDocument()
 
-    // Dropdown button
-    expect(screen.getByText("View data in a tabular format")).toBeInTheDocument()
+  // Dropdown button
+  expect(screen.getByText('View data in a tabular format')).toBeInTheDocument()
+})
+
+test("The table dropdown content doesn't show initially", () => {
+  render(
+    <VirusSummary
+      virus={mockData.virus}
+      description={mockData.description}
+      points={mockData.points}
+    />
+  )
+
+  expect(screen.getByText('Monthly TestVirus cases')).not.toBeVisible()
+})
+
+test('Click button, and dropdown content to be shown', async () => {
+  const user = userEvent.setup()
+
+  render(
+    <VirusSummary
+      virus={mockData.virus}
+      description={mockData.description}
+      points={mockData.points}
+    />
+  )
+
+  const dropdownButton = screen.getByText('View data in a tabular format')
+  await user.click(dropdownButton)
+
+  expect(screen.getByText('Monthly TestVirus cases')).toBeVisible()
 })
