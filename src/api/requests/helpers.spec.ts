acc45c0dc7316f1a8f797dca7aa0890a47deb350
Tom  Christian <tctc91@gmail.com>
2023-03-13 13:18:27 +0000

    refactor: clean up

--- src/api/requests/helpers.spec.ts
+++ src/api/requests/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { getCmsApiPath, getChartApiPath } from './helpers'
+import { getCmsApiPath, getChartsApiPath } from './helpers'
 
 /**
  * The process.env mocks powering these tests live inside <rootDir>/config/jest/jest.env.js
@@ -7,6 +7,6 @@ test('Get the CMS API path', () => {
   expect(getCmsApiPath()).toEqual('http://backend.com/api/v2')
 })
 
-test('Get the Chart API path', () => {
-  expect(getChartApiPath()).toEqual('http://backend.com/api/chart')
+test('Get the Charts API path', () => {
+  expect(getChartsApiPath()).toEqual('http://backend.com/api/charts')
 })
