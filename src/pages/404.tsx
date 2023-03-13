88ddfa06b74b407bd8ab4cfe4f34d4e43dd94caa
Tom  Christian <tctc91@gmail.com>
2023-03-13 13:21:29 +0000

    feat: 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { H1 } from 'govuk-react'
+
+export const NotFoundPage = () => {
+  return (
+    <>
+      <H1>This page could not be found</H1>
+    </>
+  )
+}
+
+export default NotFoundPage
