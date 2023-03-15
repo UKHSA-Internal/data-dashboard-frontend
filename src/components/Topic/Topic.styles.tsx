03461128e585161534fa3213e11cfb07c402b15e
Tom  Christian <tctc91@gmail.com>
2023-03-15 16:11:22 +0000

    feat: e2e

--- src/components/Topic/Topic.styles.tsx
+++ src/components/Topic/Topic.styles.tsx
@@ -1,15 +1,12 @@
-import { Details } from 'govuk-react'
 import styled from 'styled-components'
+import { SPACING } from '@govuk-react/constants'
 
 export const Container = styled.div``
 
-export const ChartContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 220px;
-  margin-bottom: 30px;
-`
-
-export const DataTableDropDown = styled(Details)`
-  width: 100%;
-`
+export const ChartContainer = styled.div({
+  width: '100%',
+  height: 220,
+  position: 'relative',
+  marginTop: SPACING.SCALE_4,
+  marginBottom: SPACING.SCALE_2,
+})
