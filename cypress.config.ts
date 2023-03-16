ef8f5b54ad4957a6a8eb325207986a5460f646fe
Tom  Christian <tctc91@gmail.com>
2023-03-16 13:00:28 +0000

    feat: download e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'cypress'
+import { rmdir } from 'fs'
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     video: false,
+    setupNodeEvents(on) {
+      on('task', {
+        deleteFolder(folderName) {
+          console.log('deleting folder %s', folderName)
+
+          return new Promise((resolve, reject) => {
+            rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
+              if (err) {
+                console.error(err)
+                return reject(err)
+              }
+              resolve(null)
+            })
+          })
+        },
+      })
+    },
   },
 })
