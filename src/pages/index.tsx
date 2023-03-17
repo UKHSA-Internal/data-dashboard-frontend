e4089c7f1bd6148dbe70e572c47a99d4a8c2c544
Rhys <rhys.howard@burendo.com>
2023-03-17 10:33:51 +0000

    Updated e2e tests for trend components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import {
-  getVirusesSummary,
-  VirusesResponse,
-} from '@/api/requests/getVirusesSummary'
+import { getVirusesSummary, VirusesResponse } from '@/api/requests/getVirusesSummary'
 import { GridCol, GridRow, Paragraph } from 'govuk-react'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Topic from '@/components/Topic/Topic'
@@ -10,11 +7,7 @@ import { Contents, ContentsItem } from '@/components/Contents'
 import { Card, CardColumn } from '@/components/Card'
 import { Statistic } from '@/components/Statistic'
 import { Page } from '@/components/Page'
-import {
-  DashboardPage,
-  getPage,
-  PageResponse,
-} from '@/api/requests/cms/getPage'
+import { DashboardPage, getPage, PageResponse } from '@/api/requests/cms/getPage'
 import { initMocks } from '@/api/msw'
 import { DownloadLink } from '@/components/Links'
 import Trend from '@/components/Trend/Trend'
@@ -27,9 +20,7 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
       <Paragraph>{body}</Paragraph>
       <Contents label="Respiratory viruses in this dashboard">
         <ContentsItem heading="Coronavirus">
-          <Paragraph>
-            The UKHSA dashboard for data and insights on Coronavirus.
-          </Paragraph>
+          <Paragraph>The UKHSA dashboard for data and insights on Coronavirus.</Paragraph>
           <Card label="Coronavirus summary">
             <CardColumn heading="Cases">
               <Statistic heading="Weekly" value="20,629,892" />
@@ -46,7 +37,7 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
             <CardColumn heading="Healthcare">
               <Statistic heading="Patients admitted" value="981,596" />
               <Statistic heading="Last 7 days">
-                <Trend positive={false} value={'5,911 (0.3%)'} />
+                <Trend positive={false} value={'4,807 (0.2%)'} />
               </Statistic>
             </CardColumn>
             <CardColumn heading="Vaccines">
@@ -56,23 +47,16 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
             <CardColumn heading="Testing">
               <Statistic heading="Virus tests positivity (%)" value="10.9%" />
               <Statistic heading="Last 7 days">
-                <Trend positive={false} value={'5,911 (0.3%)'} />
+                <Trend positive={false} value={'5,425 (0.4%)'} />
               </Statistic>
             </CardColumn>
           </Card>
           <GridRow>
             <GridCol setWidth="one-half">
               <Card label="Coronavirus cases">
-                <CardColumn
-                  heading="Cases"
-                  sideContent={
-                    <DownloadLink href="/api/download">Download</DownloadLink>
-                  }
-                >
+                <CardColumn heading="Cases" sideContent={<DownloadLink href="/api/download">Download</DownloadLink>}>
                   <Statistic heading="People tested positive in England">
-                    <Paragraph supportingText>
-                      Up to and including 25th February 2023
-                    </Paragraph>
+                    <Paragraph supportingText>Up to and including 25th February 2023</Paragraph>
                   </Statistic>
                   <GridRow>
                     <GridCol setWidth="columnOneThird">
@@ -92,16 +76,9 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
             </GridCol>
             <GridCol setWidth="one-half">
               <Card label="Coronavirus deaths">
-                <CardColumn
-                  heading="Deaths"
-                  sideContent={
-                    <DownloadLink href="/api/download">Download</DownloadLink>
-                  }
-                >
+                <CardColumn heading="Deaths" sideContent={<DownloadLink href="/api/download">Download</DownloadLink>}>
                   <Statistic heading="Deaths with COVID-19 on the death certificate in England">
-                    <Paragraph supportingText>
-                      Up to and including 3rd February 2023
-                    </Paragraph>
+                    <Paragraph supportingText>Up to and including 3rd February 2023</Paragraph>
                   </Statistic>
                   <GridRow>
                     <GridCol setWidth="columnOneThird">
@@ -122,14 +99,12 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
           </GridRow>
         </ContentsItem>
         <ContentsItem heading="Influenza">
-          <Paragraph>
-            The UKHSA dashboard for data and insights on Influenza.
-          </Paragraph>
+          <Paragraph>The UKHSA dashboard for data and insights on Influenza.</Paragraph>
           <Card label="Influenza summary">
             <CardColumn heading="Healthcare">
               <Statistic heading="Patients admitted" value="981,596" />
               <Statistic heading="Last 7 days">
-                <Trend positive value={'5,911 (0.3%)'} />
+                <Trend positive value={'5,788 (0.3%)'} />
               </Statistic>
             </CardColumn>
             <CardColumn heading="Vaccines">
@@ -139,7 +114,7 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
             <CardColumn heading="Testing">
               <Statistic heading="Virus tests positivity (%)" value="10.9%" />
               <Statistic heading="Last 7 days">
-                <Trend positive={false} value={'5,911 (2.3%)'} />
+                <Trend positive={false} value={'16,109 (2.3%)'} />
               </Statistic>
             </CardColumn>
           </Card>
@@ -148,14 +123,10 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
               <Card label="Influenza healthcare">
                 <CardColumn
                   heading="Healthcare"
-                  sideContent={
-                    <DownloadLink href="/api/download">Download</DownloadLink>
-                  }
+                  sideContent={<DownloadLink href="/api/download">Download</DownloadLink>}
                 >
                   <Statistic heading="Weekly hospital admission rates for Influenza">
-                    <Paragraph supportingText>
-                      Up to and including 25th February 2023
-                    </Paragraph>
+                    <Paragraph supportingText>Up to and including 25th February 2023</Paragraph>
                   </Statistic>
                   <GridRow>
                     <GridCol setWidth="columnOneThird">
@@ -175,16 +146,9 @@ export default function Home({ title, body, relatedLinks }: HomeProps) {
             </GridCol>
             <GridCol setWidth="one-half">
               <Card label="Influenza testing">
-                <CardColumn
-                  heading="Testing"
-                  sideContent={
-                    <DownloadLink href="/api/download">Download</DownloadLink>
-                  }
-                >
+                <CardColumn heading="Testing" sideContent={<DownloadLink href="/api/download">Download</DownloadLink>}>
                   <Statistic heading="Weekly positivity by age">
-                    <Paragraph supportingText>
-                      Up to and including 3rd February 2023
-                    </Paragraph>
+                    <Paragraph supportingText>Up to and including 3rd February 2023</Paragraph>
                   </Statistic>
                   <GridRow>
                     <GridCol setWidth="columnOneThird">
@@ -221,15 +185,9 @@ export const getStaticProps: GetStaticProps<{
     await initMocks()
   }
 
-  const [viruses] = await Promise.all([
-    await getVirusesSummary({ searchTerm: '' }),
-  ])
+  const [viruses] = await Promise.all([await getVirusesSummary({ searchTerm: '' })])
 
-  const {
-    title,
-    body,
-    related_links: relatedLinks,
-  } = await getPage<DashboardPage>(1)
+  const { title, body, related_links: relatedLinks } = await getPage<DashboardPage>(1)
 
   return {
     props: {
