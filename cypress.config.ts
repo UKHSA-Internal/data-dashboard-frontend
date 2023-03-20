74c16895feb4928d88315b6674cc3f21083ef675
Tom  Christian <tctc91@gmail.com>
2023-03-20 11:09:10 +0000

    fix: ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 import { rmdir } from 'fs'
 
 export default defineConfig({
+  projectId: process.env.CYPRESS_PROJECT_ID,
   e2e: {
     baseUrl: 'http://localhost:3000',
     video: false,
