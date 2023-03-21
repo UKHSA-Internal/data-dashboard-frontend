d097d8e87395435496d63a3068285046f01b8ff8
Tom  Christian <tctc91@gmail.com>
2023-03-21 12:03:14 +0000

    feat: update e2es

--- cypress/e2e/HowToUseThisData.cy.ts
+++ cypress/e2e/HowToUseThisData.cy.ts
@@ -1,9 +1,12 @@
 describe('How to use this data', () => {
-  it('displays a title, body and related links', () => {
+  it('displays correctly', () => {
     cy.visit('/how-to-use-this-data')
 
     // Title
     cy.findByRole('heading', { name: 'How to use this data', level: 1 })
+
+    // Last updated
+    cy.findByText('Last updated on January 15th 2023 at 10:25am')
 
     // Body
     cy.findByText('How to use this data CMS page content!')
--- cypress/e2e/WhatsNew.cy.ts
+++ cypress/e2e/WhatsNew.cy.ts
@@ -1,9 +1,12 @@
 describe("What's new", () => {
-  it('displays a title, body and related links', () => {
+  it('displays correctly', () => {
     cy.visit('/whats-new')
 
     // Title
     cy.findByRole('heading', { name: "What's new", level: 1 })
+
+    // Last updated
+    cy.findByText('Last updated on March 3rd 2023 at 11:15am')
 
     // Body
     cy.findByText("What's new CMS page content!")
