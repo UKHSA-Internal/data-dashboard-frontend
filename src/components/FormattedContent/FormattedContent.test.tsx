80b3905a4cd9ca216162f08381c28e4720bb7bcf
rhys-burendo <126494410+rhys-burendo@users.noreply.github.com>
2023-03-23 13:33:16 +0000

    Update src/components/FormattedContent/FormattedContent.test.tsx
    Co-authored-by: Tom Christian <hello@elseif.io>

--- src/components/FormattedContent/FormattedContent.test.tsx
+++ src/components/FormattedContent/FormattedContent.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { FormattedContent } from './FormattedContent'
 
-jest.mock('next/router', () => require('next-router-mock'))
 
 const mockData =
   '<h1>Primary test</h1><h2>Secondary test</h2><h3>Tertiary test</h3><a href="/test">Link test</a><ul><li>List item test</li></ul>'
