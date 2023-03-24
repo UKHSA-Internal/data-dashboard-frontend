c17c80787f0a74c556e47b9c26b67aeaea96a125
Tom  Christian <tctc91@gmail.com>
2023-03-24 11:58:54 +0000

    fix: better destructuring

--- src/components/FormattedContent/FormattedContent.tsx
+++ src/components/FormattedContent/FormattedContent.tsx
@@ -16,12 +16,12 @@ type Components = ReactMarkdownProps['components']
  */
 const corePlugins: Plugins = [rehypeRaw]
 const coreComponents: Components = {
-  h1: ({ ...props }) => <H1>{props.children}</H1>,
-  h2: ({ ...props }) => <H2>{props.children}</H2>,
-  h3: ({ ...props }) => <H3>{props.children}</H3>,
-  a: ({ ...props }) => <Link href={props.href}>{props.children}</Link>,
-  ul: ({ ...props }) => <UnorderedList>{props.children}</UnorderedList>,
-  li: ({ ...props }) => <ListItem>{props.children}</ListItem>,
+  h1: ({ children }) => <H1>{children}</H1>,
+  h2: ({ children }) => <H2>{children}</H2>,
+  h3: ({ children }) => <H3>{children}</H3>,
+  a: ({ children, href }) => <Link href={href}>{children}</Link>,
+  ul: ({ children }) => <UnorderedList>{children}</UnorderedList>,
+  li: ({ children }) => <ListItem>{children}</ListItem>,
 }
 
 /**
@@ -29,19 +29,19 @@ const coreComponents: Components = {
  */
 const linkedHeadingsPlugins: Plugins = [rehypeSlug, [rehypeToc, { headings: ['h2'] }]]
 const linkedHeadingsComponents: Components = {
-  a: ({ ...props }) => <Link href={props.href}>{props.children}</Link>,
-  h2: ({ ...props }) => <Contents.SectionHeading id={props.id ?? ''}>{props.children}</Contents.SectionHeading>,
-  nav: ({ ...props }) => {
-    if (props.className?.includes('toc')) {
-      return <Contents.Nav>{props.children}</Contents.Nav>
+  a: ({ children, href }) => <Link href={href}>{children}</Link>,
+  h2: ({ children, id }) => <Contents.SectionHeading id={id ?? ''}>{children}</Contents.SectionHeading>,
+  nav: ({ children, className }) => {
+    if (className?.includes('toc')) {
+      return <Contents.Nav>{children}</Contents.Nav>
     }
-    return <nav>{props.children}</nav>
+    return <nav>{children}</nav>
   },
-  li: ({ ...props }) => {
-    if (props.className?.includes('toc')) {
-      return <Contents.NavListItem id={props.id ?? ''}>{props.children}</Contents.NavListItem>
+  li: ({ children, className, id }) => {
+    if (className?.includes('toc')) {
+      return <Contents.NavListItem id={id ?? ''}>{children}</Contents.NavListItem>
     }
-    return <ListItem>{props.children}</ListItem>
+    return <ListItem>{children}</ListItem>
   },
 }
 
