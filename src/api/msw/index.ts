f2b627b03de22886774bf8ec3d1ce39b18f0de48
Tom  Christian <tctc91@gmail.com>
2023-03-29 08:14:46 +0100

    fix: load correct env in cypress + e2e fixes

--- src/api/msw/index.ts
+++ src/api/msw/index.ts
@@ -1,11 +1,11 @@
 export async function initMocks() {
-  // if (typeof window === 'undefined') {
-  //   const { server } = await import('./server')
-  //   server.listen()
-  // } else {
-  //   const { worker } = await import('./browser')
-  //   worker.start()
-  // }
+  if (typeof window === 'undefined') {
+    const { server } = await import('./server')
+    server.listen()
+  } else {
+    const { worker } = await import('./browser')
+    worker.start()
+  }
 }
 
 export {}
