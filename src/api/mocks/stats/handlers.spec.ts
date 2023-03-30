08d4587d120c4111aff68d7829a974b8e68497e0
Tom  Christian <tctc91@gmail.com>
2023-03-30 16:07:48 +0100

    feat: add api key headers

--- src/api/mocks/stats/handlers.spec.ts
+++ src/api/mocks/stats/handlers.spec.ts
@@ -1,5 +1,5 @@
 import { server } from '@/api/msw/server'
-import { getStatsApiPath } from '@/api/requests/helpers'
+import { getStatsApiPath, requestOptions } from '@/api/requests/helpers'
 import 'whatwg-fetch'
 import { coronavirusStatsMock } from './data/topics/coronavirus'
 import { influenzaStatsMock } from './data/topics/influenza'
@@ -11,17 +11,33 @@ afterEach(() => server.resetHandlers())
 const baseUrl = getStatsApiPath()
 
 test('GET /stats/COVID-19 returns the correct stats mock', async () => {
-  const res = await fetch(`${baseUrl}/COVID-19`)
+  const res = await fetch(`${baseUrl}/COVID-19`, requestOptions)
   const json = await res.json()
 
   expect(res.status).toEqual(200)
   expect(json).toEqual(coronavirusStatsMock)
 })
 
+test('GET /stats/COVID-19 returns a 403 when the api key is invalid', async () => {
+  const res = await fetch(`${baseUrl}/COVID-19`, { headers: { 'X-Api-Key': 'not-valid' } })
+  const json = await res.json()
+
+  expect(res.status).toEqual(403)
+  expect(json).toEqual({ detail: 'You do not have permission to perform this action' })
+})
+
 test('GET /stats/Influenza returns the correct stats mock', async () => {
-  const res = await fetch(`${baseUrl}/Influenza`)
+  const res = await fetch(`${baseUrl}/Influenza`, requestOptions)
   const json = await res.json()
 
   expect(res.status).toEqual(200)
   expect(json).toEqual(influenzaStatsMock)
+})
+
+test('GET /stats/Influenza returns a 403 when the api key is invalid', async () => {
+  const res = await fetch(`${baseUrl}/Influenza`, { headers: { 'X-Api-Key': 'not-valid' } })
+  const json = await res.json()
+
+  expect(res.status).toEqual(403)
+  expect(json).toEqual({ detail: 'You do not have permission to perform this action' })
 })
