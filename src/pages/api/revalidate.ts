25f074272ce41ed32bd9ad52f042c37ef6410935
Tom  Christian <tctc91@gmail.com>
2023-04-03 15:15:44 +0100

    feat: tests

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -21,6 +21,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     console.log(err)
-    return res.status(500).send('Error revalidating')
+    return res.status(500).json({ message: 'Error revalidating' })
   }
 }
