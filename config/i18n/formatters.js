16d434b2416076b4cd8141f3ac43c483fc650a2f
Tom  Christian <tctc91@gmail.com>
2023-04-12 09:50:46 +0100

    feat: comment

--- config/i18n/formatters.js
+++ config/i18n/formatters.js
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const dayjs = require('dayjs')
 
+/**
+ * Custom formatters used within our i18n json locales
+ * Example: "Last updated on {{value, date}} at {{value, time}}"
+ */
 module.exports = (value, format) => {
   if (format === 'date') return dayjs(value).format('dddd, D MMMM YYYY')
   if (format === 'time') return dayjs(value).format('hh:mma')
