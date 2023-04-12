099479747ad84f9c607f770b9630c19fb99bb666
Tom  Christian <tctc91@gmail.com>
2023-04-12 09:42:55 +0100

    feat: comment

--- src/api/requests/charts/getAllDashboardCharts.ts
+++ src/api/requests/charts/getAllDashboardCharts.ts
@@ -1,6 +1,13 @@
 import { TopicName } from '../stats/getStats'
 import { getChart } from './getChart'
 
+/**
+ * This function orchestrates together the various chart API calls required to display
+ * the relevant charts on the dashbooard.
+ *
+ * When chart mocks are disabled it returns the SVGs as strings, otherwise it will return an empty
+ * string for the UI to handle accordingly.
+ */
 export const getAllDashboardCharts = async () => {
   // Initialise the charts with default values
   const charts: Record<TopicName, Record<string, string>> = {
