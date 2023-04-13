fcf05e59db793360590ac50621ff171b8e9cbf20
Tom  Christian <tctc91@gmail.com>
2023-04-13 14:37:47 +0100

    fix: mock url

--- config/jest/jest.env.js
+++ config/jest/jest.env.js
@@ -1,5 +1,5 @@
 process.env.NEXT_PUBLIC_BASE_URL = 'fake-frontend.gov.uk'
-process.env.NEXT_PUBLIC_API_URL = 'fake-backend.gov.uk'
+process.env.NEXT_PUBLIC_API_URL = 'http://fake-backend.gov.uk'
 process.env.API_KEY = 'fake-api-key'
 process.env.API_PATH_CMS = '/api/pages'
 process.env.NEXT_PUBLIC_API_PATH_CHART = '/charts'
