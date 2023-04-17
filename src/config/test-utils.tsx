02c13f18483c7b9ad99d682d08c4b175d870487e
Rhys <rhys.howard@burendo.com>
2023-04-17 09:56:23 +0100

    Back to top button now progressively enhanced & tested

--- src/config/test-utils.tsx
+++ src/config/test-utils.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/export */
 import React, { ReactElement } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
+// eslint-disable-next-line no-restricted-imports
 import { I18nextProvider } from 'react-i18next'
 
 import i18n from '../../config/i18n/i18nForTests'
