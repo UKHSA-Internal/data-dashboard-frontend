cde76c6c6bf0a06a1757675aeb1e499a4ec5420b
Rhys <rhys.howard@burendo.com>
2023-04-17 12:37:08 +0100

    Updating cypress tests & removing unecessary ID

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
-        <body id="content">
+        <body>
           <Main />
           <NextScript />
         </body>
