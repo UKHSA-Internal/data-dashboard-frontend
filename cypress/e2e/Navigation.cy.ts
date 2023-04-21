971bb6a615db212c86cb5c2c5bf804ca5412ba03
Rhys <rhys.howard@burendo.com>
2023-04-21 11:34:21 +0100

    Updating Cypress

--- cypress/e2e/Navigation.cy.ts
+++ cypress/e2e/Navigation.cy.ts
@@ -52,9 +52,8 @@ describe('Navigation', () => {
 
   it('Returns to the top of the page', () => {
     cy.visit('/')
-    cy.findByText('Back to top').should('not.be.visible')
 
-    cy.scrollTo(0, 500)
+    cy.scrollTo('bottom')
     cy.findByText('Back to top').click()
 
     cy.findByRole('heading', { level: 1, name: 'Respiratory viruses' }).should('be.visible')
