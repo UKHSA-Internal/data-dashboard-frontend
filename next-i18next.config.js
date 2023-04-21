273531a290a127c81efc3d74b7828f8b0c5ecc51
Tom Christian <hello@elseif.io>
2023-04-21 11:46:46 +0100

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,7 @@ const formatters = require('./config/i18n/formatters')
 
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
+  // Set debug to true to enable detailed logs in the terminal
   debug: false,
   reloadOnPrerender: process.env.NODE_ENV === 'development',
   serializeConfig: false,
