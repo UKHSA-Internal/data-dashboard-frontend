536dab010bf316bd19aa222d78822850b95dfedb
Tom  Christian <tctc91@gmail.com>
2023-04-25 14:06:14 +0100

    fix: remove dupe

--- src/api/models/cms/Page/BodyType.ts
+++ src/api/models/cms/Page/BodyType.ts
@@ -1,10 +1,5 @@
 import { z } from 'zod'
 
-export const BodyType = z.enum([
-  'text',
-  'headline_numbers_row_card',
-  'chart_with_headline_and_trend_card',
-  'headline_numbers_row_card',
-])
+export const BodyType = z.enum(['text', 'headline_numbers_row_card', 'chart_with_headline_and_trend_card'])
 
 export type BodyType = z.infer<typeof BodyType>
