0023506d8c701c03bc4563f199977c437f994212
Tom  Christian <tctc91@gmail.com>
2023-05-03 12:36:44 +0100

    refactor: cleanup

--- src/api/requests/helpers.spec.ts
+++ src/api/requests/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { getCmsApiPath, getChartsApiPath, getStatsApiPath, requestOptions, getApiBaseUrl } from './helpers'
+import { getApiBaseUrl, getCmsApiPath, requestOptions } from './helpers'
 
 /**
  * The process.env mocks powering these tests live inside <rootDir>/config/jest/jest.env.js
@@ -9,14 +9,6 @@ test('Get the API url', () => {
 
 test('Get the CMS API path', () => {
   expect(getCmsApiPath()).toEqual('http://fake-backend.gov.uk/api/pages')
-})
-
-test('Get the Chart API path', () => {
-  expect(getChartsApiPath()).toEqual('http://fake-backend.gov.uk/charts')
-})
-
-test('Get the Stats API path', () => {
-  expect(getStatsApiPath()).toEqual('http://fake-backend.gov.uk/stats')
 })
 
 test('Fetch request options', () => {
