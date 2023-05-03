62a6ba6e4734166056811090152a38e1d5669109
Tom  Christian <tctc91@gmail.com>
2023-05-03 11:20:12 +0100

    refactor: remove switch

--- src/components/CMS/Utils/renderCard.tsx
+++ src/components/CMS/Utils/renderCard.tsx
@@ -6,29 +6,23 @@ import { Cards, Utils } from '@/components/CMS'
  * This map utility is responsible for rendering top level cards returned from the CMS page body.
  */
 export const renderCard = ({ type, value, id }: z.infer<typeof ContentTypes>) => {
-  switch (type) {
-    case 'text_card': {
-      const { body } = value
-      return <Cards.Text key={id}>{body}</Cards.Text>
-    }
-    case 'headline_numbers_row_card': {
-      const { columns } = value
-      return (
-        <Cards.HeadlineNumbersRow
-          key={id}
-          columns={columns.map(Utils.renderHeadlineRowColumn)}
-          cardProps={{
-            'data-testid': 'summary-section',
-          }}
-        />
-      )
-    }
-    case 'chart_row_card': {
-      const { columns } = value
-      return <Cards.ChartRow key={id} columns={columns.map(Utils.renderChartRowColumn)} />
-    }
-    default: {
-      return null
-    }
+  if (type === 'text_card') {
+    return <Cards.Text key={id}>{value.body}</Cards.Text>
+  }
+
+  if (type === 'headline_numbers_row_card') {
+    return (
+      <Cards.HeadlineNumbersRow
+        key={id}
+        columns={value.columns.map(Utils.renderHeadlineRowColumn)}
+        cardProps={{
+          'data-testid': 'summary-section',
+        }}
+      />
+    )
+  }
+
+  if (type === 'chart_row_card') {
+    return <Cards.ChartRow key={id} columns={value.columns.map(Utils.renderChartRowColumn)} />
   }
 }
