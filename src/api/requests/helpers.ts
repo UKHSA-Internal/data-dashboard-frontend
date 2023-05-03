21904df857f4298ab6fd5ef71d843723721e6462
Rhys <rhys.howard@burendo.com>
2023-05-03 08:10:06 +0100

    Tabular data MSW

--- src/api/requests/helpers.ts
+++ src/api/requests/helpers.ts
@@ -2,6 +2,7 @@ const baseUrl = process.env.NEXT_PUBLIC_API_URL
 const cmsPath = process.env.API_PATH_CMS
 const chartPath = process.env.NEXT_PUBLIC_API_PATH_CHART
 const statsPath = process.env.API_PATH_STATS
+const tabularPath = process.env.API_PATH_TABULAR
 
 export const getApiBaseUrl = () => `${baseUrl}/api`
 
@@ -15,6 +16,10 @@ export const getChartsApiPath = () => {
 
 export const getStatsApiPath = () => {
   return `${baseUrl}${statsPath}`
+}
+
+export const getTabularApiPath = () => {
+  return `${baseUrl}${tabularPath}`
 }
 
 export const requestOptions: RequestInit = {
