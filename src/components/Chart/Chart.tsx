1fcedc177590143ce1b30eb18671a9c22f54f253
Rhys <rhys.howard@burendo.com>
2023-05-04 15:44:58 +0100

    Reverting tabular implementation

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,30 +1,28 @@
-import { ChartContainer, Container, TabularData } from './Chart.styles'
+import { ChartContainer, Container } from './Chart.styles'
 import Image from 'next/image'
-// import TopicTable from './ChartTable'
-// import { getTabular } from '@/api/requests/tabular/getTabular'
 
 interface ChartProps {
   src: string
 }
 
 export const Chart = async ({ src }: ChartProps) => {
-  // const tabularData = await getTabular({ topic: 'COVID-19', metric: 'new_cases_daily' })
-  // console.log()
-  //success: true,
-  // Data: {
-  //  date: x
-  //  value: y
-  // }
-
-  Expect: return (
+  return (
     <Container>
       <ChartContainer>
         <Image priority unoptimized alt="" fill sizes="100vw" src={src} />
       </ChartContainer>
 
-      <TabularData summary="View data in a tabular format">
-        {/* <TopicTable caption={`Monthly cases`} data={tabularData.data} /> */}
-      </TabularData>
+      {/* <TabularData summary="View data in a tabular format">
+        <TopicTable
+          caption={`Monthly cases`}
+          data={[
+            {
+              date: 'Jan 23',
+              value: 12,
+            },
+          ]}
+        />
+      </TabularData> */}
     </Container>
   )
 }
