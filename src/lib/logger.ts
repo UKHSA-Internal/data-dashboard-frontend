4e0c12ee863fed1264fca9c03df474bab731f04f
Tom  Christian <tctc91@gmail.com>
2023-05-09 09:11:54 +0100

    fix: logger env

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,10 +1,16 @@
 import pino from 'pino'
 
-export const logger = pino({
+const devOptions = {
   transport: {
     target: 'pino-pretty',
     options: {
       colorize: true,
     },
   },
-})
+}
+
+const prodOptions = {}
+
+const options = process.env.NODE_ENV === 'development' ? devOptions : prodOptions
+
+export const logger = pino(options)
