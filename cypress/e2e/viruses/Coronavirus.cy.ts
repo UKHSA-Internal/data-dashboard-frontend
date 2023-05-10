52294752fc02aa37b6c14c5a2ca7d18c95963e4f
Tom  Christian <tctc91@gmail.com>
2023-05-10 11:43:27 +0100

    fix: tz

--- cypress/e2e/viruses/Coronavirus.cy.ts
+++ cypress/e2e/viruses/Coronavirus.cy.ts
@@ -32,7 +32,7 @@ describe('Coronavirus topic page', () => {
     cy.findByRole('heading', { name: 'Coronavirus', level: 1 })
 
     // Last updated
-    cy.findByText('Last updated on Thursday, 4 May 2023 at 04:37pm')
+    cy.findByText('Last updated on Thursday, 4 May 2023 at 03:37pm')
 
     // Contents
     cy.findByRole('navigation', {
