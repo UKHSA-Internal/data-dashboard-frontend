27b2d26dd035a89438c04fea5686a0b2c01633ca
Tom  Christian <tctc91@gmail.com>
2023-05-16 13:02:39 +0100

    fix: remove chart mocks due to size issue with msw

--- src/api/models/cms/Page/Meta.ts
+++ src/api/models/cms/Page/Meta.ts
@@ -8,7 +8,7 @@ export const Meta = z.object({
   show_in_menus: z.boolean(),
   seo_title: z.string(),
   search_description: z.string(),
-  first_published_at: z.string(),
+  first_published_at: z.string().nullable(),
   alias_of: z.null(),
   parent: z.object({
     id: z.number(),
--- src/api/requests/cms/getPage.ts
+++ src/api/requests/cms/getPage.ts
@@ -22,6 +22,7 @@ const SharedPageData = z.object({
   title: z.string(),
   last_published_at: z.string(),
   related_links: RelatedLinks,
+  meta: Meta,
 })
 
 const WithHomeData = SharedPageData.extend({
