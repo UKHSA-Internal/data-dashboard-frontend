e15152966738669f4daab8c5be2aa38e30a3cc38
Rhys <rhys.howard@burendo.com>
2023-05-22 15:55:18 +0100

    CDD-837 - Added additional spacing, CDD-773 - Added max width wrapping so no button overlap

---
+++ src/components/CMS/Cards/ChartColumn/ChartColumn.styles.tsx
@@ -0,0 +1,7 @@
+import { Paragraph } from 'govuk-react'
+import styled from 'styled-components'
+
+export const SecondaryText = styled(Paragraph)`
+  margin-top: 8px;
+  max-width: calc(100% - 105px);
+`
