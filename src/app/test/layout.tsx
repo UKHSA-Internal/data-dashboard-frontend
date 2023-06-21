83ad5e81e19f3b93776c0197f0992879d3460a6d
tctc91 <git-commit-address>
2023-06-21 16:25:31 +0100

    chore: setup tailwind and gds

--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -1,3 +1,7 @@
 export default async function HomeLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return (
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-two-thirds">{children}</div>
+    </div>
+  )
 }
