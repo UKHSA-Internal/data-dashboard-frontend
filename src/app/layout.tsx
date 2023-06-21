d87bb5040a2ac37edfe896994880f3b4e95718a1
tctc91 <git-commit-address>
2023-06-21 16:25:31 +0100

    chore: setup tailwind and gds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
+import { Header } from '@/components/AppDir/Header/Header'
 import { Metadata } from 'next'
+
+import './globals.scss'
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -8,7 +11,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <div className="govuk-width-container">
+          <main className="govuk-main-wrapper">{children}</main>
+        </div>
+      </body>
     </html>
   )
 }
