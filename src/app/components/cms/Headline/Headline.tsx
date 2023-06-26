9acec087dc7e8a2fe00f29a680192c662bf63460
tctc91 <git-commit-address>
2023-06-26 19:39:28 +0100

    chore: cleanup

---
+++ src/app/components/cms/Headline/Headline.tsx
@@ -0,0 +1,3 @@
+export async function Headline() {
+  return null
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,42 @@ module.exports = {
     fontFamily: {
       sans: ['var(--font-roboto)'],
     },
-    // https://design-system.service.gov.uk/styles/spacing/
-    spacing: {
-      0: '0',
-      1: '5px',
-      2: '10px',
-      3: '15px',
-      4: '20px',
-      5: '25px',
-      6: '30px',
-      7: '40px',
-      8: '50px',
-      9: '60px',
+    fontSize: {
+      // Note: Use govuk classes for font sizes
+      // https://design-system.service.gov.uk/styles/spacing/
     },
+    spacing: {
+      // Note: Use govuk classes for sizing (padding, margins etc)
+      // https://design-system.service.gov.uk/styles/spacing/
+    },
+    // https://design-system.service.gov.uk/styles/colour/
     colors: {
       grey: {
-        1: '#6f777b',
-        2: '#bfc1c3',
-        3: '#f3f2f1',
-        4: '#f8f8f8',
+        1: 'var(--colour-grey-1)',
+        2: 'var(--colour-grey-2)',
+        3: 'var(--colour-grey-3)',
+        4: 'var(--colour-grey-4)',
       },
-      blue: '#1d70b8',
+      red: 'var(--colour-red)',
+      yellow: 'var(--colour-yellow)',
+      green: 'var(--colour-green)',
+      blue: 'var(--colour-blue)',
+      'dark-blue': 'var(--colour-dark-blue)',
+      'light-blue': 'var(--colour-light-blue)',
+      purple: 'var(--colour-purple)',
+      black: 'var(--colour-black)',
+      'dark-grey': 'var(--colour-dark-grey)',
+      'mid-grey': 'var(--colour-mid-grey)',
+      'light-grey': 'var(--colour-light-grey)',
+      white: 'var(--colour-white)',
+      'light-purple': 'var(--colour-light-purple)',
+      'bright-purple': 'var(--colour-bright-purple)',
+      pink: 'var(--colour-pink)',
+      'light-pink': 'var(--colour-light-pink)',
+      orange: 'var(--colour-orange)',
+      brown: 'var(--colour-brown)',
+      'light-green': 'var(--colour-light-green)',
+      turquoise: 'var(--colour-turquoise)',
     },
     extend: {},
   },
