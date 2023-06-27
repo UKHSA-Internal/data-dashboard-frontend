783c0762ef4feecb6365c3e0e81742da0de2c157
tctc91 <git-commit-address>
2023-06-27 16:08:55 +0100

    fix: await

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -38,7 +38,7 @@ async function getPage() {
 
 export default async function Page() {
   if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-    initMocks()
+    await initMocks()
   }
 
   const page = await getPage()
