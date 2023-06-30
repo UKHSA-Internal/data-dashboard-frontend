3f7062fae50774d63133563f06898cfcf6fb3b0b
tctc91 <git-commit-address>
2023-06-30 15:02:25 +0100

    chore: added log for health

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -1,5 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { logger } from '@/lib/logger'
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).send(null)
+  logger.info('healthy')
+  return res.status(200).json({ status: 'ok' })
 }
