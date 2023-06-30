acadd7cf9397e89b637c319a48f1417c5c581de9
tctc91 <git-commit-address>
2023-06-30 13:00:03 +0100

    chore: tests

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).send(null)
+  return res.status(200).json({ status: 'ok' })
 }
