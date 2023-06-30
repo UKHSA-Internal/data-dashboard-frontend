90f85866a71f5e18069f1f1ba77139d92f023c63
Rhys <rhys.howard@burendo.com>
2023-06-30 13:54:34 +0100

    Updated tests

--- src/pages/feedback/confirmation.test.tsx
+++ src/pages/feedback/confirmation.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react'
+import { GetStaticPropsContext } from 'next'
 
-import FeedbackConfirmation from './confirmation'
+import FeedbackConfirmation, { getStaticProps } from './confirmation'
 
-test('Shows confirmation panel', () => {
-  render(<FeedbackConfirmation />)
+test('Shows confirmation panel', async () => {
+  const { props } = (await getStaticProps({ locale: 'en' } as GetStaticPropsContext)) as {
+    props: Record<string, never>
+  }
+
+  render(FeedbackConfirmation.getLayout(<FeedbackConfirmation {...props} />))
 
   expect(screen.getByText('Form submitted')).toBeInTheDocument()
   expect(screen.getByText('Thank you for your feedback')).toBeInTheDocument()
