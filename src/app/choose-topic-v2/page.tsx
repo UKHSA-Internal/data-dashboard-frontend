b7f865ba2ea83e405bb7e4a2e4f2443b1061c9f6
tctc91 <git-commit-address>
2023-07-03 09:12:41 +0100

    chore: new pages

---
+++ src/app/choose-topic-v2/page.tsx
@@ -0,0 +1,9 @@
+import { Wrapper } from '@/app/components/ui/ukhsa'
+
+export default async function ChooseTopicPage() {
+  return (
+    <Wrapper heading={'heading'} description={'description'} lastUpdated={'lastUpdated'}>
+      choose topic
+    </Wrapper>
+  )
+}
