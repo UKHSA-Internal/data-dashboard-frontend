410b4aa3b0688d99fbfc66f0fbbab8ff307ba8d4
Tom Christian <hello@elseif.io>
2023-07-11 13:26:33 +0100

    chore: update src/api/requests/helpers.ts

--- src/api/requests/helpers.ts
+++ src/api/requests/helpers.ts
@@ -1,6 +1,5 @@
 const baseUrl = process.env.API_URL
 
-// Non-CMS endpoints don't have an `/api` prefix at the moment
 export const getApiBaseUrl = () => `${baseUrl}/api`
 
 export const getCmsApiPath = () => {
