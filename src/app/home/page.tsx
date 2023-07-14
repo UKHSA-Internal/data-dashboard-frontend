1be5d15605147dcc812e7d6354a90fa85068fa2c
tctc91 <git-commit-address>
2023-07-14 16:05:35 +0100

    chore: msw first request

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 
+import { initMocks } from '@/api/msw'
 import { PageType } from '@/api/requests/cms/getPages'
 import { getPageBySlug } from '@/api/requests/getPageBySlug'
 
@@ -9,6 +10,10 @@ import { renderSection } from '../utils/cms.utils'
 export const revalidate = 3600 // revalidate every hour
 
 export async function generateMetadata(): Promise<Metadata> {
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+    initMocks()
+  }
+
   const {
     meta: { seo_title, search_description },
   } = await getPageBySlug('respiratory-viruses', PageType.Home)
@@ -19,6 +24,10 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function HomePage() {
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+    initMocks()
+  }
+
   const { title, body, page_description: description } = await getPageBySlug('respiratory-viruses', PageType.Home)
 
   return (
