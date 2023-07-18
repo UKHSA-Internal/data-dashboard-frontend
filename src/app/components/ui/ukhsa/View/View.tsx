f45f91f26789c317abff491cdc371a801ae1ff2c
Rhys <rhys.howard@burendo.com>
2023-07-18 14:18:48 +0100

    CDD-973 - Initial create sidenav

--- src/app/components/ui/ukhsa/View/View.tsx
+++ src/app/components/ui/ukhsa/View/View.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 
+import { SideNav } from '@/components/SideNav/SideNav'
+
 import { useTranslation } from '../../../../i18n'
 
 interface PageProps {
@@ -14,18 +16,31 @@ export async function View({ heading, showWelcome, children, description, lastUp
   const { t } = await useTranslation('common')
 
   return (
-    <>
-      {lastUpdated && (
-        <p className="govuk-!-margin-bottom-4 govuk-body-s">{t('lastUpdated', { value: new Date(lastUpdated) })}</p>
-      )}
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-one-quarter ukhsa-sidebar">
+        <SideNav
+          links={[
+            { name: 'Dashboard', href: '/' },
+            { name: 'API', href: '/api' },
+            { name: 'Maps', href: '/maps' },
+            { name: 'About', href: '/about' },
+            { name: "What's new", href: '/whats-new' },
+          ]}
+        />
+      </div>
+      <div className="govuk-grid-column-three-quarters">
+        {lastUpdated && (
+          <p className="govuk-!-margin-bottom-4 govuk-body-s">{t('lastUpdated', { value: new Date(lastUpdated) })}</p>
+        )}
 
-      {showWelcome && <p className="govuk-body-l govuk-!-margin-bottom-1 text-dark-grey">Welcome</p>}
+        {showWelcome && <p className="govuk-body-l govuk-!-margin-bottom-1 text-dark-grey">Welcome</p>}
 
-      <h1 className="govuk-heading-xl govuk-!-margin-bottom-4">{heading}</h1>
+        <h1 className="govuk-heading-xl govuk-!-margin-bottom-4">{heading}</h1>
 
-      {description && <div dangerouslySetInnerHTML={{ __html: description }} />}
+        {description && <div dangerouslySetInnerHTML={{ __html: description }} />}
 
-      {children}
-    </>
+        {children}
+      </div>
+    </div>
   )
 }
