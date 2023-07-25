eb81363d2ba1a6eee20d83f55a33ee08f63e4743
tctc91 <git-commit-address>
2023-07-25 13:40:35 +0100

    chore: migrated common pages

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from 'next'
+
+import { PageType } from '@/api/requests/cms/getPages'
+import { getPageBySlug } from '@/api/requests/getPageBySlug'
+
+import { RichText } from '../components/cms'
+import { View } from '../components/ui/ukhsa'
+
+export const revalidate = 3600 // revalidate every hour
+
+export async function generateMetadata({ params: { slug } }: { params: { slug: string } }): Promise<Metadata> {
+  const {
+    meta: { seo_title, search_description },
+  } = await getPageBySlug(slug, PageType.Common)
+
+  return {
+    title: seo_title,
+    description: search_description,
+  }
+}
+
+export default async function CommonPage({ params: { slug } }: { params: { slug: string } }) {
+  const { title, body, last_published_at: lastUpdated } = await getPageBySlug(slug, PageType.Common)
+
+  return (
+    <View heading={title} lastUpdated={lastUpdated}>
+      <RichText linkedHeadings>{body}</RichText>
+    </View>
+  )
+}
