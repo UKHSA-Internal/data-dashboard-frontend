fd3ccc16edc51b5850c8f7dd4e88bf778f231fbd
tctc91 <git-commit-address>
2023-07-25 10:06:28 +0100

    chore: feedback tweaks

--- src/__tests__/pages/feedback/confirmation.test.tsx
+++ src/__tests__/pages/feedback/confirmation.test.tsx
@@ -5,7 +5,7 @@ import FeedbackConfirmation, { getStaticProps } from '@/pages/feedback/confirmat
 
 jest.mock('next/router', () => require('next-router-mock'))
 
-test('Shows confirmation panel', async () => {
+test('Shows confirmation page', async () => {
   const { props } = (await getStaticProps({ locale: 'en' } as GetStaticPropsContext)) as {
     props: Record<string, never>
   }
@@ -14,6 +14,19 @@ test('Shows confirmation panel', async () => {
 
   expect(screen.getByRole('heading', { name: 'Form submitted', level: 1 })).toBeInTheDocument()
   expect(screen.getByText('Thank you for your feedback')).toBeInTheDocument()
-
+  expect(
+    screen.getByText(
+      "Thank you for taking the time to give feedback on the UKHSA data dashboard. We'll use your feedback to help us continually improve our services."
+    )
+  ).toBeInTheDocument()
+  expect(
+    screen.getByText(
+      "We're constantly looking to improve our users' experience of the dashboard. If you'd like to share your thoughts further, please get in touch with our user research team at"
+    )
+  ).toBeInTheDocument()
+  expect(screen.getByText('researchteam.dpd@ukhsa.gov.uk')).toHaveAttribute(
+    'href',
+    'mailto:researchteam.dpd@ukhsa.gov.uk'
+  )
   expect(screen.getByText('Return to home page')).toHaveAttribute('href', '/')
 })
