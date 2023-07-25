620974f7cb6cb530d8dbcddc731db1894f7dbff2
Rhys <rhys.howard@burendo.com>
2023-07-25 15:40:30 +0100

    Updating browse page, creating block navigation object

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,29 +1,48 @@
 'use Client'
 
-import { SideNav, SideNavLink, SideNavSubMenu, SideNavSubMenuLink } from '../components/ui/ukhsa/SideNav/SideNav'
+import { BlockNav, NavLink } from '../components/ui/ukhsa/BlockNav.tsx/BlockNav'
 
 export default function Browse() {
   return (
     <div className="w-full">
-      <SideNav browse>
-        <SideNavLink
+      <BlockNav>
+        <NavLink
           href="/"
-          subMenu={
-            <SideNavSubMenu>
-              <SideNavSubMenuLink href="/topics/coronavirus">COVID-19</SideNavSubMenuLink>
-              <SideNavSubMenuLink href="/topics/influenza">Influenza</SideNavSubMenuLink>
-              <SideNavSubMenuLink href="/topics/other-respiratory-viruses">
-                Other respiratory viruses
-              </SideNavSubMenuLink>
-            </SideNavSubMenu>
-          }
-        >
-          Dashboard
-        </SideNavLink>
-        <SideNavLink href={`${process.env.PUBLIC_API_URL}/api/public/timeseries`}>API</SideNavLink>
-        <SideNavLink href="/about">About</SideNavLink>
-        <SideNavLink href="/whats-new">What&apos;s new</SideNavLink>
-      </SideNav>
+          name="Dashboard"
+          description="The UKHSA data dashboard is for anyone interested in UK health data. Currently, the dashboard reports data for respiratory viruses."
+        />
+        <NavLink
+          href="/topics/coronavirus"
+          name="COVID-19"
+          description="The UKHSA data dashboard is for anyone interested in UK health data. Currently, the dashboard reports data for respiratory viruses."
+        />
+        <NavLink
+          href="/topics/influenza"
+          name="Influenza"
+          description="The UKHSA data dashboard is for anyone interested in UK health data. Currently, the dashboard reports data for respiratory viruses."
+        />
+        <NavLink
+          href="/topics/other-respiratory-viruses"
+          name="Other respiratory viruses"
+          description="The UKHSA data dashboard is for anyone interested in UK health data. Currently, the dashboard reports data for respiratory viruses."
+        />
+
+        <NavLink
+          href={`${process.env.PUBLIC_API_URL}/api/public/timeseries`}
+          name="API"
+          description="Search and download data by using the UKHSA data dashboard’s API."
+        />
+        <NavLink
+          href="/about"
+          name="About"
+          description="The UKHSA data dashboard presents public health data in the UK. It’s produced by the UK Health Security Agency."
+        />
+        <NavLink
+          href="/whats-new"
+          name="What's new"
+          description="The UKHSA data dashboard is regularly updated with new data and features. View a timeline of changes."
+        />
+      </BlockNav>
     </div>
   )
 }
