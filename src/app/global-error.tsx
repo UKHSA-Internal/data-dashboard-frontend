69253cb1635ecd2d7c105b7d991c6e6c563444d0
tctc91 <git-commit-address>
2023-07-27 12:25:11 +0100

    fix: logger doesnot work client side

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import { logger } from '@/lib/logger'
-
 import Error from './error'
 
 export default function GlobalError({ error }: { error: Error }) {
-  logger.error('Global error page: ', error)
+  console.error('Global error page: ', error)
   return (
     <html>
       <body>
