09b181753b02b4f05f918690e149e99de6393346
tctc91 <git-commit-address>
2023-07-30 22:09:32 +0100

    fix: test

--- src/pages/feedback/confirmation.tsx
+++ src/pages/feedback/confirmation.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import { Trans } from 'next-i18next'
 import { ReactElement } from 'react'
 
 import { Layout } from '@/components/Layout'
@@ -12,19 +11,17 @@ const FeedbackConfirmation = () => {
           <h1 className="govuk-panel__title">Form submitted</h1>
           <div className="govuk-panel__body">Thank you for your feedback</div>
         </div>
-        <Trans i18nKey="feedback.confirmation.message">
-          <p>
-            Thank you for taking the time to give feedback on the UKHSA data dashboard. We&apos;ll use your feedback to
-            help us continually improve our services.
-          </p>
-          <p>
-            We&apos;re constantly looking to improve our users&apos; experience of the dashboard. If you&apos;d like to
-            share your thoughts further, please get in touch with our user research team at{' '}
-            <a className="govuk-link--no-visited-state" href="mailto:researchteam.dpd@ukhsa.gov.uk">
-              researchteam.dpd@ukhsa.gov.uk
-            </a>
-          </p>
-        </Trans>
+        <p>
+          Thank you for taking the time to give feedback on the UKHSA data dashboard. We&apos;ll use your feedback to
+          help us continually improve our services.
+        </p>
+        <p>
+          We&apos;re constantly looking to improve our users&apos; experience of the dashboard. If you&apos;d like to
+          share your thoughts further, please get in touch with our user research team at{' '}
+          <a className="govuk-link--no-visited-state" href="mailto:researchteam.dpd@ukhsa.gov.uk">
+            researchteam.dpd@ukhsa.gov.uk
+          </a>
+        </p>
         <Link href="/" className="govuk-link--no-visited-state govuk-!-margin-top-2 govuk-body inline-block">
           Return to home page
         </Link>
