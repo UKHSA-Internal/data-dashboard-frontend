f6acea62404c121ca7af1d23bfdd8f09ea719636
Rhys <rhys.howard@burendo.com>
2023-07-31 12:07:55 +0100

    Adding metadata, fixing failing tests

--- src/app/feedback/confirmation/confirmation.spec.tsx
+++ src/app/feedback/confirmation/confirmation.spec.tsx
@@ -1,32 +1,26 @@
-import { GetStaticPropsContext } from 'next'
-
 import FeedbackConfirmation from '@/app/feedback/confirmation/page'
 import { render, screen } from '@/config/test-utils'
 
 jest.mock('next/router', () => require('next-router-mock'))
 
 test('Shows confirmation page', async () => {
-  // const { props } = (await getStaticProps({ locale: 'en' } as GetStaticPropsContext)) as {
-  //   props: Record<string, never>
-  }
+  render(await FeedbackConfirmation())
 
-render(FeedbackConfirmation.getLayout(<FeedbackConfirmation />))
-
-expect(screen.getByRole('heading', { name: 'Form submitted', level: 1 })).toBeInTheDocument()
-expect(screen.getByText('Thank you for your feedback')).toBeInTheDocument()
-expect(
-  screen.getByText(
-    "Thank you for taking the time to give feedback on the UKHSA data dashboard. We'll use your feedback to help us continually improve our services."
+  expect(screen.getByRole('heading', { name: 'Form submitted', level: 1 })).toBeInTheDocument()
+  expect(screen.getByText('Thank you for your feedback')).toBeInTheDocument()
+  expect(
+    screen.getByText(
+      "Thank you for taking the time to give feedback on the UKHSA data dashboard. We'll use your feedback to help us continually improve our services."
+    )
+  ).toBeInTheDocument()
+  expect(
+    screen.getByText(
+      /We're constantly looking to improve our users' experience of the dashboard. If you'd like to share your thoughts further, please get in touch with our user research team at/
+    )
+  ).toBeInTheDocument()
+  expect(screen.getByRole('link', { name: /researchteam.dpd@ukhsa.gov.uk/ })).toHaveAttribute(
+    'href',
+    'mailto:researchteam.dpd@ukhsa.gov.uk'
   )
-).toBeInTheDocument()
-expect(
-  screen.getByText(
-    "We're constantly looking to improve our users' experience of the dashboard. If you'd like to share your thoughts further, please get in touch with our user research team at"
-  )
-).toBeInTheDocument()
-expect(screen.getByText('researchteam.dpd@ukhsa.gov.uk')).toHaveAttribute(
-  'href',
-  'mailto:researchteam.dpd@ukhsa.gov.uk'
-)
-expect(screen.getByText('Return to home page')).toHaveAttribute('href', '/')
+  expect(screen.getByText('Return to home page')).toHaveAttribute('href', '/')
 })
