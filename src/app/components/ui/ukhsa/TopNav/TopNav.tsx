cd86360eccd8d2ef65eccfef25cbacf1a25e3155
Rhys <rhys.howard@burendo.com>
2023-08-03 11:00:48 +0100

    Resolving padding and spacing issues for all mobile widths

--- src/app/components/ui/ukhsa/TopNav/TopNav.tsx
+++ src/app/components/ui/ukhsa/TopNav/TopNav.tsx
@@ -20,7 +20,7 @@ export const TopNav = ({ children }: { children: ReactNode }) => {
     <>
       <Link
         className={clsx(
-          ' ukhsa-chevron govuk-link govuk-link--inverse float-right ml-5 px-2 py-1 no-underline motion-reduce:transition-none md:right-[30px] md:top-[6px] lg:hidden',
+          'ukhsa-chevron govuk-link govuk-link--inverse float-right ml-5 mt-[-5px] px-2 py-2 no-underline shadow-none motion-reduce:transition-none md:right-[30px] md:top-[6px] lg:hidden',
           {
             open: menuOpen,
           }
@@ -36,7 +36,7 @@ export const TopNav = ({ children }: { children: ReactNode }) => {
 
       <nav
         className={clsx(
-          'sticky mx-[-15px] my-0 h-full min-w-[var(--ukhsa-nav-width)] overflow-hidden border-b-[1px] border-mid-grey bg-grey-3 sm:mx-[-30px] lg:mt-7 lg:hidden lg:h-full',
+          'sticky mx-[-15px] my-0 h-full w-[calc(100%+30px)] min-w-[var(--ukhsa-nav-width)] overflow-hidden border-b-[1px] border-mid-grey bg-grey-3 sm:mx-[-30px] sm:w-[calc(100%+60px)] md:mx-[-30px] lg:mt-7 lg:hidden lg:h-full',
           {
             'hidden h-0': !menuOpen,
           }
@@ -49,36 +49,3 @@ export const TopNav = ({ children }: { children: ReactNode }) => {
     </>
   )
 }
-
-// /**
-//  * TopNav link
-//  */
-// interface TopNavLinkProps {
-//   children: ReactNode
-//   href: string
-//   subMenu?: ReactNode
-// }
-
-// export const TopNavLink = ({ children, href, subMenu }: TopNavLinkProps) => {
-//   const pathname = usePathname()
-//   const isActive = pathname === href
-
-//   return (
-//     <li>
-//       <Link
-//         href={href}
-//         className={clsx(
-//           'govuk-link--no-visited-state block border-l-4 px-2 py-[8px] no-underline decoration-1 hover:underline [&:not(:focus)]:hover:bg-grey-3',
-//           {
-//             'border-[var(--colour-blue)] font-bold [&:not(:focus)]:bg-grey-3': isActive,
-//             'border-transparent no-underline': !isActive,
-//           }
-//         )}
-//         aria-current={isActive ? 'page' : undefined}
-//       >
-//         {children}
-//       </Link>
-//       {subMenu}
-//     </li>
-//   )
-// }
