811586451409d2b18bab2e6ec17eba83b1b6edff
tctc91 <git-commit-address>
2023-08-08 19:21:17 +0100

    fix: broken redirect

--- config/jest/jest.setup.js
+++ config/jest/jest.setup.js
@@ -2,3 +2,5 @@ import '@testing-library/jest-dom'
 import 'whatwg-fetch'
 
 jest.mock('next/router', () => require('next-router-mock'))
+
+jest.mock('@/lib/logger')
--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -17,18 +17,18 @@ export async function POST(req: NextRequest) {
       throw new Error('form submission to backend failed')
     }
 
-    return NextResponse.redirect(new URL('/feedback/confirmation', req.url))
+    return NextResponse.redirect(new URL('/feedback/confirmation', req.url), 302)
   } catch (error) {
     if (error instanceof ZodError) {
       // For validation errors, we bypass the database insertion and just redirect
       // directly to the confirmation page to simulate a valid submission. This is to satisfy
       // business requirements of having the form completely optional but still submittable...
-      return NextResponse.redirect(new URL('/feedback/confirmation', req.url))
+      return NextResponse.redirect(new URL('/feedback/confirmation', req.url), 302)
     }
 
     logger.error(error)
 
     // Anything else, we return an actual error to the ui
-    return NextResponse.redirect(new URL('/feedback/?error=1', req.url))
+    return NextResponse.redirect(new URL('/feedback/?error=1', req.url), 302)
   }
 }
