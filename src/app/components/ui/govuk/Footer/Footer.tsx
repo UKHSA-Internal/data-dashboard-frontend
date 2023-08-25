a3c8ec50ce52410067ca8e4f2fb84af525c7f8b1
Rhys <rhys.howard@burendo.com>
2023-08-25 13:38:52 +0100

    Updating compliance naming

--- src/app/components/ui/govuk/Footer/Footer.tsx
+++ src/app/components/ui/govuk/Footer/Footer.tsx
@@ -32,7 +32,7 @@ export const Footer = forwardRef<HTMLElement, FooterProps>(({ className, ...rest
               </li>
               <li className="govuk-footer__inline-list-item">
                 <Link href="/compliance" className="govuk-footer__link">
-                  Compliance statement
+                  Compliance
                 </Link>
               </li>
             </ul>
