8d55001dc2515678bf409c455caa776cf8914266
Rhys <rhys.howard@burendo.com>
2023-08-31 12:54:39 +0100

    Changed to govuk padding & useWindowScroll for location

--- src/app/components/ui/ukhsa/BackToTop/BackToTop.tsx
+++ src/app/components/ui/ukhsa/BackToTop/BackToTop.tsx
@@ -1,27 +1,40 @@
 'use client'
 
+import clsx from 'clsx'
+import { useWindowScroll } from 'react-use'
+
 interface BackToTopProps {
   label: string
   href?: string
 }
 
-export const BackToTop = ({ label, href = '#main-content' }: BackToTopProps) => (
-  <a
-    href={href}
-    className="govuk-link--no-visited-state sticky bottom-8 mt-[calc(100vh+20px)] inline-flex items-center px-[12px] py-[6px] opacity-90 [&:not(:focus)]:bg-white"
-    onClick={(event) => {
-      event.preventDefault()
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      })
-    }}
-  >
-    <div
-      className="govuk-!-margin-right-2 h-[16px] w-[16px] bg-back_to_top bg-center bg-no-repeat"
-      aria-hidden
-      data-testid="up-arrow"
-    />
-    {label}
-  </a>
-)
+export const BackToTop = ({ label, href = '#main-content' }: BackToTopProps) => {
+  const { y } = useWindowScroll()
+
+  return (
+    <a
+      href={href}
+      className={clsx(
+        'govuk-link--no-visited-state govuk-!-padding-1 govuk-!-padding-right-2 bottom-3 inline-flex items-center opacity-90 [&:not(:focus)]:bg-white',
+        {
+          sticky: y > 200,
+        }
+      )}
+      //mt-[calc(100vh+20px)]
+      onClick={(event) => {
+        event.preventDefault()
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        })
+      }}
+    >
+      <div
+        className="govuk-!-margin-right-2 h-[16px] w-[16px] bg-back_to_top bg-center bg-no-repeat"
+        aria-hidden
+        data-testid="up-arrow"
+      />
+      {label}
+    </a>
+  )
+}
