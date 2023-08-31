dcf83dcc2baa2ba7debc2a94a4eae709273d03a8
Rhys <rhys.howard@burendo.com>
2023-08-31 11:49:31 +0100

    Back to top background

--- src/app/components/ui/ukhsa/BackToTop/BackToTop.tsx
+++ src/app/components/ui/ukhsa/BackToTop/BackToTop.tsx
@@ -8,7 +8,7 @@ interface BackToTopProps {
 export const BackToTop = ({ label, href = '#main-content' }: BackToTopProps) => (
   <a
     href={href}
-    className="govuk-link--no-visited-state sticky bottom-8 mt-[calc(100vh+20px)] inline-flex items-center"
+    className="govuk-link--no-visited-state sticky bottom-8 mt-[calc(100vh+20px)] inline-flex items-center bg-white px-[12px] py-[6px] opacity-90"
     onClick={(event) => {
       event.preventDefault()
       window.scrollTo({
