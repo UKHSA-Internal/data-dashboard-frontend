95f8e7ff9dd8c9a17c4468b356215f7df859b3f8
Rhys <rhys.howard@burendo.com>
2023-09-19 10:58:36 +0100

    CDD-1220 - Updating to use feedback api

--- src/api/requests/suggestions/postSuggestions.ts
+++ src/api/requests/suggestions/postSuggestions.ts
@@ -4,14 +4,17 @@ import { client } from '@/api/api-utils'
 import { logger } from '@/lib/logger'
 import { feedbackSchema } from '@/schemas/feedback.schema'
 
+const baseUrl = process.env.FEEDBACK_API
+
 export const requestSchema = feedbackSchema
 
 export type RequestParams = z.infer<typeof requestSchema>
 
 export const postSuggestions = async (suggestions: RequestParams) => {
   try {
-    const { status } = await client<Response>('suggestions/v1', {
+    const { status } = await client<Response>('api/suggestions/v1', {
       body: suggestions,
+      baseUrl: baseUrl,
     })
     if (status !== 200) {
       throw new Error('Failed to submit, invalid status code')
