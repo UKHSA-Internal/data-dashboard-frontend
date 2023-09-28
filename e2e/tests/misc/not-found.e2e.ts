0c222be14498baca6b8a01da31f69f181f6e2e08
tctc91 <git-commit-address>
2023-09-28 12:19:02 +0100

    refactor: new e2e format

--- e2e/tests/misc/not-found.e2e.ts
+++ e2e/tests/misc/not-found.e2e.ts
@@ -1,24 +1,26 @@
 import { test } from '../../fixtures/app.fixture'
 
-test('Not found page', async ({ notFoundPage }) => {
-  await test.step('navigate to an unknown url', async () => {
-    await notFoundPage.goto('/some-non-existent-page')
+test.describe('Not found page', () => {
+  test('Default layout', async ({ notFoundPage }) => {
+    await test.step('navigate to an unknown url', async () => {
+      await notFoundPage.goto('/some-non-existent-page')
+    })
+    await test.step('displays not found page', async () => {
+      await notFoundPage.hasPageContent()
+    })
   })
-  await test.step('displays not found page', async () => {
-    await notFoundPage.hasPageContent()
-  })
-})
 
-test('Return to home page', async ({ notFoundPage, homePage }) => {
-  await test.step('navigate to an unknown url', async () => {
-    await notFoundPage.goto('/some-non-existent-page')
-  })
-  await test.step('click the return to home link', async () => {
-    await notFoundPage.clickReturnToHome()
-  })
-  await test.step('loads the home page', async () => {
-    await homePage.hasHeading()
-    await homePage.hasMetadata()
-    await homePage.hasWelcomeText()
+  test('Return to home page', async ({ notFoundPage, homePage }) => {
+    await test.step('navigate to an unknown url', async () => {
+      await notFoundPage.goto('/some-non-existent-page')
+    })
+    await test.step('click the return to home link', async () => {
+      await notFoundPage.clickReturnToHome()
+    })
+    await test.step('loads the home page', async () => {
+      await homePage.hasHeading()
+      await homePage.hasMetadata()
+      await homePage.hasWelcomeText()
+    })
   })
 })
