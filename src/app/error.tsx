acf93166d26e43eea92b85f78c15826d81bf7e6f
tctc91 <git-commit-address>
2023-09-28 12:21:38 +0100

    chore: update global error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,11 +2,11 @@
 
 import Link from 'next/link'
 
-export default function Error({ heading = 'Sorry, there is a problem with the service' }: { heading?: string }) {
+export default function Error() {
   return (
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-two-thirds-from-desktop">
-        <h1 className="govuk-heading-xl mb-4">{heading}</h1>
+        <h1 className="govuk-heading-xl mb-4">Sorry, there is a problem with the service</h1>
         <p>Please try again later.</p>
         <p>
           If you need to get in touch with UK Health Security Agency, you can find contact information at the bottom of
