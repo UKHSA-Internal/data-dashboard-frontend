3a05c5966beaafa61bed4cf4c0adfcabf696c76d
Rhys <rhys.howard@burendo.com>
2023-10-10 10:23:10 +0100

    Updating export route tests

--- src/app/api/chart/export/route.spec.ts
+++ src/app/api/chart/export/route.spec.ts
@@ -15,22 +15,30 @@ import { POST } from './route'
 jest.mock('@/lib/logger')
 jest.mock('@/api/api-utils')
 
-const formData = new FormData()
-formData.set('format', 'csv')
-formData.set(
-  'plots',
-  '{"topic":"COVID-19","metric":"new_cases_daily","stratum":"default","geography":"England","geography_type":"Nation","date_from":null,"date_to":null}'
-)
-
-const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
-  headers: {
-    get: () => 'http://localhost:3000',
-  },
-  formData: () => formData,
-})
-
 describe('POST /api/chart/export', () => {
   test('Downloads the requested chart in csv format', async () => {
+    const formData = new FormData()
+    formData.set('format', 'csv')
+    formData.set(
+      'plots',
+      JSON.stringify({
+        topic: 'COVID-19',
+        metric: 'new_cases_daily',
+        date_from: null,
+        date_to: null,
+        stratum: 'default',
+        geography: 'England',
+        geography_type: 'Nation',
+      })
+    )
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      headers: {
+        get: () => 'http://localhost:3000',
+      },
+      formData: () => formData,
+    })
+
     jest.mocked(client).mockResolvedValueOnce({
       data: downloadsCsvFixture,
       status: 200,
@@ -44,12 +52,32 @@ describe('POST /api/chart/export', () => {
   })
 
   test('Downloads the requested chart in json format', async () => {
+    const formData = new FormData()
+    formData.set('format', 'json')
+    formData.set(
+      'plots',
+      JSON.stringify({
+        topic: 'COVID-19',
+        metric: 'new_cases_daily',
+        date_from: null,
+        date_to: null,
+        stratum: 'default',
+        geography: 'England',
+        geography_type: 'Nation',
+      })
+    )
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      headers: {
+        get: () => 'http://localhost:3000',
+      },
+      formData: () => formData,
+    })
+
     jest.mocked(client).mockResolvedValueOnce({
       data: JSON.stringify(downloadsJsonFixture),
       status: 200,
     })
-
-    formData.set('format', 'json')
 
     const res = await POST(req)
 
@@ -59,12 +87,32 @@ describe('POST /api/chart/export', () => {
   })
 
   test('Returns status 301 when wrong form body is sent', async () => {
+    const formData = new FormData()
+    formData.set('format', 'not_valid')
+    formData.set(
+      'plots',
+      JSON.stringify({
+        topic: 'COVID-19',
+        metric: 'new_cases_daily',
+        date_from: null,
+        date_to: null,
+        stratum: 'default',
+        geography: 'England',
+        geography_type: 'Nation',
+      })
+    )
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      headers: {
+        get: () => 'http://localhost:3000',
+      },
+      formData: () => formData,
+    })
+
     jest.mocked(client).mockResolvedValueOnce({
       data: null,
       status: 200,
     })
-
-    formData.set('format', 'not_valid')
 
     const res = await POST(req)
 
@@ -83,16 +131,60 @@ describe('POST /api/chart/export', () => {
   })
 
   test('Returns status 301 when the proxied request fails', async () => {
+    const formData = new FormData()
+    formData.set('format', 'csv')
+    formData.set(
+      'plots',
+      JSON.stringify({
+        topic: 'COVID-19',
+        metric: 'new_cases_daily',
+        date_from: null,
+        date_to: null,
+        stratum: 'default',
+        geography: 'England',
+        geography_type: 'Nation',
+      })
+    )
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      headers: {
+        get: () => 'http://localhost:3000',
+      },
+      formData: () => formData,
+    })
+
     jest.mocked(client).mockResolvedValueOnce({
       data: null,
       status: 301,
     })
-
-    formData.set('format', 'csv')
 
     const res = await POST(req)
 
     expect(logger.error).toHaveBeenCalledWith('Proxied request to /api/downloads/v2 failed')
     expect(res.status).toBe(301)
   })
+})
+
+test('returns status 301 when the formData is not a string', async () => {
+  const fileExample = new Blob()
+
+  const formData = new FormData()
+  formData.set('format', 'csv')
+  formData.set('plots', fileExample)
+
+  const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+    headers: {
+      get: () => 'http://localhost:3000',
+    },
+    formData: () => formData,
+  })
+
+  jest.mocked(client).mockResolvedValueOnce({
+    data: null,
+    status: 301,
+  })
+
+  const res = await POST(req)
+
+  expect(res.status).toBe(301)
 })
