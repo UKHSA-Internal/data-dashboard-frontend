948b87e1affb14a15f1e8e225ffe9c8035d4f281
8lane <hello@elseif.io>
2023-10-24 12:07:55 +0100

    CDD-1299: Tabbed Cards (#241)
    * feat: tabbed cards setup
    
    * refactor: mobile tweaks
    
    * feat: js fallback
    
    * fix: fouc
    
    * fix: focus state
    
    * fix: download icon focus state
    
    * fix: manual tab activation
    
    * feat: tabs unit tests
    
    * fix: home e2es
    
    * fix: download e2e
    
    * fix: test format
    
    * fix: broken selector
    
    * fix: remove unused class
    
    * fix: link spacebar behaviour
    
    * fix: neutral styling for headline numbers row card
    
    ---------
    
    Co-authored-by: tctc91 <git-commit-address>

--- src/app/components/cms/Download/Download.tsx
+++ src/app/components/cms/Download/Download.tsx
@@ -64,9 +64,16 @@ export function Download({ chart }: DownloadProps) {
       ))}
 
       <button
-        className="govuk-link govuk-body govuk-!-margin-bottom-0 bg-download bg-[left_center] bg-no-repeat pl-5 text-blue hover:bg-download_dark hover:text-dark-blue print:hidden"
+        className="govuk-button govuk-button--primary govuk-!-margin-bottom-0 flex w-auto items-center gap-2 print:hidden"
         type="submit"
       >
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
+          <path fill="currentColor" d="M7 0h2v11H7zM0 16v-2h16v2z" />
+          <path fill="currentColor" d="M8.414 12.11 7 10.698 11.696 6l1.414 1.414z" />
+          <path fill="currentColor" d="M9 11H7V1h2z" />
+          <path fill="currentColor" d="M3 7.414 4.414 6l4.696 4.696-1.414 1.414z" />
+          <path fill="currentColor" d="M7.168 11.574 7.742 11l.889.889-.574.574z" />
+        </svg>
         {downloading ? 'Downloading (csv)' : 'Download (csv)'}
       </button>
     </form>
