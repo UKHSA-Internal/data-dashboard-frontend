b9463b3115d09f8b4080b7e46bae571d21b75b69
Rhys <rhys.howard@burendo.com>
2023-11-08 08:39:39 +0000

    Updating pages to use richtext & i18n components

--- src/app/(pages)/whats-new-v2/[slug]/page.tsx
+++ src/app/(pages)/whats-new-v2/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { Trans } from 'react-i18next/TransWithoutContext'
 
 import { PageType } from '@/api/requests/cms/getPages'
 import { getPageBySlug } from '@/api/requests/getPageBySlug'
@@ -26,10 +27,28 @@ export default async function WhatsNewChildPage({ params: { slug } }: { params: 
   )
 
   return (
-    <View heading={title} backLink="/whats-new-v2" lastUpdated={date_posted}>
+    <View backLink="/whats-new-v2" lastUpdated={date_posted}>
+      <div className={`govuk-tag govuk-tag--${badge.colour} govuk-!-margin-bottom-3`}>
+        <Trans
+          i18nKey="entryCategory"
+          t={t}
+          components={[<span key={0} className="govuk-visually-hidden" />]}
+          values={{ value: badge.text }}
+        />
+      </div>
+      <Trans
+        i18nKey="entryTitle"
+        t={t}
+        components={[
+          <h1 className="govuk-heading-xl govuk-!-margin-bottom-6" key={0}>
+            <span className="govuk-visually-hidden" key={0} />
+          </h1>,
+        ]}
+        values={{ value: title }}
+      />
+
       <div className="govuk-grid-row">
         <div className="govuk-grid-column-three-quarters-from-desktop">
-          <div className={`govuk-tag govuk-tag--${badge.colour} govuk-!-margin-bottom-3`}>{badge.text}</div>
           <div className="govuk-body-s">
             <RichText>{body}</RichText>
           </div>
