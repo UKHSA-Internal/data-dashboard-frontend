63fd8f4bc032b0ea8e9c3d1b7dd608ed6c015df5
Rhys <rhys.howard@burendo.com>
2023-11-16 08:32:44 +0000

    MetricsCard component & unit tests

---
+++ src/app/components/ui/ukhsa/MetricsCard/MetricsCard.spec.tsx
@@ -0,0 +1,23 @@
+import { render } from '@/config/test-utils'
+
+import { MetricsCard } from './MetricsCard'
+
+test('renders MetricsCard', async () => {
+  const { getByRole, getByText } = render(
+    await MetricsCard({
+      title: 'Test content',
+      slug: 'test',
+      description: 'Description for test content card',
+      category: 'Healthcare',
+      topic: 'COVID-19',
+      apiName: 'test_apiname',
+    })
+  )
+
+  expect(getByRole('heading', { level: 2, name: 'Test content' })).toBeVisible()
+  expect(getByRole('link', { name: 'Test content' })).toHaveAttribute('href', 'metrics/test')
+  expect(getByText('Description for test content card')).toBeVisible()
+  expect(getByText('Healthcare')).toBeVisible()
+  expect(getByText('COVID-19')).toBeVisible()
+  expect(getByText('test_apiname')).toBeVisible()
+})
