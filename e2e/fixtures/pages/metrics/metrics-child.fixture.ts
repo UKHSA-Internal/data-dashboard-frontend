08157b94683cdef7e8bee68c76c87ef7e857f93d
Rhys <rhys.howard@burendo.com>
2023-11-22 09:17:36 +0000

    Added e2es

---
+++ e2e/fixtures/pages/metrics/metrics-child.fixture.ts
@@ -0,0 +1,30 @@
+import type { Page } from '@playwright/test'
+import { expect } from '@playwright/test'
+
+export class MetricsChildPage {
+  readonly page: Page
+
+  constructor(page: Page) {
+    this.page = page
+  }
+
+  async goto() {
+    await this.page.goto('/metrics/new-cases-7days-sum')
+  }
+
+  async hasMetadata() {
+    await expect(this.page).toHaveTitle(/Metrics child | UKHSA data dashboard/)
+  }
+
+  async hasHeading(name: string) {
+    await expect(this.page.getByRole('heading', { name, level: 1 })).toBeVisible()
+  }
+
+  async hasLastUpdated() {
+    await expect(this.page.getByText(/Last updated on Tuesday, 24 October 2023 at 04:09pm/)).toBeVisible()
+  }
+
+  async clickBackButton() {
+    await this.page.getByRole('link', { name: 'Back', exact: true }).click()
+  }
+}
