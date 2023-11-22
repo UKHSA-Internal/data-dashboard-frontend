13e8b6979b64eee3bb6624d65e9b333d89099dfc
Rhys <rhys.howard@burendo.com>
2023-11-22 15:56:42 +0000

    adding comments to seperate

--- src/app/components/ui/ukhsa/MetricsCard/MetricsCard.spec.tsx
+++ src/app/components/ui/ukhsa/MetricsCard/MetricsCard.spec.tsx
@@ -16,12 +16,16 @@ test('renders MetricsCard', async () => {
 
   expect(getByRole('link')).toHaveAttribute('href', 'metrics/test')
   expect(getByText('Description for test content card')).toBeVisible()
+
+  // Category
   expect(getByText('Category')).toBeVisible()
   expect(getByText('Healthcare')).toBeVisible()
 
+  // Topic
   expect(getByText('Topic')).toBeVisible()
   expect(getByText('COVID-19')).toBeVisible()
 
+  // API name
   expect(getByText('API name')).toBeVisible()
   expect(getByText('test_apiname')).toBeVisible()
 })
