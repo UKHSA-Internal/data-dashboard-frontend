8f4cf70d8357d123975120730f4297f66d34065d
Rhys <rhys.howard@burendo.com>
2023-11-30 09:06:03 +0000

    GetPagesBySlug test

--- src/api/requests/getPageBySlug.spec.ts
+++ src/api/requests/getPageBySlug.spec.ts
@@ -1,14 +1,34 @@
-import { relatedLinksMock } from '@/mock-server/handlers/cms/pages/fixtures/elements'
-
 import { client } from '../api-utils'
+import { relatedLinksMock } from '../mocks/cms/data/elements'
 import { PageType } from './cms/getPages'
 import { getPageBySlug } from './getPageBySlug'
 
 jest.mock('@/lib/logger')
 jest.mock('@/api/api-utils')
+const getPages = jest.mocked(client)
 const getPage = jest.mocked(client)
 
 test('returns page successfully', async () => {
+  getPages.mockResolvedValueOnce({
+    status: 200,
+    data: {
+      items: [
+        {
+          id: 21,
+          meta: {
+            type: 'whats_new.WhatsNewParentPage',
+            detail_url: 'http://localhost/api/pages/21/',
+            html_url: null,
+            slug: 'whats-new',
+            show_in_menus: true,
+            first_published_at: '2023-10-24T16:09:35.359598+01:00',
+          },
+          title: "What's new",
+        },
+      ],
+    },
+  })
+
   getPage.mockResolvedValueOnce({
     status: 200,
     data: {
@@ -42,4 +62,35 @@ test('returns page successfully', async () => {
   })
 
   const result = await getPageBySlug('whats-new', PageType.WhatsNewParent)
+
+  expect(result).toEqual({
+    id: 21,
+    title: 'whats-new',
+    meta: {
+      type: 'whats_new.WhatsNewParentPage',
+      detail_url: 'http://localhost/api/pages/21/',
+      html_url: null,
+      slug: 'whats-new',
+      show_in_menus: true,
+      first_published_at: '2023-10-24T16:09:35.359598+01:00',
+      alias_of: null,
+      parent: {
+        id: 3,
+        meta: {
+          type: 'home.HomePage',
+          detail_url: 'http://localhost/api/pages/3/',
+          html_url: null,
+        },
+        title: 'UKHSA Dashboard Root',
+      },
+    },
+  })
 })
+
+// getPage with no type, expect error "no page type provided"
+
+// getPage with no slug, expect error "no slug provided"
+
+// page with weird type, error 'could not get pages with type'
+
+// page with no slug, error 'no page found for slug'
