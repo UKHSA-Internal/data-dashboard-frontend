6eda53fec4d953bb8b6f3b6a7827ca9eacd3eab4
Rhys <rhys.howard@burendo.com>
2023-12-01 07:49:13 +0000

    getPageBySlug success

--- src/api/requests/getPageBySlug.spec.ts
+++ src/api/requests/getPageBySlug.spec.ts
@@ -26,6 +26,9 @@ test('returns page successfully', async () => {
           title: "What's new",
         },
       ],
+      meta: {
+        total_count: 1,
+      },
     },
   })
 
@@ -65,8 +68,9 @@ test('returns page successfully', async () => {
 
   expect(result).toEqual({
     id: 21,
-    title: 'whats-new',
     meta: {
+      seo_title: "What's new | UKHSA data dashboard",
+      search_description: '',
       type: 'whats_new.WhatsNewParentPage',
       detail_url: 'http://localhost/api/pages/21/',
       html_url: null,
@@ -84,13 +88,10 @@ test('returns page successfully', async () => {
         title: 'UKHSA Dashboard Root',
       },
     },
+    title: "What's new",
+    date_posted: '2023-10-24',
+    body: '<p data-block-key="yjvuu">We regularly update the dashboard with new data and features. Here we&#x27;ll show a timeline of changes that have happened outside of the weekly data refresh.</p>',
+    related_links: relatedLinksMock,
+    last_published_at: '2023-10-24T16:09:35.359598+01:00',
   })
 })
-
-// getPage with no type, expect error "no page type provided"
-
-// getPage with no slug, expect error "no slug provided"
-
-// page with weird type, error 'could not get pages with type'
-
-// page with no slug, error 'no page found for slug'
