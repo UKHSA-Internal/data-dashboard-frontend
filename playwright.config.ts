5b793be9f65136f344ceade1e56537cbd41314d7
8lane <git-commit-address>
2023-12-07 15:49:44 +0000

    chore: remove env changes for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig, devices } from '@playwright/test'
-import dotenvFlow from 'dotenv-flow'
 
-dotenvFlow.config()
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://localhost:${process.env.PLAYWRIGHT_PORT || '3000'}`,
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
