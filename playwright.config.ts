dab6edf91376128f06c577745fc18402bac67e11
8lane <--global>
2024-01-29 15:16:18 +0000

    chore: restore dev

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,10 +58,10 @@ export default defineConfig({
     },
   ],
 
-  // /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'NODE_ENV=test npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'NODE_ENV=test npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 })
