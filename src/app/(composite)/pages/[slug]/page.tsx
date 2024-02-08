a928e7b815e4a4b7d4f55de7f338cdbf2ccebdb8
8lane <--global>
2024-02-08 13:58:11 +0000

    feat: add composite page route

---
+++ src/app/(composite)/pages/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import { Metadata } from 'next'
+
+import { PageType } from '@/api/requests/cms/getPages'
+import { getPageBySlug } from '@/api/requests/getPageBySlug'
+import { RichText } from '@/app/components/cms'
+import { RelatedLink, RelatedLinks, View } from '@/app/components/ui/ukhsa'
+
+export async function generateMetadata({ params: { slug } }: { params: { slug: string } }): Promise<Metadata> {
+  const {
+    meta: { seo_title, search_description },
+  } = await getPageBySlug(slug, PageType.Composite)
+
+  return {
+    title: seo_title,
+    description: search_description,
+  }
+}
+
+export default async function CompositePage({ params: { slug } }: { params: { slug: string } }) {
+  const {
+    title,
+    body,
+    last_published_at: lastUpdated,
+    related_links: relatedLinks,
+  } = await getPageBySlug(slug, PageType.Composite)
+
+  return (
+    <View heading={title} lastUpdated={lastUpdated}>
+      {body.map(({ type, id, value }) => {
+        if (type === 'text') {
+          return (
+            <RichText key={id} linkedHeadings>
+              {value}
+            </RichText>
+          )
+        }
+      })}
+      <RelatedLinks variant="footer">
+        {relatedLinks.map(({ title, body, url, id }) => (
+          <RelatedLink key={id} url={url} title={title}>
+            {body}
+          </RelatedLink>
+        ))}
+      </RelatedLinks>
+    </View>
+  )
+}
