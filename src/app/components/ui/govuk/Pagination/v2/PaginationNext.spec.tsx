ebd3915eaab7ff2b228a9e4d6528fa84a5e3867b
Rhys <rhys.howard@burendo.com>
2024-02-09 15:09:43 +0000

    Added tests for pagination components, removed old block

---
+++ src/app/components/ui/govuk/Pagination/v2/PaginationNext.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+import { PaginationNext } from './PaginationNext'
+
+test('Shows next section', () => {
+  render(<PaginationNext href="/nextLinkTest">test text</PaginationNext>)
+
+  expect(screen.getByRole('link', { name: 'Next page : test text' })).toHaveAttribute('href', '/nextLinkTest')
+})
