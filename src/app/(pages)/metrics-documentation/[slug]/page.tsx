fb8d3f6f8f980df1b474945f56024ecbf75ffa50
Rhys <rhys.howard@burendo.com>
2024-02-13 08:56:42 +0000

    Removing test stuff from slug

--- src/app/(pages)/metrics-documentation/[slug]/page.tsx
+++ src/app/(pages)/metrics-documentation/[slug]/page.tsx
@@ -3,10 +3,6 @@ import { Metadata } from 'next'
 import { PageType } from '@/api/requests/cms/getPages'
 import { getPageBySlug } from '@/api/requests/getPageBySlug'
 import { RichText } from '@/app/components/cms'
-import { usePaginationBlock } from '@/app/components/ui/govuk/Pagination/v2/hooks/usePaginationBlock'
-import { Pagination as PaginationV2 } from '@/app/components/ui/govuk/Pagination/v2/Pagination'
-import { PaginationNext } from '@/app/components/ui/govuk/Pagination/v2/PaginationNext'
-import { PaginationPrevious } from '@/app/components/ui/govuk/Pagination/v2/PaginationPrevious'
 import { PageSection, PageSectionWithContents, View } from '@/app/components/ui/ukhsa'
 import { useTranslation } from '@/app/i18n'
 
@@ -25,13 +21,6 @@ export async function generateMetadata({ params: { slug } }: { params: { slug: s
 
 export default async function MetricsChildPage({ params: { slug } }: { params: { slug: string } }) {
   const { t } = await useTranslation('metrics')
-  const { previousText, previousPageHref, nextText, nextPageHref } = usePaginationBlock({
-    links: [
-      { pageHref: '/metrics-documentation/new-cases-7days-sum', pageText: 'Overview' },
-      { pageHref: '/metrics-documentation/new-deaths-7days-change-percentage', pageText: 'What is an API' },
-      { pageHref: '/metrics-documentation/covid-occupied-beds-latest', pageText: 'Getting started' },
-    ],
-  })
 
   const {
     title,
@@ -47,11 +36,6 @@ export default async function MetricsChildPage({ params: { slug } }: { params: {
       <div className="govuk-grid-row">
         :
         <div className="govuk-grid-column-three-quarters-from-desktop">
-          <PaginationV2 variant="block">
-            {previousText && <PaginationPrevious href={previousPageHref}>{previousText}</PaginationPrevious>}
-            {nextText && <PaginationNext href={nextPageHref}>{nextText}</PaginationNext>}
-          </PaginationV2>
-
           <PageSectionWithContents>
             <PageSection heading={'Summary'}>
               <MetricsSummary topic={topic} group={group} metric={metric} />
