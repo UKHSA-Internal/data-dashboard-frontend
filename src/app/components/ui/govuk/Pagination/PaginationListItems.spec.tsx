46a9c774df21eef0c4671ad177cc5de9fef190c6
8lane <--global>
2024-02-20 15:22:26 +0000

    fix: broken e2es

--- src/app/components/ui/govuk/Pagination/PaginationListItems.spec.tsx
+++ src/app/components/ui/govuk/Pagination/PaginationListItems.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import { PaginationListItems } from './PaginationListItems'
 
 describe('Pagination list items', () => {
-  test('displays an under ordered list container', () => {
+  test('displays an unordered list container', () => {
     render(<PaginationListItems>test text</PaginationListItems>)
     expect(screen.getByRole('list')).toHaveClass('govuk-pagination__list')
     expect(screen.getByText('test text')).toBeInTheDocument()
