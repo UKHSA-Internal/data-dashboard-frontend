6a05c77fe766f007591cff6db153417b0fbfc415
8lane <--global>
2024-02-20 14:42:27 +0000

    chore: pagination component tests

--- src/app/components/ui/govuk/Pagination/PaginationNext.spec.tsx
+++ src/app/components/ui/govuk/Pagination/PaginationNext.spec.tsx
@@ -1,13 +1,32 @@
 import { render, screen } from '@testing-library/react'
+import { UrlObject } from 'url'
 
 import { PaginationNext } from './PaginationNext'
 
-test('Shows next section', () => {
-  render(
-    <PaginationNext href="/nextLinkTest" variant="block">
-      test text
-    </PaginationNext>
-  )
+describe('Pagination next link', () => {
+  test('"block" variant shows an icon, label and custom page name', () => {
+    render(
+      <PaginationNext href="/nextLinkTest" variant="block">
+        test text
+      </PaginationNext>
+    )
 
-  expect(screen.getByRole('link', { name: 'Next page : test text' })).toHaveAttribute('href', '/nextLinkTest')
+    expect(screen.getByTestId('icon-next')).toHaveAttribute('aria-hidden', 'true')
+    expect(screen.getByRole('link', { name: 'Next page : test text' })).toHaveAttribute('href', '/nextLinkTest')
+  })
+
+  test('"list-item" variant shows just an icon and label', () => {
+    render(<PaginationNext href="/nextLinkTest" variant="list-item" />)
+
+    expect(screen.getByTestId('icon-next')).toHaveAttribute('aria-hidden', 'true')
+    expect(screen.getByRole('link', { name: 'Next page' })).toHaveAttribute('href', '/nextLinkTest')
+  })
+
+  test('Using a UrlObject for the href prop', () => {
+    const href: UrlObject = { pathname: '/pathname', query: { page: 1 } }
+
+    render(<PaginationNext href={href} variant="list-item" />)
+
+    expect(screen.getByRole('link', { name: 'Next page' })).toHaveAttribute('href', '/pathname?page=1')
+  })
 })
--- src/app/components/ui/govuk/Pagination/PaginationNext.tsx
+++ src/app/components/ui/govuk/Pagination/PaginationNext.tsx
@@ -15,7 +15,11 @@ export const PaginationNext = ({ href, children, variant }: PaginationNextProps)
   return (
     <div className="govuk-pagination__next">
       <Link className="govuk-link govuk-pagination__link govuk-link--no-visited-state" href={href} rel="next">
-        {variant === 'list-item' && <span className="govuk-pagination__link-title">Next</span>}
+        {variant === 'list-item' && (
+          <span className="govuk-pagination__link-title">
+            Next<span className="govuk-visually-hidden"> page</span>
+          </span>
+        )}
         <svg
           aria-hidden="true"
           className="govuk-pagination__icon govuk-pagination__icon--next"
@@ -24,6 +28,7 @@ export const PaginationNext = ({ href, children, variant }: PaginationNextProps)
           viewBox="0 0 15 13"
           width="15"
           xmlns="http://www.w3.org/2000/svg"
+          data-testid="icon-next"
         >
           <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z" />
         </svg>
