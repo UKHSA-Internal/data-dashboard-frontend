18a0615c005d7c503b905d0b78ee9eef6dcc7b8d
8lane <tom.christian@ukhsa.gov.uk>
2024-02-21 09:39:02 +0000

    CDD-1724: Pagination refactor  (#324)
    * chore: pagination list item components integrated into whats new and metrics
    
    * chore: rename hook
    
    * chore: pagination component tests
    
    * fix: broken e2es
    
    * chore: hook tests
    
    ---------
    
    Co-authored-by: 8lane <--global>

--- src/app/components/ui/govuk/Pagination/v2/PaginationNext.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-import { PaginationNext } from './PaginationNext'
-
-test('Shows next section', () => {
-  render(<PaginationNext href="/nextLinkTest">test text</PaginationNext>)
-
-  expect(screen.getByRole('link', { name: 'Next page : test text' })).toHaveAttribute('href', '/nextLinkTest')
-})
--- src/app/components/ui/govuk/Pagination/v2/PaginationPrevious.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-import { PaginationPrevious } from './PaginationPrevious'
-
-test('Shows previous section', () => {
-  render(<PaginationPrevious href="/previousLinkTest">test text</PaginationPrevious>)
-
-  expect(screen.getByRole('link', { name: 'Previous page : test text' })).toHaveAttribute('href', '/previousLinkTest')
-})
