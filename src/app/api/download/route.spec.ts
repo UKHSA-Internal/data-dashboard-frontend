6497784167b5fce2a0da494ae1a8aaf1fdded410
8lane <--global>
2024-02-23 14:33:46 +0000

    chore: add missing coverage

--- src/app/api/download/route.spec.ts
+++ src/app/api/download/route.spec.ts
@@ -9,11 +9,116 @@ import { client } from '@/api/utils/api.utils'
 import { logger } from '@/lib/logger'
 import { bulkDownloadCsv, bulkDownloadJson } from '@/mock-server/handlers/bulkdownloads/fixtures/bulk-download-zip'
 
-import { POST } from './route'
+import { GET, POST } from './route'
 
 const clientMock = jest.mocked(client)
 
-describe('POST api/download/bulk', () => {
+describe('GET api/download', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
+  test('Downloads bulk data in csv format', async () => {
+    const url = new URL('http://localhost/mock-page')
+    url.searchParams.set('file_format', 'csv')
+    url.searchParams.set('endpoint', 'bulkdownloads/v1')
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      url: url.toString(),
+    })
+
+    clientMock.mockResolvedValueOnce({
+      data: bulkDownloadCsv,
+      status: 200,
+      headers: new Headers({ 'content-type': 'application/zip' }),
+    })
+
+    const res = await GET(req)
+
+    expect(client).toHaveBeenCalledWith('bulkdownloads/v1', {
+      searchParams: url.searchParams,
+    })
+    expect(logger.error).not.toHaveBeenCalled()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toEqual('application/zip')
+    expect(await res.text()).toEqual(bulkDownloadCsv)
+  })
+
+  test('Downloads bulk data in json format', async () => {
+    const url = new URL('http://localhost/mock-page')
+    url.searchParams.set('file_format', 'csv')
+    url.searchParams.set('endpoint', 'bulkdownloads/v1')
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      url: url.toString(),
+    })
+
+    clientMock.mockResolvedValueOnce({
+      data: JSON.stringify(bulkDownloadJson),
+      status: 200,
+      headers: new Headers({ 'content-type': 'application/zip' }),
+    })
+
+    const res = await GET(req)
+
+    expect(client).toHaveBeenCalledWith('bulkdownloads/v1', {
+      searchParams: url.searchParams,
+    })
+    expect(logger.error).not.toHaveBeenCalled()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toEqual('application/zip')
+    expect(await res.json()).toEqual(bulkDownloadJson)
+  })
+
+  test('Redirects to error page when the endpoint is not provided', async () => {
+    const url = new URL('http://localhost/mock-page')
+    url.searchParams.set('file_format', 'csv')
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      url: url.toString(),
+    })
+
+    clientMock.mockResolvedValueOnce({
+      data: null,
+      status: 200,
+      headers: new Headers({ 'content-type': 'application/zip' }),
+    })
+
+    const res = await GET(req)
+
+    expect(res).toBeUndefined()
+    expect(logger.info).not.toHaveBeenCalled()
+    expect(logger.error).toHaveBeenCalledWith('GET /api/download proxy endpoint failed')
+    expect(logger.error).toHaveBeenCalledWith(new Error('download api route handler - missing endpoint'))
+    expect(redirect).toHaveBeenCalledWith('/error')
+  })
+
+  test('Redirects to error page when backend api returns an error', async () => {
+    const url = new URL('http://localhost/mock-page')
+    url.searchParams.set('file_format', 'csv')
+    url.searchParams.set('endpoint', 'bulkdownloads/v1')
+
+    const req = Mock.of<NextRequest & { url: string; formData: () => FormData }>({
+      url: url.toString(),
+    })
+
+    clientMock.mockResolvedValueOnce({
+      data: null,
+      error: new Error('Failed!'),
+      status: 500,
+    })
+
+    const res = await GET(req)
+
+    expect(res).toBeUndefined()
+    expect(logger.info).toHaveBeenCalledWith('Triggering download to %s', 'bulkdownloads/v1')
+    expect(logger.error).toHaveBeenCalledWith('GET /api/download proxy endpoint failed')
+    expect(logger.error).toHaveBeenCalledWith(new Error('Failed!'))
+    expect(redirect).toHaveBeenCalledWith('/error')
+  })
+})
+
+describe('POST api/download', () => {
   beforeEach(() => {
     jest.resetAllMocks()
   })
--- src/app/utils/menu.utils.spec.ts
+++ src/app/utils/menu.utils.spec.ts
@@ -1,3 +1,5 @@
+import { ZodError } from 'zod'
+
 import { client } from '@/api/utils/api.utils'
 import { logger } from '@/lib/logger'
 import { allPagesMock } from '@/mock-server/handlers/cms/pages/fixtures/pages'
@@ -5,6 +7,10 @@ import { allPagesMock } from '@/mock-server/handlers/cms/pages/fixtures/pages'
 import { useMenu } from './menu.utils'
 
 const clientMock = jest.mocked(client)
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 test('fetches then formats the cms pages into a navigation menu', async () => {
   clientMock.mockResolvedValueOnce({
@@ -48,14 +54,32 @@ test('fetches then formats the cms pages into a navigation menu', async () => {
 })
 
 test('handles failed fetches to the cms', async () => {
-  clientMock.mockResolvedValueOnce({
+  clientMock.mockRejectedValue({
     data: null,
     status: 500,
   })
 
   const menu = await useMenu()
 
-  expect(logger.error).toHaveBeenCalled()
+  expect(logger.error).toHaveBeenCalledWith(
+    new ZodError([
+      {
+        code: 'invalid_type',
+        expected: 'array',
+        received: 'undefined',
+        path: ['items'],
+        message: 'Required',
+      },
+      {
+        code: 'invalid_type',
+        expected: 'object',
+        received: 'undefined',
+        path: ['meta'],
+        message: 'Required',
+      },
+    ])
+  )
+
   expect(menu).toEqual([
     {
       title: 'Homepage',
