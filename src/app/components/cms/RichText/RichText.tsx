94edaeb85703412f8d605eca9cdd0888931ed033
8lane <hello@elseif.io>
2024-03-04 14:09:38 +0000

    chore: add key to rich text

--- src/app/components/cms/RichText/RichText.tsx
+++ src/app/components/cms/RichText/RichText.tsx
@@ -1,3 +1,4 @@
+import { Fragment, useId } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 import {
@@ -16,7 +17,10 @@ interface RichTextProps {
 export const RichText = ({ children, linkedHeadings }: RichTextProps) => {
   const rehypePlugins: RehypePlugins = corePlugins || []
 
+  const id = useId()
+
   const showLinkedHeadings = linkedHeadings && children.includes('h2')
+
   if (showLinkedHeadings) {
     rehypePlugins.push(...(linkedHeadingsPlugins || []))
   }
@@ -24,10 +28,10 @@ export const RichText = ({ children, linkedHeadings }: RichTextProps) => {
   const components = { ...coreComponents, ...(showLinkedHeadings && linkedHeadingsComponents) }
 
   return (
-    <>
+    <Fragment key={id}>
       <ReactMarkdown rehypePlugins={corePlugins} components={components}>
         {children}
       </ReactMarkdown>
-    </>
+    </Fragment>
   )
 }
