5b937be96cf8a35f36538987e155d9410576a71d
8lane <hello@elseif.io>
2024-03-04 16:01:29 +0000

    feat: json and plaintext codeblock

--- src/app/components/cms/CodeBlock/CodeBlock.tsx
+++ src/app/components/cms/CodeBlock/CodeBlock.tsx
@@ -1,7 +1,6 @@
-'use client'
-
 import clsx from 'clsx'
 import { Highlight } from 'prism-react-renderer'
+import Prism from 'prismjs'
 
 import { theme } from './themes/govuk.theme'
 
@@ -11,16 +10,21 @@ interface CodeBlockProps {
   className?: string
 }
 
+;(typeof global !== 'undefined' ? global : window).Prism = Prism
+require('prismjs/components/prism-json')
+
 export const CodeBlock = ({ children, language, className }: CodeBlockProps) => {
+  const lang = language.toLowerCase()
+
   return (
-    <Highlight theme={theme} code={children} language={language}>
+    <Highlight prism={Prism} theme={theme} code={children} language={lang}>
       {({ style, tokens, getLineProps, getTokenProps }) => (
         <pre
           style={style}
           // eslint-disable-next-line tailwindcss/no-custom-classname
           className={clsx(
             className,
-            `language-${language} govuk-!-padding-4 govuk-!-margin-bottom-6 print:text-shadow-none overflow-auto`,
+            `language-${lang} govuk-!-padding-4 govuk-!-margin-bottom-6 print:text-shadow-none overflow-auto`,
             `whitespace-pre-wrap break-words font-[var(--ukhsa-font-code)] text-[var(--colour-code-text)] text-shadow-[0_1px_white]`
           )}
         >
