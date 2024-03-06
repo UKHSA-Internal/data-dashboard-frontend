27f06a00bc87ad91b23f5453bea77a3911640764
8lane <tom.christian@ukhsa.gov.uk>
2024-03-06 15:24:30 +0000

    CDD-1672: Remove API link (#330)
    * chore: remove api link
    
    * chore: remove old comment
    
    * chore: remove another

--- e2e/tests/smoke/smoke.e2e.ts
+++ e2e/tests/smoke/smoke.e2e.ts
@@ -55,11 +55,6 @@ test.describe('Smoke tests', () => {
       await accessibilityStatementPage.goto()
       await accessibilityStatementPage.hasHeading()
     })
-    // TODO: Removed API tests for now, until further API work done
-    // await test.step('loads the "api" external url', async () => {
-    //   await app.nav.getByRole('link', { name: /API/ }).click()
-    //   await app.page.waitForURL('http://uhd-uat-public-api-1804026123.eu-west-2.elb.amazonaws.com')
-    // })
     await test.step('loads the "Error" page', async () => {
       await errorPage.goto()
       await errorPage.hasHeading()
@@ -127,11 +122,6 @@ test.describe('Smoke tests - no JavaScript', () => {
       await accessibilityStatementPage.goto()
       await accessibilityStatementPage.hasHeading()
     })
-    // TODO: Removed API tests for now, until further API work done
-    // await test.step('loads the "api" external url', async () => {
-    //   await app.nav.getByRole('link', { name: /API/ }).click()
-    //   await app.page.waitForURL('http://uhd-uat-public-api-1804026123.eu-west-2.elb.amazonaws.com')
-    // })
     await test.step('loads the "Error" page', async () => {
       await errorPage.goto()
       await errorPage.hasHeading()
