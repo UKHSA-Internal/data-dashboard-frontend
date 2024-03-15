a1598ea69e8437e9f95daaffb022f2d7b19481e7
Rhys <rhys.howard@burendo.com>
2024-03-15 14:08:24 +0000

    Browse & bulk download pages

---
+++ src/app/(no-backToTop)/layout.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react'
+
+import { SideNav, SideNavLink, SideNavSubMenu, SideNavSubMenuLink } from '@/app/components/ui/ukhsa/SideNav/SideNav'
+import { useTranslation } from '@/app/i18n'
+import { useMenu } from '@/app/utils/menu.utils'
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  const menu = await useMenu()
+  const { t } = await useTranslation('common')
+
+  return (
+    <>
+      <div className="govuk-!-padding-top-4 flex flex-col gap-0 xl:flex-row xl:gap-7">
+        <SideNav>
+          {menu.map(({ title, slug, children }) => (
+            <SideNavLink
+              key={slug}
+              href={slug}
+              subMenu={
+                children && (
+                  <SideNavSubMenu>
+                    {children.map(({ title, slug }) => (
+                      <SideNavSubMenuLink key={slug} href={slug}>
+                        {title}
+                      </SideNavSubMenuLink>
+                    ))}
+                  </SideNavSubMenu>
+                )
+              }
+            >
+              {title}
+            </SideNavLink>
+          ))}
+        </SideNav>
+        <main className="govuk-main-wrapper govuk-!-padding-top-0" id="main-content" role="main">
+          {children}
+        </main>
+      </div>
+    </>
+  )
+}
