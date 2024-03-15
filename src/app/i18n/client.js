bda301ccea70f6b8b48433f1dca5440cfa64f616
8lane <hello@elseif.io>
2024-03-15 14:26:41 +0000

    chore: add new client useTranslation

---
+++ src/app/i18n/client.js
@@ -0,0 +1,47 @@
+'use client'
+
+// Adapted from this tutorial:
+// https://locize.com/blog/next-app-dir-i18n/
+
+import i18next from 'i18next'
+import resourcesToBackend from 'i18next-resources-to-backend'
+import { useEffect, useState } from 'react'
+import { initReactI18next, useTranslation as useTranslationOrg } from 'react-i18next'
+
+import { getOptions, languages } from './settings'
+
+const runsOnServerSide = typeof window === 'undefined'
+
+// eslint-disable-next-line import/no-named-as-default-member
+i18next
+  .use(initReactI18next)
+  .use(resourcesToBackend((language, namespace) => import(`../../../public/locales/${language}/${namespace}.json`)))
+  .init({
+    ...getOptions(),
+    lng: undefined, // let detect the language on client side
+    detection: {
+      order: ['path', 'htmlTag', 'cookie', 'navigator'],
+    },
+    preload: runsOnServerSide ? languages : [],
+  })
+
+export function useTranslation(ns, options = { lng: 'en' }) {
+  const ret = useTranslationOrg(ns, options)
+  const { i18n } = ret
+  if (runsOnServerSide && options.lng && i18n.resolvedLanguage !== lng) {
+    i18n.changeLanguage(lng)
+  } else {
+    const [activeLng, setActiveLng] = useState(i18n.resolvedLanguage)
+
+    useEffect(() => {
+      if (activeLng === i18n.resolvedLanguage) return
+      setActiveLng(i18n.resolvedLanguage)
+    }, [activeLng, i18n.resolvedLanguage])
+
+    useEffect(() => {
+      if (!options.lng || i18n.resolvedLanguage === options.lng) return
+      i18n.changeLanguage(options.lng)
+    }, [options.lng, i18n])
+  }
+  return ret
+}
