5604fb2cbf7bf0d8c5c71b8edbf63dff3dd74187
Rhys <rhys.howard@burendo.com>
2024-03-25 20:13:52 +0000

    Announcement tests & remove instances

---
+++ src/app/components/ui/ukhsa/Announcement/Annoucement.spec.tsx
@@ -0,0 +1,41 @@
+import { render } from '@/config/test-utils'
+
+import { Announcement } from './Announcement'
+
+test('renders "information variant" announcement', async () => {
+  const { getByText, container } = render(
+    await Announcement({
+      variant: 'Information',
+      heading: 'Test information heading',
+      summary: 'Summary information content for announcement',
+    })
+  )
+
+  expect(getByText('Information')).toHaveClass('govuk-tag')
+
+  const announcementElement = container.firstChild
+  expect(announcementElement).toHaveClass('govuk-inset-text govuk-!-margin-top-0 border-blue')
+
+  expect(getByText('Test information heading')).toHaveClass(
+    'govuk-heading-m govuk-!-margin-top-2 govuk-!-margin-bottom-2'
+  )
+  expect(getByText('Summary information content for announcement')).toBeVisible()
+})
+
+test('renders "warning variant" announcement', async () => {
+  const { getByText, container } = render(
+    await Announcement({
+      variant: 'Warning',
+      heading: 'Test warning heading',
+      summary: 'Summary warning content for announcement',
+    })
+  )
+
+  expect(getByText('Warning')).toHaveClass('govuk-tag govuk-tag--red')
+
+  const announcementElement = container.firstChild
+  expect(announcementElement).toHaveClass('govuk-inset-text govuk-!-margin-top-0 border-red')
+
+  expect(getByText('Test warning heading')).toHaveClass('govuk-heading-m govuk-!-margin-top-2 govuk-!-margin-bottom-2')
+  expect(getByText('Summary warning content for announcement')).toBeVisible()
+})
