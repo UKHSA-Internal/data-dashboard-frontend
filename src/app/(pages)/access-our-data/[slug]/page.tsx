d0649bc2adb08bcc57b7a828a1a7ab88f1be041f
8lane <hello@elseif.io>
2024-04-02 14:06:23 +0100

    fix: add last updated text to access our data

--- src/app/(pages)/access-our-data/[slug]/page.tsx
+++ src/app/(pages)/access-our-data/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next'
 
 import { PageType } from '@/api/requests/cms/getPages'
 import { getPageBySlug } from '@/api/requests/getPageBySlug'
+import { useTranslation } from '@/app/i18n'
 import { renderCompositeBlock } from '@/app/utils/cms.utils'
 
 export async function generateMetadata({ params: { slug } }: { params: { slug: string } }): Promise<Metadata> {
@@ -16,11 +17,18 @@ export async function generateMetadata({ params: { slug } }: { params: { slug: s
 }
 
 export default async function AccessOurDataChild({ params: { slug } }: { params: { slug: string } }) {
-  const { title, body } = await getPageBySlug<PageType.Composite>(slug)
+  const { t } = await useTranslation('common')
+
+  const { title, body, last_published_at: lastUpdated } = await getPageBySlug<PageType.Composite>(slug)
 
   return (
     <>
+      {lastUpdated && (
+        <p className="govuk-!-margin-bottom-0 govuk-body-xs">{t('lastUpdated', { value: new Date(lastUpdated) })}</p>
+      )}
+
       <h2 className="govuk-heading-l govuk-!-margin-bottom-4">{title}</h2>
+
       {body.map(renderCompositeBlock)}
     </>
   )
