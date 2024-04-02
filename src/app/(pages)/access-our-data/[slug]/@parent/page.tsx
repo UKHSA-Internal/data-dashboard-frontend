503313fb840eca2798dffe749a8931c5355d6a6a
Rhys <rhys.howard@burendo.com>
2024-04-02 08:53:29 +0100

    Announcements mobile responsiveness

--- src/app/(pages)/access-our-data/[slug]/@parent/page.tsx
+++ src/app/(pages)/access-our-data/[slug]/@parent/page.tsx
@@ -1,6 +1,7 @@
 import { getPages, PageType } from '@/api/requests/cms/getPages'
 import { getPageBySlug } from '@/api/requests/getPageBySlug'
 import { Contents, ContentsLink } from '@/app/components/ui/ukhsa'
+import { Announcement } from '@/app/components/ui/ukhsa/Announcement/Announcement'
 import { renderCompositeBlock } from '@/app/utils/cms.utils'
 
 export default async function ParentView() {
@@ -10,6 +11,21 @@ export default async function ParentView() {
 
   return (
     <>
+      <Announcement
+        variant="Information"
+        heading="Data for COVID-19 is delayed"
+        summary="Data is only current up to 20th December 2023. We apologise for the inconvenience and aim to get this resolved
+        as soon as possible"
+        className="govuk-!-margin-bottom-3"
+      />
+
+      <Announcement
+        variant="Warning"
+        heading="UKHSA data dashboard is being replaced"
+        summary="22nd December 2023 - The UKHSA data dashboard is being replaced by the gov.uk dashboard"
+        className="govuk-!-margin-bottom-3"
+      />
+
       {body.map(renderCompositeBlock)}
 
       {childPages.success && (
