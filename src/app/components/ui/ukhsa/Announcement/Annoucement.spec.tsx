3d9cad418c5ac4745f50ce0bd10ed5468f61e86a
Rhys <rhys.howard@burendo.com>
2024-04-03 09:24:14 +0100

    Updating announcement spec

--- src/app/components/ui/ukhsa/Announcement/Annoucement.spec.tsx
+++ src/app/components/ui/ukhsa/Announcement/Annoucement.spec.tsx
@@ -3,36 +3,39 @@ import { render } from '@/config/test-utils'
 import { Announcement } from './Announcement'
 
 test('renders "information variant" announcement', async () => {
-  const { getByText, container } = render(
+  const { getByRole, getByText } = render(
     await Announcement({
       variant: 'Information',
       heading: 'Test information heading',
-      summary: 'Summary information content for announcement',
+      summary: '<p>Summary information content for announcement</p>',
+      className: 'testing-new-classname',
     })
   )
 
   expect(getByText('Information')).toHaveClass('govuk-tag')
 
-  const announcementElement = container.firstChild
-  expect(announcementElement).toHaveClass('govuk-inset-text govuk-!-margin-top-0 border-blue')
+  const announcementElement = getByRole('alert')
+  expect(announcementElement).toHaveClass('govuk-inset-text govuk-!-margin-top-0 border-blue testing-new-classname')
+  expect(announcementElement).toHaveAttribute('aria-live', 'polite')
 
   expect(getByText('Test information heading')).toHaveClass('govuk-heading-s govuk-!-margin-bottom-0')
   expect(getByText('Summary information content for announcement')).toBeVisible()
 })
 
 test('renders "warning variant" announcement', async () => {
-  const { getByText, container } = render(
+  const { getByText, getByRole } = render(
     await Announcement({
       variant: 'Warning',
       heading: 'Test warning heading',
-      summary: 'Summary warning content for announcement',
+      summary: '<p>Summary warning content for announcement</p>',
     })
   )
 
   expect(getByText('Warning')).toHaveClass('govuk-tag govuk-tag--red')
 
-  const announcementElement = container.firstChild
+  const announcementElement = getByRole('alert')
   expect(announcementElement).toHaveClass('govuk-inset-text govuk-!-margin-top-0 border-red')
+  expect(announcementElement).toHaveAttribute('aria-live', 'assertive')
 
   expect(getByText('Test warning heading')).toHaveClass('govuk-heading-s govuk-!-margin-bottom-0')
   expect(getByText('Summary warning content for announcement')).toBeVisible()
