62e9436697a9ac6ca74e5a4cb46ca7264480aa1e
8lane <hello@elseif.io>
2024-04-15 15:38:00 +0100

    chore: remove old button type from mock

--- src/mock-server/handlers/cms/pages/fixtures/page/bulk-downloads.ts
+++ src/mock-server/handlers/cms/pages/fixtures/page/bulk-downloads.ts
@@ -32,18 +32,6 @@ export const bulkDownloadsPageMock: PageResponse<PageType.Composite> = {
         '<p data-block-key="0od86">You can download all the data from the UKHSA data dashboard charts on this page by clicking the Download button. </p><p data-block-key="ea5ul">This is useful if you want to analyse the data using your own tools. </p><p data-block-key="cqg56">The data is available in a zip file format, which has a size of about 70 kB so it should be quick to download even on slower internet connections.</p>',
       id: '5eeefac8-ca5e-4838-b63d-3ecbbba59eed',
     },
-    // TODO: Remove once button is fully deprecated
-    {
-      type: 'button',
-      value: {
-        text: 'Download',
-        loading_text: '',
-        endpoint: '/api/bulkdownloads/v1',
-        method: 'POST',
-        button_type: 'DOWNLOAD',
-      },
-      id: 'b1b97fd7-8c46-4cf4-bab1-1a3d7ae5a912',
-    },
     {
       type: 'internal_button',
       value: {
