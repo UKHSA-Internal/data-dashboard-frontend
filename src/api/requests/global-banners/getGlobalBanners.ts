2bf807b2a6e2d7f40169ae8dfbbae7e72cfb060c
8lane <tom.christian@ukhsa.gov.uk>
2024-04-16 09:38:08 +0100

    CDD-1659: Setup global banner api mocks (#368)
    * chore: setup global banner api mocks
    
    * refactor: consistent file naming

---
+++ src/api/requests/global-banners/getGlobalBanners.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod'
+
+import { client } from '@/api/utils/api.utils'
+import { logger } from '@/lib/logger'
+
+export const responseSchema = z.object({
+  'active-global-banner': z.object({
+    title: z.string(),
+    body: z.string(),
+    banner_type: z.enum(['Information', 'Warning']),
+  }),
+})
+
+export type ResponseSchema = z.infer<typeof responseSchema>
+
+export const getGlobalBanners = async () => {
+  try {
+    const { data } = await client<z.infer<typeof responseSchema>>('global-banners/v1')
+    return responseSchema.safeParse(data)
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(error.message)
+    }
+    return responseSchema.safeParse(error)
+  }
+}
