a850164c7bdb1d0ba6a5c9b87991680ea89d81a4
8lane <hello@elseif.io>
2024-04-18 14:25:21 +0100

    feat: setup dialog component

---
+++ src/app/components/ui/ukhsa/Dialog/Dialog.spec.tsx
@@ -0,0 +1,83 @@
+import userEvent from '@testing-library/user-event'
+import Link from 'next/link'
+
+import { render, screen } from '@/config/test-utils'
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './Dialog'
+
+const dialogHeader = (
+  <DialogHeader>
+    <DialogTitle>Dialog title</DialogTitle>
+    <DialogDescription>Dialog description</DialogDescription>
+  </DialogHeader>
+)
+
+const dialogContent = <DialogContent>{dialogHeader}</DialogContent>
+
+describe('Dialog', () => {
+  beforeEach(() => {
+    console.error = jest.fn()
+  })
+
+  test('Opening a dialog from a trigger', async () => {
+    const { container } = render(
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        {dialogContent}
+      </Dialog>
+    )
+
+    expect(container).toMatchSnapshot()
+    expect(screen.queryByRole('dialog', { name: 'Dialog title' })).not.toBeInTheDocument()
+
+    const triggerElement = screen.getByRole('button', { name: 'Open' })
+    await userEvent.click(triggerElement)
+
+    const dialog = screen.getByRole('dialog', { name: 'Dialog title' })
+    expect(dialog).toMatchSnapshot()
+
+    expect(dialog).toBeInTheDocument()
+    expect(screen.getByText('Dialog description')).toBeInTheDocument()
+  })
+
+  test('Defaulting open on page load', async () => {
+    render(<Dialog defaultOpen>{dialogContent}</Dialog>)
+
+    expect(screen.getByRole('dialog', { name: 'Dialog title' })).toBeInTheDocument()
+    expect(screen.getByText('Dialog description')).toBeInTheDocument()
+  })
+
+  test('Closing a dialog', async () => {
+    render(<Dialog defaultOpen>{dialogContent}</Dialog>)
+
+    expect(screen.getByRole('dialog', { name: 'Dialog title' })).toBeInTheDocument()
+    await userEvent.click(screen.getByRole('button', { name: 'Close' }))
+    expect(screen.queryByRole('dialog', { name: 'Dialog title' })).not.toBeInTheDocument()
+  })
+
+  test('Closing a dialog with a custom close button', async () => {
+    render(
+      <Dialog defaultOpen>
+        <DialogContent closeButton={<Link href="/custom-href">Custom close</Link>}>{dialogHeader}</DialogContent>
+      </Dialog>
+    )
+
+    expect(screen.getByRole('dialog', { name: 'Dialog title' })).toBeInTheDocument()
+
+    const customCloseBtn = screen.getByRole('link', { name: 'Custom close' })
+    expect(customCloseBtn).toBeInTheDocument()
+    await userEvent.click(customCloseBtn)
+
+    expect(screen.queryByRole('dialog', { name: 'Dialog title' })).not.toBeInTheDocument()
+  })
+
+  test('Fullscreen', () => {
+    render(
+      <Dialog defaultOpen>
+        <DialogContent fullscreen>{dialogHeader}</DialogContent>
+      </Dialog>
+    )
+
+    expect(screen.getByRole('dialog', { name: 'Dialog title' })).toHaveClass('w-full h-full')
+  })
+})
--- src/app/components/ui/ukhsa/Dialog/Dialog.tsx
+++ src/app/components/ui/ukhsa/Dialog/Dialog.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import * as DialogPrimitive from '@radix-ui/react-dialog'
-import clsx from 'clsx'
 import * as React from 'react'
+
+import { clsx } from '@/lib/clsx'
 
 const Dialog = DialogPrimitive.Root
 
@@ -19,7 +20,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={clsx(
-      'bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0  data-[state=open]:fade-in-0 fixed inset-0 z-50',
+      'bg-black/80 fixed inset-0 z-50  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     {...props}
@@ -29,21 +30,28 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
+    fullscreen?: boolean
+    closeButton?: React.ReactNode
+  }
+>(({ className, children, fullscreen, closeButton, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
       ref={ref}
       className={clsx(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white p-6 shadow-lg duration-200 sm:rounded-lg',
+        'fixed left-1/2 top-1/2 z-50 grid w-full -translate-x-1/2 -translate-y-1/2 gap-4 border-0 bg-white p-6 pt-[5rem] shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+        {
+          ['w-full h-full']: fullscreen,
+          ['max-w-lg']: !fullscreen,
+        },
         className
       )}
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-        X<span className="sr-only">Close</span>
+      <DialogPrimitive.Close className="focus:govuk-focus absolute left-4 top-4 disabled:pointer-events-none" asChild>
+        {closeButton || <button className="govuk-button govuk-button--secondary">Close</button>}
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
@@ -51,7 +59,7 @@ const DialogContent = React.forwardRef<
 DialogContent.displayName = DialogPrimitive.Content.displayName
 
 const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={clsx('space-y-1.5 flex flex-col text-center sm:text-left', className)} {...props} />
+  <div className={clsx('flex flex-col text-center sm:text-left', className)} {...props} />
 )
 DialogHeader.displayName = 'DialogHeader'
 
@@ -64,11 +72,7 @@ const DialogTitle = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Title
-    ref={ref}
-    className={clsx('text-lg font-semibold leading-none tracking-tight', className)}
-    {...props}
-  />
+  <DialogPrimitive.Title ref={ref} className={clsx('govuk-heading-l', className)} {...props} />
 ))
 DialogTitle.displayName = DialogPrimitive.Title.displayName
 
@@ -76,7 +80,7 @@ const DialogDescription = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Description ref={ref} className={clsx('text-sm text-muted-foreground', className)} {...props} />
+  <DialogPrimitive.Description ref={ref} className={clsx('govuk-body', className)} {...props} />
 ))
 DialogDescription.displayName = DialogPrimitive.Description.displayName
 
