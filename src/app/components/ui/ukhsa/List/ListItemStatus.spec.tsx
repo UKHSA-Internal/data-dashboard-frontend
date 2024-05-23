a1f719cc8edac68faf1d72855c044aa5aee2fe36
Rhys <rhys.howard@burendo.com>
2024-05-23 08:39:03 +0100

    Updated tests with correct naming

--- src/app/components/ui/ukhsa/List/ListItemStatus.spec.tsx
+++ src/app/components/ui/ukhsa/List/ListItemStatus.spec.tsx
@@ -84,47 +84,47 @@ describe('renders correct tag', () => {
   test('renders green tag', async () => {
     const { getByText } = render(await ListItemStatusTag({ type: 'heat', level: 'Green', region: 'East of England' }))
 
-    expect(getByText('green')).toHaveClass('govuk-tag--green')
-    expect(getByText('green')).toHaveAttribute(
+    expect(getByText('Green')).toHaveClass('govuk-tag--green')
+    expect(getByText('Green')).toHaveAttribute(
       'aria-label',
-      'There is currently a green heat alert status for East of England'
+      'There is currently a Green heat alert status for East of England'
     )
   })
 
   test('renders yellow tag', async () => {
     const { getByText } = render(await ListItemStatusTag({ type: 'heat', level: 'Yellow', region: 'South East' }))
 
-    expect(getByText('yellow')).toHaveClass('govuk-tag--yellow')
-    expect(getByText('yellow')).toHaveAttribute(
+    expect(getByText('Yellow')).toHaveClass('govuk-tag--yellow')
+    expect(getByText('Yellow')).toHaveAttribute(
       'aria-label',
-      'There is currently a yellow heat alert status for South East'
+      'There is currently a Yellow heat alert status for South East'
     )
   })
 
   test('renders amber tag', async () => {
     const { getByText } = render(await ListItemStatusTag({ type: 'cold', level: 'Amber', region: 'West Midlands' }))
 
-    expect(getByText('amber')).toHaveClass('govuk-tag--orange')
-    expect(getByText('amber')).toHaveAttribute(
+    expect(getByText('Amber')).toHaveClass('govuk-tag--orange')
+    expect(getByText('Amber')).toHaveAttribute(
       'aria-label',
-      'There is currently an amber cold alert status for West Midlands'
+      'There is currently an Amber cold alert status for West Midlands'
     )
   })
 
   test('renders red tag', async () => {
     const { getByText } = render(await ListItemStatusTag({ type: 'cold', level: 'Red', region: 'East Midlands' }))
 
-    expect(getByText('red')).toHaveClass('govuk-tag--red')
-    expect(getByText('red')).toHaveAttribute(
+    expect(getByText('Red')).toHaveClass('govuk-tag--red')
+    expect(getByText('Red')).toHaveAttribute(
       'aria-label',
-      'There is currently a red cold alert status for East Midlands'
+      'There is currently a Red cold alert status for East Midlands'
     )
   })
 
   test('renders no alerts tag', async () => {
     const { getByText } = render(await ListItemStatusTag({ type: 'heat', level: 'No alerts', region: 'London' }))
 
-    expect(getByText('no alerts')).not.toHaveClass('govuk-tag')
-    expect(getByText('no alerts')).toHaveAttribute('aria-label', 'There are currently no alerts for London')
+    expect(getByText('No alerts')).not.toHaveClass('govuk-tag')
+    expect(getByText('No alerts')).toHaveAttribute('aria-label', 'There are currently no alerts for London')
   })
 })
