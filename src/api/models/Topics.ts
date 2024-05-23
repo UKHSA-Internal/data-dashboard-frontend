f11704a9a4fa8a5ce318ccdbe2ed45a18a8563f0
8lane <hello@elseif.io>
2024-05-23 09:06:56 +0100

    feat: add support for measles

--- src/api/models/Topics.ts
+++ src/api/models/Topics.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
-export const Topics = z.enum(['COVID-19', 'Influenza', 'Adenovirus', 'RSV', 'Rhinovirus', 'Parainfluenza', 'hMPV'])
+export const Topics = z
+  .enum(['COVID-19', 'Influenza', 'Adenovirus', 'RSV', 'Rhinovirus', 'Parainfluenza', 'hMPV'])
+  .or(z.string())
 
 export type Topics = z.infer<typeof Topics>
