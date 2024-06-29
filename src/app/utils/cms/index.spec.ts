f94cd6dccde7b95c38d92e2382e1e0288c8493f1
8lane <hello@elseif.io>
2024-06-29 17:19:28 +0100

    chore: setup whats new to be fully cms driven

--- src/app/utils/cms/index.spec.ts
+++ src/app/utils/cms/index.spec.ts
@@ -1,6 +1,8 @@
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
+import { Mock } from 'ts-mockery'
 
+import { PageResponse } from '@/api/requests/cms/getPage'
 import { PageType } from '@/api/requests/cms/getPages'
 import { client } from '@/api/utils/api.utils'
 import { SearchParams, Slug } from '@/app/types'
@@ -11,12 +13,16 @@ import {
   dashboardMock,
   metricsChildMocks,
   metricsParentMock,
+  whatsNewChildMocks,
+  whatsNewParentMock,
 } from '@/mock-server/handlers/cms/pages/fixtures/page'
 import {
   pagesWithCompositeTypeMock,
   pagesWithHomeTypeMock,
   pagesWithMetricsChildTypeMock,
   pagesWithTopicTypeMock,
+  pagesWithWhatsNewChildTypeMock,
+  pagesWithWhatsNewParentTypeMock,
 } from '@/mock-server/handlers/cms/pages/fixtures/pages'
 
 import {
@@ -25,6 +31,7 @@ import {
   getPageMetadata,
   getPagesByContentType,
   getPageTypeBySlug,
+  getParentPage,
   validateUrlWithCms,
 } from '.'
 
@@ -49,7 +56,7 @@ describe('validateUrlWithCms', () => {
     const slug: Slug = []
     const result = await validateUrlWithCms(slug, PageType.Home)
 
-    expect(result).toEqual<Metadata>(dashboardMock)
+    expect(result).toEqual<PageResponse<PageType.Home>>(dashboardMock)
   })
 
   test('Root level page', async () => {
@@ -65,7 +72,7 @@ describe('validateUrlWithCms', () => {
     const slug: Slug = ['bulk-downloads']
     const result = await validateUrlWithCms(slug, PageType.Composite)
 
-    expect(result).toEqual<Metadata>(bulkDownloadsPageMock)
+    expect(result).toEqual<PageResponse<PageType.Composite>>(bulkDownloadsPageMock)
   })
 
   test('Nested metrics documentation child page', async () => {
@@ -76,7 +83,27 @@ describe('validateUrlWithCms', () => {
     const slug: Slug = ['metrics-documentation', 'new-cases-7days-sum']
     const result = await validateUrlWithCms(slug, PageType.MetricsChild)
 
-    expect(result).toEqual<Metadata>(metricsChildMocks[0])
+    expect(result).toEqual<PageResponse<PageType.MetricsChild>>(metricsChildMocks[0])
+  })
+
+  test('Nested whats new child page', async () => {
+    getPages.mockResolvedValueOnce({ status: 200, data: pagesWithWhatsNewChildTypeMock })
+    getPage.mockResolvedValueOnce({ status: 200, data: whatsNewChildMocks[0] }) // Mock initial child request
+    getPage.mockResolvedValueOnce({ status: 200, data: whatsNewParentMock }) // Mock parent request
+
+    const slug: Slug = ['whats-new', 'soft-launch-of-the-ukhsa-data-dashboard']
+    const result = await validateUrlWithCms(slug, PageType.WhatsNewChild)
+
+    const mock = whatsNewChildMocks[0]
+    const expectedResult = Mock.of<PageResponse<PageType.WhatsNewChild>>({
+      ...mock,
+      badge: {
+        ...mock.badge,
+        colour: 'grey',
+      },
+    })
+
+    expect(result).toEqual<PageResponse<PageType.WhatsNewChild>>(expectedResult)
   })
 
   test('404 Not Found when a URL cannot be matched against the CMS', async () => {
@@ -151,12 +178,50 @@ describe('getPageMetadata', () => {
     })
   })
 
-  test('Failing to get page metadata', async () => {
-    getPages.mockRejectedValueOnce({
-      success: false,
+  test('Getting metadata for whats-new', async () => {
+    getPages.mockResolvedValueOnce({ status: 200, data: pagesWithWhatsNewParentTypeMock })
+    getPage.mockResolvedValueOnce({ status: 200, data: whatsNewParentMock })
+    getPages.mockResolvedValueOnce({ status: 200, data: pagesWithWhatsNewChildTypeMock })
+
+    const slug: Slug = ['whats-new']
+    const searchParams: SearchParams = {}
+    const result = await getPageMetadata(slug, searchParams, PageType.WhatsNewParent)
+
+    expect(result).toEqual<Metadata>({
+      alternates: { canonical: 'http://fake-backend.gov.uk/whats-new' },
+      description: '',
+      openGraph: {
+        description: '',
+        title: "What's new (page 1 of 4) | UKHSA data dashboard",
+      },
+      title: "What's new (page 1 of 4) | UKHSA data dashboard",
+      twitter: {
+        description: '',
+        title: "What's new (page 1 of 4) | UKHSA data dashboard",
+      },
+    })
+  })
+
+  test('Failing to get whats-new metadata', async () => {
+    getPages.mockResolvedValueOnce({ status: 200, data: pagesWithWhatsNewParentTypeMock })
+    getPage.mockResolvedValueOnce({ status: 200, data: whatsNewParentMock })
+    getPages.mockRejectedValueOnce({ success: false, data: null, error: 'API call failed' })
+
+    const slug: Slug = ['whats-new']
+    const searchParams: SearchParams = {}
+    const result = await getPageMetadata(slug, searchParams, PageType.WhatsNewParent)
+
+    expect(logger.error).toHaveBeenCalledWith({
       data: null,
       error: 'API call failed',
+      success: false,
     })
+    expect(notFound).toHaveBeenCalled()
+    expect(result).not.toBeDefined()
+  })
+
+  test('Failing to get page metadata', async () => {
+    getPages.mockRejectedValueOnce({ success: false, data: null, error: 'API call failed' })
 
     const slug: Slug = ['bulk-downloads']
     const searchParams: SearchParams = {}
@@ -241,6 +306,26 @@ describe('getHomePage', () => {
       expect(logger.error).toHaveBeenCalledWith(new Error('No homepage matches found'))
       expect(notFound).toHaveBeenCalledTimes(1)
 
+      expect(result).not.toBeDefined()
+    })
+  })
+})
+
+describe('getParentPage', () => {
+  describe('Successfully getting the parent page for a given page', () => {
+    test('returns page successfully', async () => {
+      getPage.mockResolvedValueOnce({ status: 200, data: whatsNewParentMock })
+      const result = await getParentPage(whatsNewChildMocks[0])
+      expect(result).toEqual<PageResponse<PageType.WhatsNewParent>>(whatsNewParentMock)
+    })
+  })
+
+  describe('Failing to get pages from content type from the API', () => {
+    test('getting the pages from the API fails with a server error', async () => {
+      getPage.mockRejectedValueOnce({ success: false, data: null, error: 'API call failed' })
+      const result = await getParentPage(whatsNewChildMocks[0])
+      expect(logger.error).toHaveBeenCalledWith(expect.any(Error))
+      expect(notFound).toHaveBeenCalledTimes(1)
       expect(result).not.toBeDefined()
     })
   })
