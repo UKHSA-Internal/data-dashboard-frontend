42bf93f01b82db228148dae478bacaf736240157
8lane <hello@elseif.io>
2024-07-10 10:49:17 +0100

    fix: tag manager broken tests

--- src/app/components/cms/Table/Table.spec.tsx
+++ src/app/components/cms/Table/Table.spec.tsx
@@ -107,6 +107,7 @@ const mockData: ComponentProps<typeof Table>['data'] = {
   headline_number_columns: [],
   title: 'Table Title ABC/XYZ',
   body: 'Table Body',
+  tag_manager_event_id: '',
 }
 
 const mockSize = 'narrow'
