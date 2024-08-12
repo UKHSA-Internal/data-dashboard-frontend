32dedefecf241ae80d01ccbf229e549f0ebe6452
8lane <hello@elseif.io>
2024-08-12 10:50:47 +0100

    chore: comment

--- src/app/components/ui/ukhsa/MegaMenu/MegaMenu.tsx
+++ src/app/components/ui/ukhsa/MegaMenu/MegaMenu.tsx
@@ -9,6 +9,12 @@ interface MegaMenuProps {
   className?: string
 }
 
+/**
+ * This is an MVP implementation of the GOV.UK menu system that is hooked up to our CMS menu snippet system.
+ *
+ * Currently it is configured to only display responsive columns in a 1/3 grid system.
+ * TODO: Expand this to be more dynamic according to how we receive content from the CMS.
+ */
 export async function MegaMenu({ className = 'govuk-!-padding-top-7' }: MegaMenuProps) {
   const menu = await getMenu()
 
