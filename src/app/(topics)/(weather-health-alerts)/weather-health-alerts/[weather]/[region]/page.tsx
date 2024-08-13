98ada93e62dabf46e616cf2da91e815a4818b736
8lane <hello@elseif.io>
2024-08-13 15:55:14 +0100

    chore: clean out weather health feature flag

--- src/app/(topics)/(weather-health-alerts)/weather-health-alerts/[weather]/[region]/page.tsx
+++ src/app/(topics)/(weather-health-alerts)/weather-health-alerts/[weather]/[region]/page.tsx
@@ -4,22 +4,12 @@ import { HealthAlertTypes } from '@/api/models/Alerts'
 import { PageType } from '@/api/requests/cms/getPages'
 import { getPageBySlug } from '@/api/requests/getPageBySlug'
 import { RelatedLink, RelatedLinks } from '@/app/components/ui/ukhsa'
-import { flags } from '@/app/constants/flags.constants'
-import { getFeatureFlag } from '@/app/utils/flags.utils'
 
 import AlertBody from './AlertBody'
 
 export const dynamic = 'force-dynamic'
 
 export async function generateMetadata({ params: { region } }: { params: { region: string } }): Promise<Metadata> {
-  const { enabled } = await getFeatureFlag(flags.weatherHealthAlert)
-
-  if (!enabled)
-    return {
-      title: 'Page not found | UKHSA data dashboard',
-      description: 'Error - Page not found',
-    }
-
   return {
     title: `Weather alert for ${region} | UKHSA data dashboard`,
     description: `Weather alert for ${region}`,
