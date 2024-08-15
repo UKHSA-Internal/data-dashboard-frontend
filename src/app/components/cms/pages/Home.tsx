f1ad66d6a4c206581c107a36504f37399c9b5d44
8lane <hello@elseif.io>
2024-08-15 13:31:03 +0100

    chore: interactive charts

--- src/app/components/cms/pages/Home.tsx
+++ src/app/components/cms/pages/Home.tsx
@@ -2,11 +2,15 @@ import { RelatedLink, RelatedLinks, View } from '@/app/components/ui/ukhsa'
 import { getHomePage } from '@/app/utils/cms'
 import { renderSection } from '@/app/utils/cms.utils'
 
+import { PlotlyChart } from './PlotlyChart'
+
 export default async function HomePage() {
   const { title, body, page_description: description, related_links: relatedLinks } = await getHomePage()
 
   return (
     <View heading={title} description={description} showWelcome>
+      <PlotlyChart />
+      <br />
       {body.map(renderSection)}
 
       <RelatedLinks variant="footer">
