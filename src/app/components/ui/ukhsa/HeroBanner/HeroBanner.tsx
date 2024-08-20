8ab8064c8e95e17e6346496498105507c5b2eb1d
Rhys <rhys.howard@burendo.com>
2024-08-20 13:17:24 +0100

    Blue bar padding, button hover effects, not found issues resolved

--- src/app/components/ui/ukhsa/HeroBanner/HeroBanner.tsx
+++ src/app/components/ui/ukhsa/HeroBanner/HeroBanner.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 const HeroBanner = () => {
   return (
     <div className="bg-blue">
-      <div className="govuk-width-container govuk-!-padding-bottom-4  bg-blue ">
-        <h1 className="govuk-heading-xl govuk-!-padding-top-5 govuk-!-margin-bottom-4 text-white">
+      <div className="govuk-width-container govuk-!-padding-bottom-8  bg-blue ">
+        <h1 className="govuk-heading-xl govuk-!-padding-top-7 govuk-!-margin-bottom-4 text-white">
           UKHSA data dashboard
         </h1>
         <h2 className="govuk-heading-xl govuk-!-margin-bottom-3 text-offwhite">
