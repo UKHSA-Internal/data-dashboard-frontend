5c82eb54bbaebaf8f677ada5848c25eaca39b6e6
Rhys <rhys.howard@burendo.com>
2024-09-19 14:18:11 +0100

    Additional e2e updates

--- e2e/fixtures/pages/common/about.fixture.ts
+++ e2e/fixtures/pages/common/about.fixture.ts
@@ -26,7 +26,7 @@ export class AboutPage {
   }
 
   async hasLastUpdated() {
-    await expect(this.page.getByText(/Last updated on Friday, 12 May 2023 at 02:40pm/)).toBeVisible()
+    await expect(this.page.getByText(/Last updated on Tuesday, 2 July 2024 at 12:44pm/)).toBeVisible()
   }
 
   async hasPageContent() {
