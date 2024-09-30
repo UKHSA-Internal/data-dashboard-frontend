be135ec3f05a190d52306bb22d23372099c69c14
8lane <hello@elseif.io>
2024-09-30 14:36:10 +0100

    chore: use axios

--- src/api/requests/global-banners/getGlobalBanners.ts
+++ src/api/requests/global-banners/getGlobalBanners.ts
@@ -1,14 +1,20 @@
+import axios from 'axios'
+
 const baseUrl = process.env.API_URL ?? ''
 
 export const getGlobalBanners = async () => {
   try {
-    const req = await fetch(`${baseUrl}/api/global-banners/v1`, {
+    const { data } = await axios.get(`${baseUrl}/api/global-banners/v1`, {
       method: 'GET',
     })
-    console.log('get global banner')
-    const json = await req.json()
 
-    return json
+    console.log('AXIOS: ', data)
+
+    return {
+      success: true,
+      data,
+    }
+    // return json
   } catch (error) {
     console.log('error')
   }
