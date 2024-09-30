d860d8411404da4fb073fc23aeac2e1a1ae780bc
8lane <hello@elseif.io>
2024-09-30 14:40:49 +0100

    fix: ts

--- src/api/requests/global-banners/getGlobalBanners.ts
+++ src/api/requests/global-banners/getGlobalBanners.ts
@@ -1,6 +1,23 @@
 import axios from 'axios'
+import { z } from 'zod'
 
-const baseUrl = process.env.API_URL ?? ''
+import { getApiBaseUrl } from '../helpers'
+
+const baseUrl = getApiBaseUrl()
+
+export const bannerTypes = z.enum(['Information', 'Warning'])
+
+export const responseSchema = z.object({
+  active_global_banner: z
+    .object({
+      title: z.string(),
+      body: z.string(),
+      banner_type: bannerTypes,
+    })
+    .or(z.null()),
+})
+
+export type ResponseSchema = z.infer<typeof responseSchema>
 
 export const getGlobalBanners = async () => {
   try {
@@ -8,14 +25,8 @@ export const getGlobalBanners = async () => {
       method: 'GET',
     })
 
-    console.log('AXIOS: ', data)
-
-    return {
-      success: true,
-      data,
-    }
-    // return json
+    return responseSchema.safeParse(data)
   } catch (error) {
-    console.log('error')
+    return responseSchema.safeParse(error)
   }
 }
