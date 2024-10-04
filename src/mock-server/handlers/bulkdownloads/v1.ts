ff93f33060b65b4454fe201deb0f5a493a45ecaa
8lane <tom.christian@ukhsa.gov.uk>
2024-10-04 10:40:32 +0100

    CDD-2205: Memory leak fix - switch native `fetch` out for `cross-fetch` (#516)
    * chore: bump node to 20.17.0
    
    * fix: use cross-fetch instead of native fetch
    
    * chore: update download form fetch usage
    
    * fix: handle actual zip file in bulk dl tests

--- src/mock-server/handlers/bulkdownloads/v1.ts
+++ src/mock-server/handlers/bulkdownloads/v1.ts
@@ -1,30 +1,52 @@
+import archiver from 'archiver'
 import { Request, Response } from 'express'
 
 import { logger } from '@/lib/logger'
-
-import { bulkDownloadCsv, bulkDownloadJson } from './fixtures/bulk-download-zip'
 
 export default async function handler(req: Request, res: Response) {
   try {
     if (req.method !== 'GET') {
       logger.error(`Unsupported request method ${req.method}`)
-      return res.status(405)
+      return res.status(405).end()
     }
 
+    // Set headers for ZIP file
     res.setHeader('content-type', 'application/zip')
 
-    // Return a fixture based on the chosen file format
+    const archive = archiver('zip', {
+      zlib: { level: 9 }, // Compression level
+    })
+
+    // Archive error handling
+    archive.on('error', (err) => {
+      throw err
+    })
+
     if (req.query.file_format === 'csv') {
       res.setHeader('content-disposition', 'attachment; filename=ukhsa-mock-download-csv.zip')
-      return res.send(bulkDownloadCsv)
-    }
 
-    if (req.query.file_format === 'json') {
+      // Stream the zip data back to the response
+      archive.pipe(res)
+
+      // Add a file to the ZIP (simulated CSV content)
+      archive.append('mocked,file,csv', { name: 'mock.csv' })
+
+      await archive.finalize()
+    } else if (req.query.file_format === 'json') {
       res.setHeader('content-disposition', 'attachment; filename=ukhsa-mock-download-json.zip')
-      return res.json(bulkDownloadJson)
+
+      // Stream the zip data back to the response
+      archive.pipe(res)
+
+      // Add a file to the ZIP (simulated JSON content)
+      archive.append(JSON.stringify({ file: 'mocked file json' }, null, 2), { name: 'mock.json' })
+
+      await archive.finalize()
+    } else {
+      res.status(400).json({ error: 'Invalid file format' })
     }
   } catch (error) {
     logger.error(error)
-    return res.status(500)
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 }
