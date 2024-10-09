0c610d27836ad848b2efbdb1197fbcb7e69def65
8lane <hello@elseif.io>
2024-10-09 10:37:15 +0100

    chore: keep alive, static generation timeout and ipv4first

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const dns = require('dns')
+
+dns.setDefaultResultOrder('ipv4first')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  httpAgentOptions: {
+    keepAlive: false,
+  },
   output: 'standalone',
+  staticPageGenerationTimeout: 1000,
   reactStrictMode: true,
   logging: {
     fetches: {
