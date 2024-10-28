22d29fc773987fd37bebbd5d2071652e9c0daad9
8lane <hello@elseif.io>
2024-10-28 09:44:49 +0000

    chore: cleanup overrides now that the be supports them

--- src/app/components/cms/ChartInteractive/ChartInteractive.tsx
+++ src/app/components/cms/ChartInteractive/ChartInteractive.tsx
@@ -16,37 +16,14 @@ export default function ChartInteractive({ fallbackUntilLoaded, figure: { data, 
     freezeOnceVisible: true,
   })
 
-  delete layout.width
-
   return (
     <div ref={ref}>
       {!loaded ? fallbackUntilLoaded : null}
       {isIntersecting ? (
         <Plot
           onInitialized={() => setLoaded(true)}
-          data={data.map((plot) => ({ ...plot, hovertemplate: `%{y} <extra></extra>` }))}
-          layout={{
-            ...layout,
-            legend: {
-              ...layout.legend,
-              itemclick: false,
-              itemdoubleclick: false,
-            },
-            hoverlabel: {
-              bgcolor: '#0b0c0c',
-              bordercolor: '#0b0c0c',
-              font: {
-                color: 'white',
-                size: 16,
-                family: 'var(--font-primary), arial, sans-serif',
-              },
-            },
-            xaxis: {
-              ...layout.xaxis,
-              showspikes: false,
-            },
-            autosize: true,
-          }}
+          data={data}
+          layout={layout}
           style={{ width: '100%', height: '100%' }}
           config={{
             displayModeBar: false,
