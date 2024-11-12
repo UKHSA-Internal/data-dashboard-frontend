6bc18a9ad43d195e7ddaa4328fbc8ce35b32dadc
Rhys <rhys.howard@burendo.com>
2024-11-12 15:59:58 +0000

    Remove whats new e2e

--- e2e/tests/whats-new/whats-new.e2e.ts
+++ e2e/tests/whats-new/whats-new.e2e.ts
@@ -19,9 +19,6 @@ test.describe("What's new parent page", () => {
     await test.step('displays last updated date', async () => {
       await whatsNewParentPage.hasLastUpdated()
     })
-    await test.step('displays related links', async () => {
-      await app.hasRelatedLinks()
-    })
     await test.step('displays back to top', async () => {
       await app.hasBackToTop()
     })
