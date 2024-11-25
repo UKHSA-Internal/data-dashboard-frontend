ca33494d0e85aa3b9df46129d8ca0b129deb2ab4
Luke Towell <luke.towell@burendo.com>
2024-11-25 13:34:45 +0000

    CDD-2300: configure validation in handler - add it handler validation for validation of required fields. - configure the front end conditional formatting depending on errors - hardcoded the required response for errors

--- src/app/components/cms/Feedback/Feedback.tsx
+++ src/app/components/cms/Feedback/Feedback.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { Fragment, useEffect } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { useFormState } from 'react-dom'
 import { z } from 'zod'
 
@@ -11,7 +11,7 @@ import { handler } from '../utils/handler'
 
 const initialState = {
   message: '',
-  errors: {},
+  errors: [],
 }
 
 interface FeedbackProps {
@@ -28,8 +28,13 @@ interface FeedbackProps {
   }[]
 }
 
+interface FieldError {
+  clean_name: string
+  label: string
+}
+
 export default function Feedback({ formFields }: FeedbackProps) {
-  const [state, formAction] = useFormState(handler, initialState)
+  const [state, formAction] = useFormState(handler.bind(null, formFields), initialState)
 
   useEffect(() => {
     if (state.message) {
@@ -42,8 +47,9 @@ export default function Feedback({ formFields }: FeedbackProps) {
 
   return (
     <div className="govuk-grid-row">
+      {state.errors.length > 0 ? renderErrorSummary(state.errors) : null}
       <form className="govuk-grid-column-two-thirds" action={formAction}>
-        {formFields.map(renderFormFields)}
+        {formFields.map(renderFormFields.bind(null, state.errors))}
 
         <div className="govuk-button-group">
           <button className="govuk-button" type="submit">
@@ -58,39 +64,83 @@ export default function Feedback({ formFields }: FeedbackProps) {
   )
 }
 
-export const renderFormFields = ({
-  id,
-  clean_name: cleanName,
-  label,
-  field_type: fieldType,
-  help_text: helpText,
-  required,
-  choices,
-  // default_value: defaultValue,
-}: z.infer<typeof FormField>) => {
+export const renderErrorSummary = (errors: FieldError[]) => {
+  return (
+    <div className="govuk-error-summary" data-module="govuk-error-summary">
+      <div role="alert">
+        <h2 className="govuk-error-summary__title">
+          <span className="govuk-visually-hidden">Error:</span>The following form fields have errors:{' '}
+        </h2>
+        <div className="govuk-error-summary__body">
+          <ul className="govuk-list govuk-error-summary__list">
+            {errors.map((item) => {
+              return (
+                <li>
+                  <a href={'#' + item.clean_name}>{item.label}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const renderFormFields = (
+  errors: FieldError[],
+  {
+    id,
+    clean_name: cleanName,
+    label,
+    field_type: fieldType,
+    help_text: helpText,
+    required,
+    choices,
+    // default_value: defaultValue,
+  }: z.infer<typeof FormField>
+) => {
   const choicesList = choices.includes('\r\n') ? choices.split('\r\n') : choices.split(',')
 
   // TODO: Implement default values only for checkboxes
   // const defaultValuesList = defaultValue.includes('\r\n') ? defaultValue.split('\r\n') : defaultValue.split(',')
+
+  //does field have errors
+
+  const fieldHasError = errors.find(({ clean_name }) => (clean_name === cleanName ? true : false))
 
   return (
     <Fragment key={id}>
       {fieldType === 'singleline' && (
         <div className="govuk-form-group govuk-!-margin-bottom-9">
           <h2 className="govuk-label-wrapper">
-            <label className="govuk-label govuk-label--m govuk-error-message" htmlFor={cleanName}>
+            <label
+              className={'govuk-label govuk-label--m' + (fieldHasError ? 'govuk-error-message' : null)}
+              htmlFor={cleanName}
+            >
               {label}
             </label>
           </h2>
 
           {helpText.length > 0 ? <div className="govuk-hint">{helpText}</div> : null}
 
-          <textarea className="govuk-textarea" name={cleanName} id={cleanName} rows={1}></textarea>
+          {fieldHasError ? (
+            <p id="multiline-error" className="govuk-error-message">
+              <span className="govuk-visually-hidden">Error:</span> Please enter a value as this field is required
+            </p>
+          ) : null}
+
+          <textarea
+            className="govuk-textarea govuk-textarea--error"
+            name={cleanName}
+            id={cleanName}
+            rows={1}
+          ></textarea>
         </div>
       )}
 
       {fieldType === 'multiline' && (
-        <div className={'govuk-form-group' + (required ? '--error ' : null) + 'govuk-!-margin-bottom-9'}>
+        <div className={'govuk-form-group' + (fieldHasError ? '--error ' : null) + 'govuk-!-margin-bottom-9'}>
           <h2 className="govuk-label-wrapper">
             <label className="govuk-label govuk-label--m" htmlFor={cleanName}>
               {label}
@@ -99,34 +149,55 @@ export const renderFormFields = ({
 
           {helpText.length > 0 ? <div className="govuk-hint">{helpText}</div> : null}
 
-          {required ? (
+          {fieldHasError ? (
             <p id="multiline-error" className="govuk-error-message">
-              <span className="govuk-visually-hidden">Error:</span> {cleanName}
+              <span className="govuk-visually-hidden">Error:</span> Please enter a value as this field is required
             </p>
           ) : null}
 
-          <textarea className="govuk-textarea" name={cleanName} id={cleanName} rows={5} />
+          <textarea
+            className={'govuk-textarea ' + (fieldHasError ? 'govuk-textarea--error' : null)}
+            name={cleanName}
+            id={cleanName}
+            rows={5}
+          />
         </div>
       )}
 
       {fieldType === 'radio' && (
-        <fieldset className="govuk-fieldset govuk-!-margin-bottom-9">
-          <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
-            <h2 className="govuk-fieldset__heading">{label}</h2>
-          </legend>
-          <div className="govuk-radios" data-module="govuk-radios">
-            {choicesList.map((choice, key) => {
-              return (
-                <div key={key} className="govuk-radios__item">
-                  <input className="govuk-radios__input" id={cleanName} name={cleanName} type="radio" value={choice} />
-                  <label className="govuk-label govuk-radios__label" htmlFor={cleanName}>
-                    {choice}
-                  </label>
-                </div>
-              )
-            })}
-          </div>
-        </fieldset>
+        <div className={'govuk-form-group govuk-form-group' + (fieldHasError ? '--error ' : null)}>
+          <fieldset className="govuk-fieldset govuk-!-margin-bottom-9">
+            <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
+              <h2 className="govuk-fieldset__heading">{label}</h2>
+            </legend>
+
+            {helpText.length > 0 ? <div className="govuk-hint">{helpText}</div> : null}
+
+            {fieldHasError ? (
+              <p id="multiline-error" className="govuk-error-message">
+                <span className="govuk-visually-hidden">Error:</span> Please enter a value as this field is required
+              </p>
+            ) : null}
+            <div className="govuk-radios" data-module="govuk-radios">
+              {choicesList.map((choice, key) => {
+                return (
+                  <div key={key} className="govuk-radios__item">
+                    <input
+                      className="govuk-radios__input"
+                      id={cleanName}
+                      name={cleanName}
+                      type="radio"
+                      value={choice}
+                    />
+                    <label className="govuk-label govuk-radios__label" htmlFor={cleanName}>
+                      {choice}
+                    </label>
+                  </div>
+                )
+              })}
+            </div>
+          </fieldset>
+        </div>
       )}
     </Fragment>
   )
--- src/app/components/cms/utils/handler.ts
+++ src/app/components/cms/utils/handler.ts
@@ -8,12 +8,56 @@ import { feedbackSchema } from '@/schemas/feedback.schema'
 
 interface FormError {
   message: string
-  errors: Record<string, string[]>
+  errors: fieldError[]
 }
 
-export async function handler(prevState: FormError, formData: FormData) {
+interface FormFields {
+  id: number
+  meta: { type: 'forms.FormField' }
+  clean_name: string
+  label: string
+  field_type: string
+  help_text: string
+  required: boolean
+  choices: string
+  default_value: string
+}
+
+interface fieldError {
+  clean_name: string
+  label: string
+}
+
+export async function handler(formFields: FormFields[], prevState: FormError, formData: FormData) {
   try {
+    const requiredFields: fieldError[] = []
+    const errors: fieldError[] = []
+
     // Validate form request body
+    //for each form field identify fields that are required
+    formFields.map(({ clean_name: cleanName, label, required }) => {
+      required ? requiredFields.push({ clean_name: cleanName, label: label }) : null
+    })
+
+    // For each required field ensure that the value entered into the form is valid if it is not then add the field name to the errors array
+    requiredFields.map((requiredField) => {
+      const fieldValue = formData.get(requiredField.clean_name)
+
+      if (fieldValue) {
+        fieldValue.toString().length > 0 ? null : errors.push(requiredField)
+      } else {
+        errors.push(requiredField)
+      }
+    })
+
+    //if there are errors then return errors to the front end
+    if (errors) {
+      return {
+        message: 'Errors have been identified in the form',
+        errors: errors,
+      }
+    }
+
     const validatedFields = await feedbackSchema.safeParse(Object.fromEntries(formData))
 
     if (!validatedFields.success) {
@@ -21,30 +65,33 @@ export async function handler(prevState: FormError, formData: FormData) {
       // directly to the confirmation page to simulate a valid submission. This is to satisfy
       // business requirements of having the form completely optional but still submittable...
       logger.error('Feedback form validation failed, redirecting to confirmation anyway...')
+
       redirect('/feedback/confirmation')
     }
 
-    const isEmptySubmission = Array.from(formData.values()).every((value) => value === '')
+    // No errors - send results to the backend
+    if (!errors) {
+      logger.info(`Feedback submitted successfully, redirecting to confirmation`)
 
-    if (isEmptySubmission) {
-      logger.info(`Empty feedback form submitted, redirecting to confirmation and skipping api request`)
-      redirect('/feedback/confirmation')
-    }
+      //const { success } = await postSuggestions(validatedFields.data)
 
-    if (!isEmptySubmission) {
-      // Send results to the backend
-      const { success } = await postSuggestions(validatedFields.data)
+      const success = true
 
       if (!success) {
         return {
           message: 'Unknown error',
-          errors: {},
+          errors: errors,
         }
       }
+
+      //redirect('/feedback/confirmation')
     }
 
-    logger.info(`Feedback submitted successfully, redirecting to confirmation`)
-    redirect('/feedback/confirmation')
+    // errors - return errors
+    return {
+      message: '',
+      errors: errors,
+    }
   } catch (error) {
     throw error
   }
