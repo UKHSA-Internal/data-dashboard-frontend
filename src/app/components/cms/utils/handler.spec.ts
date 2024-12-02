39368fb7b9d6a8d1c1f1186cd3fb3c7f9e10db49
Luke Towell <luke.towell@burendo.com>
2024-12-02 14:51:11 +0000

    CDD-2300: added test for submitted and unsubmitted field validation

--- src/app/components/cms/utils/handler.spec.ts
+++ src/app/components/cms/utils/handler.spec.ts
@@ -58,6 +58,67 @@ test('Return errors to feedback page when required fields are not provided', asy
       choices: '',
       default_value: '',
     },
+    {
+      id: 2,
+      meta: { type: 'forms.FormField' },
+      clean_name: 'how_could_we_improve_your_experience_with_the_dashboard',
+      label: 'How could we improve your experience with the dashboard?',
+      field_type: 'multiline',
+      help_text: '',
+      required: true,
+      choices: '',
+      default_value: '',
+    },
+  ]
+
+  const formData = new FormData()
+  formData.set('how_could_we_improve_your_experience_with_the_dashboard', '')
+  formData.set('did_you_find_everything_you_were_looking_for', 'yes')
+  formData.set('what_was_your_reason_for_visiting_the_dashboard_today', '')
+  formData.set('what_would_you_like_to_see_on_the_dashboard_in_the_future', 'nothing')
+
+  const expectedError = [
+    {
+      clean_name: 'what_was_your_reason_for_visiting_the_dashboard_today',
+      label: 'What was your reason for visiting the dashboard today?',
+    },
+    {
+      clean_name: 'how_could_we_improve_your_experience_with_the_dashboard',
+      label: 'How could we improve your experience with the dashboard?',
+    },
+  ]
+
+  const res = await handler(formFields, { message: '', errors: [] }, formData)
+
+  expect(redirect).not.toHaveBeenNthCalledWith(1, '/feedback/confirmation')
+  expect(res).toEqual({ errors: expectedError, message: 'Errors have been identified in the form' })
+})
+
+test('Return errors to feedback page when required fields are not provided', async () => {
+  const formFields: FormFields[] = [
+    {
+      id: 1,
+      meta: { type: 'forms.FormField' },
+      clean_name: 'what_was_your_reason_for_visiting_the_dashboard_today',
+      label: 'What was your reason for visiting the dashboard today?',
+      field_type: 'multiline',
+      help_text:
+        'We will not be able to get in touch with you about your responses so please do not leave any personal details, such as your name or email address.',
+      required: true,
+      choices: '',
+      default_value: '',
+    },
+    {
+      id: 2,
+      meta: { type: 'forms.FormField' },
+      clean_name: 'how_could_we_improve_your_experience_with_the_dashboard',
+      label: 'How could we improve your experience with the dashboard?',
+      field_type: 'multiline',
+      help_text: '',
+      required: true,
+      choices: '',
+      default_value: '',
+    },
   ]
 
   const formData = new FormData()
@@ -79,8 +140,51 @@ test('Return errors to feedback page when required fields are not provided', asy
   expect(res).toEqual({ errors: expectedError, message: 'Errors have been identified in the form' })
 })
 
+test('Redirect to confirmation page when required fields are provided', async () => {
+  jest.mocked(client).mockResolvedValueOnce({
+    data: null,
+    status: 200,
+  })
+
+  const formFields: FormFields[] = [
+    {
+      id: 1,
+      meta: { type: 'forms.FormField' },
+      clean_name: 'what_was_your_reason_for_visiting_the_dashboard_today',
+      label: 'What was your reason for visiting the dashboard today?',
+      field_type: 'multiline',
+      help_text:
+        'We will not be able to get in touch with you about your responses so please do not leave any personal details, such as your name or email address.',
+      required: true,
+      choices: '',
+      default_value: '',
+    },
+    {
+      id: 2,
+      meta: { type: 'forms.FormField' },
+      clean_name: 'how_could_we_improve_your_experience_with_the_dashboard',
+      label: 'How could we improve your experience with the dashboard?',
+      field_type: 'multiline',
+      help_text: '',
+      required: true,
+      choices: '',
+      default_value: '',
+    },
+  ]
+
+  const formData = new FormData()
+  formData.set('how_could_we_improve_your_experience_with_the_dashboard', 'quality!')
+  formData.set('did_you_find_everything_you_were_looking_for', 'yes')
+  formData.set('what_was_your_reason_for_visiting_the_dashboard_today', 'Looking into Data')
+  formData.set('what_would_you_like_to_see_on_the_dashboard_in_the_future', 'nothing')
+
+  await handler(formFields, { message: '', errors: [] }, formData)
+
+  expect(logger.info).toHaveBeenNthCalledWith(1, 'Feedback submitted successfully, redirecting to confirmation')
+  expect(redirect).toHaveBeenNthCalledWith(1, '/feedback/confirmation')
+})
+
 test('Redirect to confirmation page when no suggestions are provided (form is non-mandatory)', async () => {
-  5
   const formFields: FormFields[] = [
     {
       id: 1,
