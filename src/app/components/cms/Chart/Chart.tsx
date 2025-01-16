7cf911654989e5e797b9373cfa6dc58137aa301e
Rhys <rhys.howard@burendo.com>
2025-01-16 16:45:44 +0000

    Reusing disctriminated union

--- src/app/components/cms/Chart/Chart.tsx
+++ src/app/components/cms/Chart/Chart.tsx
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-import { WithChartCard, WithChartHeadlineAndTrendCard, WithSimplifiedChartCardAndLink } from '@/api/models/cms/Page'
+import { ChartSchemas } from '@/api/models/cms/Page'
 import { getCharts } from '@/api/requests/charts/getCharts'
 import { getAreaSelector } from '@/app/hooks/getAreaSelector'
 import { getPathname } from '@/app/hooks/getPathname'
@@ -12,10 +12,7 @@ import { ChartEmpty } from '../ChartEmpty/ChartEmpty'
 
 interface ChartProps {
   /* Request metadata from the CMS required to fetch from the headlines api */
-  data:
-    | z.infer<typeof WithChartHeadlineAndTrendCard>['value']
-    | z.infer<typeof WithChartCard>['value']
-    | z.infer<typeof WithSimplifiedChartCardAndLink>['value']
+  data: z.infer<typeof ChartSchemas>['value']
 
   /* Size of chart based on whether the chart is displayed in a 1 or 2 column layout, or half/third layouts for landiing page  */
   size: 'narrow' | 'wide' | 'half' | 'third'
