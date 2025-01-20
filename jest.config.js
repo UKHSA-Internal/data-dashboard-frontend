9c1d8093abc2280ed5905f32845ee20d8f5c3757
Rhys <rhys.howard@burendo.com>
2025-01-20 18:22:10 +0000

    CDD-2405: Many tests fix, additional prefix locations

--- jest.config.js
+++ jest.config.js
@@ -41,7 +41,7 @@ const customJestConfig = {
 
       // Will update back up as part of CDD-2370
       statements: 91,
-      branches: 80,
+      branches: 79,
       lines: 92,
       functions: 87,
     },
