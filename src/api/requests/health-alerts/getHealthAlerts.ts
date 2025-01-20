f0af3d54592e34fd7b3dfa24435939b7d635cc5c
Luke Towell <luke.towell@burendo.com>
2025-01-20 13:37:49 +0000

    CDD-2262: Added tests for the useWeatherHealthAlertList functionality

--- src/api/requests/health-alerts/getHealthAlerts.ts
+++ src/api/requests/health-alerts/getHealthAlerts.ts
@@ -1,7 +1,11 @@
+import { SafeParseError, SafeParseSuccess } from 'zod'
+
 import { HealthAlertList, HealthAlertTypes } from '@/api/models/Alerts'
 import { client } from '@/api/utils/api.utils'
 import { isSSR } from '@/app/utils/app.utils'
 import { logger } from '@/lib/logger'
+
+export type GetHealthAlertsResponse = SafeParseError<HealthAlertList> | SafeParseSuccess<HealthAlertList>
 
 // This endpoint is called from our Map UI which is a client component therefore we must
 // proxy the request to the API through NextJs. For RSC's we can call the backend API directly
