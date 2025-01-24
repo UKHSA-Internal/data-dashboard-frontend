29bbe4592cba3bb5116546b5e741366ffe5a97b6
Temi Akinsoto <Temi.Akinsoto@ukhsa.gov.uk>
2025-01-24 15:58:44 +0000

    extract helper function into separate file

--- src/app/components/cms/utils/handler.ts
+++ src/app/components/cms/utils/handler.ts
@@ -6,6 +6,8 @@ import { postSuggestions } from '@/api/requests/suggestions/postSuggestions'
 import { logger } from '@/lib/logger'
 import { feedbackSchema } from '@/schemas/feedback.schema'
 
+import { updateMemorableDate } from './updatedate'
+
 interface FormError {
   message: string
   errors: FieldError[]
@@ -26,32 +28,6 @@ export interface FormFields {
 interface FieldError {
   clean_name: string
   label: string
-}
-
-// Helper function to concatenate day, month, and year into a single date field
-function updateMemorableDate(formData: FormData): FormData {
-  const day = formData.get('enter_a_memorable_date-day')
-  const month = formData.get('enter_a_memorable_date-month')
-  const year = formData.get('enter_a_memorable_date-year')
-
-  // If day, month, and year are all present, concatenate them into a single field
-  if (day && month && year) {
-    const memorableDate = `${day}-${month}-${year}`
-
-    // Create a new FormData object based on the existing formData
-    const updatedFormData = formData
-
-    // Set the concatenated memorable date and remove the individual day, month, and year fields
-    updatedFormData.set('enter_a_memorable_date', memorableDate)
-    updatedFormData.delete('enter_a_memorable_date-day')
-    updatedFormData.delete('enter_a_memorable_date-month')
-    updatedFormData.delete('enter_a_memorable_date-year')
-
-    return updatedFormData
-  }
-
-  // Return the formData as is if no date fields are provided
-  return formData
 }
 
 export async function handler(formFields: FormFields[], prevState: FormError, formData: FormData) {
