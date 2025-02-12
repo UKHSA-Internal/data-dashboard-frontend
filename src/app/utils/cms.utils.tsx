89b92932cdc83697ac247c7750723989795fe866
Luke Towell <luke.towell@burendo.com>
2025-02-12 09:02:24 +0000

    feature/CDD-2285: handle selected view by state management

--- src/app/utils/cms.utils.tsx
+++ src/app/utils/cms.utils.tsx
@@ -123,7 +123,7 @@ export const renderCard = (
                       <TabsList className="hidden no-js:block sm:block">
                         <TabsTrigger
                           asChild
-                          value="chart"
+                          value={`${kebabCase(column.value.title)}-chart`}
                           aria-controls={`chart-${kebabCase(column.value.title)}-content`}
                         >
                           <Link href={`#chart-${kebabCase(column.value.title)}`}>
@@ -132,7 +132,7 @@ export const renderCard = (
                         </TabsTrigger>
                         <TabsTrigger
                           asChild
-                          value="table"
+                          value={`${kebabCase(column.value.title)}-table`}
                           aria-controls={`table-${kebabCase(column.value.title)}-content`}
                         >
                           <Link href={`#table-${kebabCase(column.value.title)}`}>
@@ -144,7 +144,7 @@ export const renderCard = (
                         </TabsTrigger>
                         <TabsTrigger
                           asChild
-                          value="download"
+                          value={`${kebabCase(column.value.title)}-download`}
                           aria-controls={`download-${kebabCase(column.value.title)}-content`}
                         >
                           <Link href={`#download-${kebabCase(column.value.title)}`}>
@@ -153,7 +153,7 @@ export const renderCard = (
                         </TabsTrigger>
                         <TabsTrigger
                           asChild
-                          value="about"
+                          value={`${kebabCase(column.value.title)}-about`}
                           aria-controls={`about-${kebabCase(column.value.title)}-content`}
                         >
                           <Link href={`#about-${kebabCase(column.value.title)}`}>
@@ -167,7 +167,7 @@ export const renderCard = (
                         chartIdentifier={kebabCase(column.value.title)}
                       />
                       <TabsContent
-                        value="chart"
+                        value={`${kebabCase(column.value.title)}-chart`}
                         className="min-h-[var(--ukhsa-chart-card-tab-min-height)] no-js:mb-7"
                         data-type="chart"
                         id={`chart-${kebabCase(column.value.title)}-content`}
@@ -206,7 +206,7 @@ export const renderCard = (
                         </AreaSelectorLoader>
                       </TabsContent>
                       <TabsContent
-                        value="table"
+                        value={`${kebabCase(column.value.title)}-table`}
                         className="max-h-[var(--ukhsa-chart-card-table-scroll-height)] min-h-[var(--ukhsa-chart-card-tab-min-height)] overflow-y-auto no-js:mb-4"
                         id={`table-${kebabCase(column.value.title)}-content`}
                       >
@@ -219,7 +219,7 @@ export const renderCard = (
                         <Table data={column.value} size={size} />
                       </TabsContent>
                       <TabsContent
-                        value="download"
+                        value={`${kebabCase(column.value.title)}-download`}
                         className="min-h-[var(--ukhsa-chart-card-tab-min-height)]"
                         id={`download-${kebabCase(column.value.title)}-content`}
                       >
@@ -232,7 +232,7 @@ export const renderCard = (
                         <Download data={column.value} />
                       </TabsContent>
                       <TabsContent
-                        value="about"
+                        value={`${kebabCase(column.value.title)}-about`}
                         className="min-h-[var(--ukhsa-chart-card-tab-min-height)]"
                         id={`about-${kebabCase(column.value.title)}-content`}
                       >
