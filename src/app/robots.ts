cbe5eb041823d72ec86e67af039ac0aa771367e3
8lane <hello@elseif.io>
2025-02-17 09:48:44 +0000

    chore: logs

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  console.log('Sitemap url in robots.txt: ', `${process.env.BASE_URL}/sitemap.xml`)
   return {
     rules: {
       userAgent: '*',
