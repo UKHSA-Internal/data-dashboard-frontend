dd2a913d937458ad150314f770d68222e121ef87
Luke Towell <luke.towell@burendo.com>
2025-03-04 10:43:58 +0000

    CDD-2502: update renderSection to make use of Dynamic imports

--- src/app/components/cms/pages/Landing.tsx
+++ src/app/components/cms/pages/Landing.tsx
@@ -1,7 +1,23 @@
+import dynamic from 'next/dynamic'
+import { z } from 'zod'
+
+import { Body } from '@/api/models/cms/Page'
 import { View } from '@/app/components/ui/ukhsa'
 import { PageComponentBaseProps } from '@/app/types'
 import { getLandingPage } from '@/app/utils/cms'
-import { renderSection } from '@/app/utils/cms.utils'
+
+interface DynamicRenderSectionProps {
+  processedParams: string[]
+  sectionData: z.infer<typeof Body>[number]
+}
+
+const DynamicRenderSection = dynamic<DynamicRenderSectionProps>(() =>
+  import('@/app/utils/cms.utils').then((mod) => {
+    return ({ processedParams, sectionData }: DynamicRenderSectionProps) => {
+      return mod.renderSection(processedParams, sectionData)
+    }
+  })
+)
 
 export default async function LandingPage({ searchParams: { section } }: PageComponentBaseProps<{ section?: string }>) {
   let processedSectionParams: string[] = []
@@ -11,7 +27,13 @@ export default async function LandingPage({ searchParams: { section } }: PageCom
   }
   const { body } = await getLandingPage()
 
-  return <View>{body.map(renderSection.bind(null, processedSectionParams))}</View>
+  return (
+    <View>
+      {body.map((sectionData, index) => (
+        <DynamicRenderSection key={index} processedParams={processedSectionParams} sectionData={sectionData} />
+      ))}
+    </View>
+  )
 }
 
 const processedParams = (value: string | string[]) => {
