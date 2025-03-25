3ed6fed31e47dc19d962756dda322ec2c728f1b4
8lane <hello@elseif.io>
2025-03-25 17:30:02 +0000

    fix: access our data middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,8 @@
+import { notFound } from 'next/navigation'
 import { type NextMiddleware, type NextRequest, NextResponse } from 'next/server'
 
+import { getPages } from './api/requests/cms/getPages'
+import { getPageBySlug } from './api/requests/getPageBySlug'
 import { auth } from './auth'
 import { validateAndRenewSession } from './lib/auth/middleware'
 import { logger } from './lib/logger'
@@ -20,6 +23,34 @@ export const middleware: NextMiddleware = async (request: NextRequest) => {
   // Add x-url header for debugging or legacy usage
   response.headers.set('x-url', request.url)
 
+  // Access our data redirects
+  if (request.nextUrl.pathname === '/access-our-data') {
+    try {
+      const { id } = await getPageBySlug('access-our-data')
+
+      const pages = await getPages({ child_of: id.toString() })
+
+      if (pages.success) {
+        // Extract first child page as the page to redirect to
+        const {
+          data: {
+            items: [
+              {
+                meta: { slug },
+              },
+            ],
+          },
+        } = pages
+
+        return NextResponse.rewrite(new URL(`/access-our-data/${slug}`, request.url))
+      }
+      notFound()
+    } catch (error) {
+      notFound()
+    }
+  }
+
+  // Non-Public (Auth) redirects
   if (process.env.AUTH_ENABLED === 'true') {
     // Filter out next-auth API requests
     if (request.nextUrl.pathname.startsWith('/api/auth/')) {
