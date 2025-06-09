1fb5dce45ef68b3b0ae773eabdf6fcf12395b81c
Manu George <manu.george@ukhsa.gov.uk>
2025-06-09 22:48:52 +0100

    CDD-2607

--- src/app/(cms)/[[...slug]]/layout.tsx
+++ src/app/(cms)/[[...slug]]/layout.tsx
@@ -26,7 +26,8 @@ export function generateMetadata() {
 }
 
 export default async function Layout({ children, params }: LayoutProps) {
-  const [{ t }, globalBanners] : any = await Promise.all([getServerTranslation('common'), getGlobalBanner()])
+  const [{ t }] : any = await Promise.all([getServerTranslation('common')])
+  const globalBanners = await getGlobalBanner()
 
 
   const onLandingPage = !params?.slug
