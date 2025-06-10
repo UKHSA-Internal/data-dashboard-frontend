ccbd960fd4d77fa1de0304b2c31e3ccf0c18d558
Rhys <rhys.howard@burendo.com>
2025-06-10 09:17:08 +0100

    CDD-2468 - Signout page

--- src/app/(auth)/auth/signin/page.tsx
+++ src/app/(auth)/auth/signin/page.tsx
@@ -2,5 +2,4 @@ import { redirect } from 'next/navigation'
 
 export default function Page() {
   redirect('/start')
-  return null
 }
--- src/app/(auth)/auth/signout/page.tsx
+++ src/app/(auth)/auth/signout/page.tsx
@@ -1,5 +1,26 @@
-import React from 'react'
+import { getServerTranslation } from '@/app/i18n'
+import { signOut } from '@/auth'
+import { authSignOutRedirectionPath } from '@/config/constants'
 
 export default async function Page() {
-  return <div>Signout</div>
+  const { t } = await getServerTranslation('auth')
+  return (
+    <>
+      <h1 className="govuk-heading-xl govuk-!-margin-top-2 govuk-!-margin-bottom-4">
+        Sign out of the UKHSA data dashboard
+      </h1>
+      <p>Are you sure you want to sign out?</p>
+      <form
+        className="inline-flex items-center gap-3"
+        action={async () => {
+          'use server'
+          await signOut({ redirectTo: authSignOutRedirectionPath })
+        }}
+      >
+        <button className="govuk-button mb-0" type="submit">
+          {t('signOutButton')}
+        </button>
+      </form>
+    </>
+  )
 }
