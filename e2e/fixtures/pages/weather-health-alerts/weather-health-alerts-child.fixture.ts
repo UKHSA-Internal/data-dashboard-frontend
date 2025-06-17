502888806c946ed5aec6f02b0a2058b28fc2d7f9
Rhys <rhys.howard@burendo.com>
2025-06-17 11:26:18 +0100

    Removing console log

--- e2e/fixtures/pages/weather-health-alerts/weather-health-alerts-child.fixture.ts
+++ e2e/fixtures/pages/weather-health-alerts/weather-health-alerts-child.fixture.ts
@@ -36,7 +36,6 @@ export class WeatherHealthAlertsChildPage {
   }
 
   async hasMapLink(weather: HealthAlertTypes) {
-    console.log(`weather param ${weather} is currently hardcoded as heat`)
     await expect(this.page.getByRole('link', { name: 'View map of weather health alerts' })).toHaveAttribute(
       'href',
       `?v=map&type=${weather}`
