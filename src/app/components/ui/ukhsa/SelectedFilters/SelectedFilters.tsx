a147a1bd22816e231e5e8c5f3aadf606e407f5bc
Rhys <rhys.howard@burendo.com>
2025-07-01 21:48:22 +0100

    Moving to i18n

--- src/app/components/ui/ukhsa/SelectedFilters/SelectedFilters.tsx
+++ src/app/components/ui/ukhsa/SelectedFilters/SelectedFilters.tsx
@@ -1,13 +1,16 @@
 import { ReactNode } from 'react'
 
+import { useTranslation } from '@/app/i18n/client'
+
 interface SelectedFiltersProps {
   children?: ReactNode
 }
 
 export function SelectedFilters({ children }: SelectedFiltersProps) {
+  const { t } = useTranslation('common')
   return (
     <div className="flex flex-wrap">
-      <h2 className="govuk-heading-s govuk-!-margin-bottom-2 w-full">Selected Filters</h2> {/*TODO: i18n for text*/}
+      <h2 className="govuk-heading-s govuk-!-margin-bottom-2 w-full">{t('globalFilterTitle')}</h2>
       {children}
     </div>
   )
