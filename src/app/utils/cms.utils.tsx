559bef234622a7ec7c92e2d4c703aa0405809656
Luke Towell <luke.towell@burendo.com>
2025-07-17 17:17:44 +0100

    CDD-2646: Update the tab design and updated the card for the filter-linked-map

--- src/app/utils/cms.utils.tsx
+++ src/app/utils/cms.utils.tsx
@@ -36,6 +36,7 @@ import { AreaSelectorLoader } from '../components/cms/AreaSelector/AreaSelectorL
 import { ListItem } from '../components/ui/ukhsa/List/ListItem'
 import DropdownTab from '../components/ui/ukhsa/Tabs/DropdownTab'
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // TODO: Move this file into cms folder
 export const renderSection = (
   showMoreSections: string[],
@@ -267,138 +268,14 @@ export const renderCard = (
         </ChartRowCard>
       )}
 
-      {type === 'linked_map_card' && (
+      {type === 'filter_linked_map' && (
         <MapRowCard>
-          {value.columns.map((column) => {
-            const size = value.columns.length === 1 ? 'wide' : 'narrow'
-            const noAbout = !column.value.about || column.value.about.length === 0
-            const noRelatedLinks = !column.value.related_links || column.value.related_links.length === 0
-            return (
-              <div
-                key={column.id}
-                className={clsx('mb-3 sm:mb-6 lg:mb-0', {
-                  'lg:w-full': value.columns.length === 1,
-                  'lg:w-1/2': value.columns.length === 2,
-                })}
-                data-testid={`map-row-card-${kebabCase(column.value.title)}`}
-              >
-                <Card
-                  asChild
-                  aria-labelledby={`map-row-card-heading-${column.id}`}
-                  className="ukhsa-map-card flex flex-col gap-6"
-                >
-                  <article>
-                    <ChartRowCardHeader id={column.id} title={column.value.title} description={column.value.body}>
-                      <Timestamp data={column.value} size={size} />
-                    </ChartRowCardHeader>
-                    <Tabs defaultValue={`${kebabCase(column.value.title)}-map`} className="govuk-!-margin-bottom-0">
-                      <TabsList className="hidden no-js:block sm:block">
-                        <TabsTrigger
-                          asChild
-                          value={`${kebabCase(column.value.title)}-map`}
-                          aria-controls={`map-${kebabCase(column.value.title)}-content`}
-                        >
-                          <Link href={`#map-${kebabCase(column.value.title)}`}>
-                            <span>Map</span>
-                          </Link>
-                        </TabsTrigger>
-                        <TabsTrigger
-                          asChild
-                          value={`${kebabCase(column.value.title)}-table`}
-                          aria-controls={`table-${kebabCase(column.value.title)}-content`}
-                        >
-                          <Link href={`#table-${kebabCase(column.value.title)}`}>
-                            <span className="govuk-visually-hidden">Tabular data</span>
-                            <span aria-hidden>
-                              Tabular <span className="hidden lg:inline">data</span>
-                            </span>
-                          </Link>
-                        </TabsTrigger>
-                        <TabsTrigger
-                          asChild
-                          value={`${kebabCase(column.value.title)}-download`}
-                          aria-controls={`download-${kebabCase(column.value.title)}-content`}
-                        >
-                          <Link href={`#download-${kebabCase(column.value.title)}`}>
-                            <span>Download</span>
-                          </Link>
-                        </TabsTrigger>
-                        {noAbout && noRelatedLinks ? null : (
-                          <TabsTrigger
-                            asChild
-                            value={`${kebabCase(column.value.title)}-about`}
-                            aria-controls={`about-${kebabCase(column.value.title)}-content`}
-                          >
-                            <Link href={`#about-${kebabCase(column.value.title)}`}>
-                              <span>About</span>
-                            </Link>
-                          </TabsTrigger>
-                        )}
-                      </TabsList>
-                      <DropdownTab
-                        aria-label="Select for selecting card content"
-                        className="govuk-select relative mb-[-1px] block min-w-[7em] rounded-none border border-b-0 border-mid-grey py-0 pl-2 no-js:hidden sm:hidden"
-                        chartTitle={column.value.title}
-                        noAbout={noAbout}
-                      />
-                      <TabsContent
-                        value={`${kebabCase(column.value.title)}-map`}
-                        className="min-h-[var(--ukhsa-chart-card-tab-min-height)] no-js:mb-7"
-                        id={`map-${kebabCase(column.value.title)}-content`}
-                      >
-                        <span
-                          className="govuk-heading-m govuk-!-margin-top-3 js:hidden"
-                          id={`map-${kebabCase(column.value.title)}`}
-                        ></span>
-                        <MapTab />
-                      </TabsContent>
-                      <TabsContent
-                        value={`${kebabCase(column.value.title)}-table`}
-                        className="max-h-[var(--ukhsa-chart-card-table-scroll-height)] min-h-[var(--ukhsa-chart-card-tab-min-height)] overflow-y-auto no-js:mb-4"
-                        id={`table-${kebabCase(column.value.title)}-content`}
-                      >
-                        <span
-                          className="govuk-heading-m govuk-!-margin-top-3 js:hidden"
-                          id={`table-${kebabCase(column.value.title)}`}
-                        >
-                          Tabular data
-                        </span>
-                        <Table data={column.value} size={size} />
-                      </TabsContent>
-                      <TabsContent
-                        value={`${kebabCase(column.value.title)}-download`}
-                        className="min-h-[var(--ukhsa-chart-card-tab-min-height)]"
-                        id={`download-${kebabCase(column.value.title)}-content`}
-                      >
-                        <span
-                          className="govuk-heading-m govuk-!-margin-top-3 js:hidden"
-                          id={`download-${kebabCase(column.value.title)}`}
-                        >
-                          Download
-                        </span>
-                        <Download data={column.value} />
-                      </TabsContent>
-                      {noAbout && noRelatedLinks ? null : (
-                        <TabsContent
-                          value={`${kebabCase(column.value.title)}-about`}
-                          className="min-h-[var(--ukhsa-chart-card-tab-min-height)]"
-                          id={`about-${kebabCase(column.value.title)}-content`}
-                        >
-                          <span
-                            className="govuk-heading-m govuk-!-margin-top-3 js:hidden"
-                            id={`about-${kebabCase(column.value.title)}`}
-                          >
-                            About
-                          </span>
-                          <About description={column.value.about} relatedLinks={column.value.related_links} />
-                        </TabsContent>
-                      )}
-                    </Tabs>
-                  </article>
-                </Card>
-              </div>
-            )
-          })}
+          <div key={id} className={clsx('mb-3 sm:mb-6 lg:mb-0', 'lg:w-full')}>
+            <article className={'ukhsa-map-card'}>
+              <ChartRowCardHeader id={`map-row-heading-${id}`} title={value.title_prefix} />
+              <MapTab />
+            </article>
+          </div>
         </MapRowCard>
       )}
 
@@ -409,7 +286,7 @@ export const renderCard = (
             'lg:grid-cols-[1fr_1fr_1fr] md:grid-cols-[1fr_1fr]': value.cards.length > 2,
           })}
         >
-          {value.cards.map((card, index) => {
+          {value.cards.map((card: any, index: any) => {
             if (value.cards.length > 3 && index == 3 && !showMoreSections.includes(kebabCase(heading))) {
               return (
                 <div key={index}>
