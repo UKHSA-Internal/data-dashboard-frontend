d8b769759b81f048dbfefda26154cb5db7b76f24
Luke Towell <luke.towell@burendo.com>
2025-07-22 07:55:05 +0100

    CDD-2641: zod schema tidy up

--- src/api/requests/cover-maps/postMaps.ts
+++ src/api/requests/cover-maps/postMaps.ts
@@ -1,42 +1,11 @@
-import { z } from 'zod'
-
-import { MapDataResponse } from '@/api/models/Maps'
+import { MapDataRequest, MapDataResponse } from '@/api/models/Maps'
 import { client } from '@/api/utils/api.utils'
 import { isSSR } from '@/app/utils/app.utils'
 import { logger } from '@/lib/logger'
 
-export const MapRequestParams = z.object({
-  date_from: z.string(),
-  date_to: z.string(),
-  parameters: z.object({
-    theme: z.string(),
-    sub_theme: z.string(),
-    topic: z.string(),
-    metric: z.string(),
-    stratum: z.string(),
-    age: z.string(),
-    sex: z.string(),
-    geography_type: z.string(),
-    geographies: z.array(z.string()),
-  }),
-  accompanying_points: z.array(
-    z.object({
-      label_prefix: z.string(),
-      label_suffix: z.string(),
-      parameters: z.object({
-        metric: z.string(),
-        geography_type: z.string(),
-        geography: z.string(),
-      }),
-    })
-  ),
-})
-
-export type MapRequestParams = z.infer<typeof MapRequestParams>
-
 // This endpoint is called from our Map UI which is a client component therefore we must
 // proxy the request to the API through NextJs. For RSC's we can call the backend API directly
-export const postMapData = async (request: MapRequestParams) => {
+export const postMapData = async (request: MapDataRequest) => {
   try {
     const path = isSSR ? `maps/v1` : `proxy/maps/v1`
     const { data } = await client<MapDataResponse>(`${path}`, {
--- src/app/api/proxy/maps/v1/route.ts
+++ src/app/api/proxy/maps/v1/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-import { MapRequestParams, postMapData } from '@/api/requests/cover-maps/postMaps'
+import { MapDataRequest } from '@/api/models/Maps'
+import { postMapData } from '@/api/requests/cover-maps/postMaps'
 
 export async function POST(req: NextRequest) {
-  const body: MapRequestParams = await req.json()
+  const body: MapDataRequest = await req.json()
 
   if (!body) {
     return new NextResponse('Missing category', {
