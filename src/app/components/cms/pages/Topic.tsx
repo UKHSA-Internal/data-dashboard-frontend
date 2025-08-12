58c1575be166b16a73dd18d15c96a5554505e845
Rhys <rhys.howard@burendo.com>
2025-08-12 18:32:21 +0100

    Temp comment out topic filters

--- src/app/components/cms/pages/Topic.tsx
+++ src/app/components/cms/pages/Topic.tsx
@@ -5,14 +5,11 @@ import { AreaSelector } from '@/app/components/cms'
 import { Details } from '@/app/components/ui/govuk'
 import {
   Announcements,
-  FilterBanners,
   PageSection,
   PageSectionWithContents,
-  SelectedFilters,
   TopicBodyContextProvider,
   View,
 } from '@/app/components/ui/ukhsa'
-import FilterDropdowns from '@/app/components/ui/ukhsa/FilterDropdowns/FilterDropdowns'
 import { getServerTranslation } from '@/app/i18n'
 import { PageComponentBaseProps } from '@/app/types'
 import { getChartTimespan } from '@/app/utils/chart.utils'
@@ -22,7 +19,6 @@ import { clsx } from '@/lib/clsx'
 import { TimePeriodsHandler } from '../../ui/ukhsa/Context/TimePeriodsHandler'
 import RedirectHandler from '../../ui/ukhsa/RedirectHandler/RedirectHandler'
 import { RelatedLinksWrapper } from '../../ui/ukhsa/RelatedLinks/RelatedLinksWrapper'
-import StaticFilter from '../../ui/ukhsa/StaticFilter/StaticFilter'
 import { Description } from '../../ui/ukhsa/View/Description/Description'
 import { Heading } from '../../ui/ukhsa/View/Heading/Heading'
 import { LastUpdated } from '../../ui/ukhsa/View/LastUpdated/LastUpdated'
@@ -136,11 +132,11 @@ export default async function TopicPage({
 
             <TopicBodyContextProvider>
               <TimePeriodsHandler timePeriods={extractedTimePeriods} />
-              <FilterBanners />
-              <StaticFilter>
+              {/* <FilterBanners /> */}
+              {/* <StaticFilter>
                 <SelectedFilters />
                 <FilterDropdowns />
-              </StaticFilter>
+              </StaticFilter> */}
 
               <PageSectionWithContents>
                 {body.map(({ id, value }) => (
