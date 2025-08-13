a1897cce9979de6ca350f02c36d98843dcb35c8d
Luke Towell <luke.towell@burendo.com>
2025-08-13 17:27:18 +0100

    CDD-2718: rewrite state

--- src/app/hooks/globalFilterHooks.ts
+++ src/app/hooks/globalFilterHooks.ts
@@ -1,5 +1,9 @@
 import { DataFilters, GeographyFilters, ThresholdFilters, TimePeriod } from '@/api/models/cms/Page/GlobalFilter'
+<<<<<<< HEAD
 import { FilterOption, useGlobalFilters } from '@/app/context/globalFilterContext'
+=======
+import { useGlobalFilters } from '@/app/context/globalFilterContext'
+>>>>>>> 661a3569 (CDD-2718: rewrite state)
 
 export function useTimePeriods(): TimePeriod[] | null {
   const { state } = useGlobalFilters()
