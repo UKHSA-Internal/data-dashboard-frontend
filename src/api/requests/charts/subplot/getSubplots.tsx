fdd9afd051b09d0aec08da69d217f9748b4bf18d
Luke Towell <luke.towell@burendo.com>
2025-08-21 16:58:04 +0100

    CDD-2697: update the subplot route and proxy

--- src/api/requests/charts/subplot/getSubplots.tsx
+++ src/api/requests/charts/subplot/getSubplots.tsx
@@ -8,32 +8,46 @@ import { chartFormat } from '@/config/constants'
 import { logger } from '@/lib/logger'
 
 export const requestSchema = z.object({
-    file_format: z.string(),
-    hart_height: z.number(),
-    chart_width: z.number(),
-    x_axis_title: z.string().optional(),
-    y_axis_title: z.string().optional(),
-    y_axis_minimum_value: z.number().nullable().optional(),
-    y_axis_maximum_value: z.number().nullable().optional(),
-    target_threshold: z.number(),
-    target_threshold_label: z.string().nullable().optional(),
-    chart_parameters: z.object({
-      x_axis: z.string().nullable().optional(),
-      y_axis: z.string().nullable().optional(),
-      theme: z.string(),
-      sub_theme: z.string(),
-      date_from: z.string().nullable().optional(),
-      date_to: z.string().nullable().optional(),  
-      age: z.string().nullable().optional(),
-      sex: z.string().nullable().optional(),
-      topic: z.string().nullable().optional(),
-      metric: z.string().nullable().optional(),
-      stratum: z.string().nullable().optional(),
-    }),
-    subplots: z.array(
-      z.object({
-        subplot_title: z.string(),
-        subplot_parameters: z.object({
+  file_format: z.string(),
+  hart_height: z.number(),
+  chart_width: z.number(),
+  x_axis_title: z.string().optional(),
+  y_axis_title: z.string().optional(),
+  y_axis_minimum_value: z.number().nullable().optional(),
+  y_axis_maximum_value: z.number().nullable().optional(),
+  target_threshold: z.number(),
+  target_threshold_label: z.string().nullable().optional(),
+  chart_parameters: z.object({
+    x_axis: z.string().nullable().optional(),
+    y_axis: z.string().nullable().optional(),
+    theme: z.string(),
+    sub_theme: z.string(),
+    date_from: z.string().nullable().optional(),
+    date_to: z.string().nullable().optional(),
+    age: z.string().nullable().optional(),
+    sex: z.string().nullable().optional(),
+    topic: z.string().nullable().optional(),
+    metric: z.string().nullable().optional(),
+    stratum: z.string().nullable().optional(),
+  }),
+  subplots: z.array(
+    z.object({
+      subplot_title: z.string(),
+      subplot_parameters: z.object({
+        age: z.string().nullable().optional(),
+        sex: z.string().nullable().optional(),
+        topic: z.string().nullable().optional(),
+        metric: z.string().nullable().optional(),
+        stratum: z.string().nullable().optional(),
+        theme: z.string().nullable().optional(),
+        sub_theme: z.string().nullable().optional(),
+      }),
+      plots: z.array(
+        z.object({
+          label: z.string().nullable().optional(),
+          geography: z.string().nullable().optional(),
+          geography_type: z.string().nullable().optional(),
+          line_colour: z.string().nullable().optional(),
           age: z.string().nullable().optional(),
           sex: z.string().nullable().optional(),
           topic: z.string().nullable().optional(),
@@ -41,24 +55,10 @@ export const requestSchema = z.object({
           stratum: z.string().nullable().optional(),
           theme: z.string().nullable().optional(),
           sub_theme: z.string().nullable().optional(),
-        }),
-        plots: z.array(
-          z.object({
-            label: z.string().nullable().optional(),
-            geography: z.string().nullable().optional(),
-            geography_type: z.string().nullable().optional(),
-            line_colour: z.string().nullable().optional(),
-            age: z.string().nullable().optional(),
-            sex: z.string().nullable().optional(),
-            topic: z.string().nullable().optional(),
-            metric: z.string().nullable().optional(),
-            stratum: z.string().nullable().optional(),
-            theme: z.string().nullable().optional(),
-            sub_theme: z.string().nullable().optional(),
-          })
-        )
-      })
-    ),
+        })
+      ),
+    })
+  ),
 })
 
 export const responseSchema = z.object({
@@ -97,7 +97,7 @@ export const getSubplots = async (chart: RequestParams) => {
   }
 
   try {
-    const path = isSSR ? `charts/subplot/v1` : `proxy/charts/subplot/v3`
+    const path = isSSR ? `charts/subplot/v1` : `proxy/charts/subplot/v1`
     const { data } = await client<z.infer<typeof responseSchema>>(path, { body })
 
     return responseSchema.safeParse(data)
