3c09adf951258d19b182abb9a400e52603e43ec3
Manu George <manu.george@ukhsa.gov.uk>
2025-08-28 11:49:50 +0100

    CDD-2693
    CDD-2693

--- src/app/components/ui/ukhsa/FilterBanners/FilterBanners.tsx
+++ src/app/components/ui/ukhsa/FilterBanners/FilterBanners.tsx
@@ -2,13 +2,14 @@
 
 import React from 'react'
 
-import { useSelectedFilters } from '@/app/hooks/globalFilterHooks'
+import { useMapData, useSelectedFilters } from '@/app/hooks/globalFilterHooks'
 
 import { FilterBanner } from '../FilterBanner/FilterBanner'
 
 export default function FilterBanners() {
   const { selectedFilters, selectedGeographyFilters } = useSelectedFilters()
   let showGeographyLimitBanner: boolean = false
+  const { mapData } = useMapData()
 
   // Group filters
   const filterGroups = selectedFilters!.reduce(
@@ -26,20 +27,34 @@ export default function FilterBanners() {
   if (selectedGeographyFilters) {
     showGeographyLimitBanner = selectedGeographyFilters.length > 3
   }
-  // Check if any filter group has more than 3 items
+  // Conditions
   const showFilterBanner = Object.values(filterGroups).some((group) => group!.length > 3)
+  const showNoDataBanner = mapData === null && Object.values(filterGroups).some((group) => group!.length > 0)
+  const locationName = Object.values(filterGroups)
+    .flatMap((group) => group!.map((filter) => filter.label))
+    .join(', ')
 
+  const labels = Object.values(filterGroups).flatMap((group) => group!.map((filter) => filter.label))
+  const countryList = ['Northern Ireland', 'Scotland', 'Wales']
+  const unavailableCountries = countryList.filter((country) => labels.includes(country))
+
+  // Build message
+  let message = '&nbsp;&nbsp;<b>Important information:</b><ul class="list-disc">'
   if (showGeographyLimitBanner) {
-    return (
-      <FilterBanner
-        message="&nbsp;&nbsp;<b>Important information :</b> You can only select <b>four locations </b> to display at a time."
-        showIcon={true}
-      />
-    )
+    message += '<li>You can only select <b>four locations</b> to display at a time.</li>'
   }
+  if (showNoDataBanner) {
+    message += `<li>There is no data to display for <b>${locationName}</b>.</li>`
+  }
+  if (unavailableCountries.length > 0) {
+    message += `<li>Regional and Local authority level of coverage data is not available for <b>${unavailableCountries.join(', ')}</b>. All data displayed is at the country level.</li>`
+  }
+  message += '</ul>'
 
-  if (!showFilterBanner) {
+  // No banners? Return nothing
+  if (!showGeographyLimitBanner && !showFilterBanner && !showNoDataBanner && unavailableCountries.length === 0) {
     return null
   }
-  return null
+
+  return <FilterBanner message={message} showIcon={true} />
 }
