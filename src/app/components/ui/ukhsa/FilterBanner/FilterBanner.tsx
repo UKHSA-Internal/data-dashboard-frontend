1367c9ee8d6f09eab1e95475cc6bb3f0512181b0
Luke Towell <luke.towell@burendo.com>
2025-09-05 12:18:12 +0100

    CDD-2824: Region functionality & map updates
    - Update to the layering of maps so that the Region lines appear over the upper tier local authorities.
    
     - This makes the lines appear cleaner and clearly identifies which UTLAs belong to which region
    
    - Implemented zoom on selected region when a region is selected from the selected filter

--- src/app/components/ui/ukhsa/FilterBanner/FilterBanner.tsx
+++ src/app/components/ui/ukhsa/FilterBanner/FilterBanner.tsx
@@ -11,9 +11,9 @@ export function FilterBanner({ message, showIcon }: FilterBannerProps) {
           <path
             d="M18 24V18M18 12H18.015M33 18C33 26.2843 26.2843 33 18 33C9.71573 33 3 26.2843 3 18C3 9.71573 9.71573 3 18 3C26.2843 3 33 9.71573 33 18Z"
             stroke="white"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       )}
