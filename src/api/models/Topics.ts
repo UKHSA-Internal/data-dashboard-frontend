590e82dfb8392046b9b762251f9890e0653e7d78
Phillip Stanley <phill.stanley@ukhsa.gov.uk>
2025-09-08 22:30:55 +0100

    CDD-2827: Implement initial subplot tables mocks and API request

--- src/api/models/Topics.ts
+++ src/api/models/Topics.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const Topics = z
-  .enum(['COVID-19', 'Influenza', 'Adenovirus', 'RSV', 'Rhinovirus', 'Parainfluenza', 'hMPV'])
+  .enum(['COVID-19', 'Influenza', 'Adenovirus', 'RSV', 'Rhinovirus', 'Parainfluenza', 'hMPV', 'immunisation'])
   .or(z.string())
 
 export type Topics = z.infer<typeof Topics>
