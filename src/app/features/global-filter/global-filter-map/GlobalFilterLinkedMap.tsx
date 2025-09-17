a3c8fa2f01f07a1c3d0a134ad9e112493f794571
Luke Towell <luke.towell@burendo.com>
2025-09-17 15:59:03 +0100

    CDD-2817: introduced about tab functionality.

--- src/app/features/global-filter/global-filter-map/GlobalFilterLinkedMap.tsx
+++ src/app/features/global-filter/global-filter-map/GlobalFilterLinkedMap.tsx
@@ -4,7 +4,7 @@ import { z } from 'zod'
 import { CardTypes } from '@/api/models/cms/Page'
 import { ChartRowCard, ChartRowCardHeader } from '@/app/components/cms'
 
-import { MapCardWrapper } from '../components/MapCard/MapCardWrapper'
+import { MapCardTabWrapper } from '../components/MapCard/MapCardTabWrapper'
 
 export function GlobalFilterLinkedMap({ type, value, id }: z.infer<typeof CardTypes>) {
   if (type === 'filter_linked_map') {
@@ -13,7 +13,7 @@ export function GlobalFilterLinkedMap({ type, value, id }: z.infer<typeof CardTy
         <div key={id} className={clsx('mb-3 sm:mb-6 lg:mb-0', 'lg:w-full')}>
           <article className={'ukhsa-map-card'}>
             <ChartRowCardHeader id={`map-row-heading-${id}`} title={value.title_prefix ? value.title_prefix : ''} />
-            <MapCardWrapper />
+            <MapCardTabWrapper id={id} about={value.about ?? null} />
           </article>
         </div>
       </ChartRowCard>
