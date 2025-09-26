77d7f026a5ea16b1a3ee72542042a5e4891a37bc
Rhys <rhys.howard@burendo.com>
2025-09-26 08:26:32 +0100

    Subplot test fix

--- src/app/components/cms/FilterLinkedSubplot/FilterLinkedSubplot.spec.tsx
+++ src/app/components/cms/FilterLinkedSubplot/FilterLinkedSubplot.spec.tsx
@@ -2,15 +2,17 @@ import { render, screen } from '@/config/test-utils'
 
 import { FilterLinkedSubplot } from './FilterLinkedSubplot'
 
-// Mock the SubplotFilterCardContainer component
-jest.mock('@/app/components/ui/ukhsa/FilterLinkedCards/SubplotFilterCardContainer', () => ({
-  SubplotFilterCardContainer: () => <div>Mocked subplot filter card container</div>,
+// Mock the FilterLinkedCardWrapper component
+jest.mock('@/app/components/ui/ukhsa/FilterLinkedCards/FilterLinkedCardWrapper', () => ({
+  FilterLinkedCardWrapper: ({ cardType }: { cardType: string }) => (
+    <div>Mocked filter linked card wrapper with cardType: {cardType}</div>
+  ),
 }))
 
 describe('FilterLinkedSubplot', () => {
   test('renders subplot filter component', () => {
     render(<FilterLinkedSubplot />)
 
-    expect(screen.getByText('Mocked subplot filter card container')).toBeInTheDocument()
+    expect(screen.getByText('Mocked filter linked card wrapper with cardType: subplot')).toBeInTheDocument()
   })
 })
