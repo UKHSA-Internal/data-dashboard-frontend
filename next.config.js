8542a5dc53aa2f4deced3f9e33fc2b18cba0baa3
Luke Towell <luke.towell@burendo.com>
2025-10-02 11:29:39 +0100

    Updated node modules and cache-handler functionality

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const sharedCache = process.env.CACHING_V2_ENABLED === 'true'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   cacheHandler: sharedCache ? require.resolve('./cache-handler.mjs') : undefined,
-  cacheMaxMemorySize: 0, // Disable default in-memory caching
+  cacheMaxMemorySize: 10000000, // Disable default in-memory caching
   output: 'standalone',
   staticPageGenerationTimeout: 1000,
   reactStrictMode: true,
