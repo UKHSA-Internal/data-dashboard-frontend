cffba2314f29460c5c662d196a372aff55510a6e
Luke Towell <luke.towell@burendo.com>
2025-10-02 11:29:39 +0100

    Updated node modules and cache-handler functionality

--- src/app/components/ui/ukhsa/Map/health-alerts/HealthAlertsMapWrapper.tsx
+++ src/app/components/ui/ukhsa/Map/health-alerts/HealthAlertsMapWrapper.tsx
@@ -1,10 +1,9 @@
-'use client'
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 
 import featureCollection from '../shared/data/geojson/ukhsa-regions'
 
-const HealthAlertsMapDialog = dynamic(() => import('./HealthAlertsMapDialog'), { ssr: false })
+const HealthAlertsMapDialog = dynamic(() => import('./HealthAlertsMapDialog'))
 
 export async function HealthAlertsMapWrapper() {
   return (
