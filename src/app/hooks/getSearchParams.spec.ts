4002c40566d5ea842f89fc9ad26e514e1b2ba272
Luke Towell <luke.towell@burendo.com>
2025-10-29 10:30:18 +0000

    CDD-2811: tsc updates

--- src/app/hooks/getSearchParams.spec.ts
+++ src/app/hooks/getSearchParams.spec.ts
@@ -9,21 +9,21 @@ describe('getSearchParams', () => {
   const mockHeaders = jest.fn()
 
   beforeAll(() => {
-    ;(headers as jest.Mock).mockImplementation(() => ({
+    ;(headers as jest.Mock).mockResolvedValue({
       get: mockHeaders,
-    }))
+    })
   })
 
-  test('should correctly extract search parameters from x-url header', () => {
+  test('should correctly extract search parameters from x-url header', async () => {
     mockHeaders.mockReturnValue('http://example.com?param1=value1&param2=value2')
-    const searchParams = getSearchParams()
+    const searchParams = await getSearchParams()
     expect(searchParams.get('param1')).toBe('value1')
     expect(searchParams.get('param2')).toBe('value2')
   })
 
-  test('should handle the absence of x-url header', () => {
+  test('should handle the absence of x-url header', async () => {
     mockHeaders.mockReturnValue(null)
-    const searchParams = getSearchParams()
+    const searchParams = await getSearchParams()
     expect([...searchParams.entries()]).toEqual([])
   })
 
