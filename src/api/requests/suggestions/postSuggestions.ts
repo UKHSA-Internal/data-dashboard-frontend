dc68a7381fadd820186511f8ba1deb1b7835ab34
Luke Towell <luke.towell@burendo.com>
2025-12-01 11:09:53 +0000

    CDD-000: remove console.logs

--- src/api/requests/suggestions/postSuggestions.ts
+++ src/api/requests/suggestions/postSuggestions.ts
@@ -12,21 +12,18 @@ export type RequestParams = z.infer<typeof requestSchema>
 
 export const postSuggestions = async (suggestions: RequestParams) => {
   try {
-    console.log(`Post Suggestions - Submitting post suggestions: ${JSON.stringify(suggestions)}`)
-    logger.info(`Post Suggestions - Submitting post suggestions: ${JSON.stringify(suggestions)}`)
+    logger.info(`Post Suggestions - Submitting post suggestions length: ${suggestions.length}`)
     const { status } = await client<Response>('suggestions/v2', {
       body: suggestions,
       baseUrl: getFeedbackApiBaseUrl(),
     })
 
     if (status !== 200) {
-      console.log(`Post Suggestions Status Error: ${status}`)
       logger.info(`Post Suggestions Status Error: ${status}`)
       throw new Error('Failed to submit, invalid status code')
     }
     return { success: true }
   } catch (error) {
-    console.log(`Post Suggestions Error: ${error}`)
     logger.info(`Post Suggestions Error: ${error}`)
     logger.error(error)
     return { success: false }
