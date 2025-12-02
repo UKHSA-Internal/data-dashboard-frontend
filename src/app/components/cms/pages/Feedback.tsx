96bbb125935c8ad8c22f268c7e9077f8f7542f4f
Manu George <manu.george@ukhsa.gov.uk>
2025-12-02 11:54:05 +0000

    CDD-2965
    CDD-2965

--- src/app/components/cms/pages/Feedback.tsx
+++ src/app/components/cms/pages/Feedback.tsx
@@ -1,8 +1,8 @@
 import { getFeedbackPage } from '@/app/utils/cms'
 
 import { Announcements, View } from '../../ui/ukhsa'
+import Feedback from '../../ui/ukhsa/Feedback/Feedback'
 import { Heading } from '../../ui/ukhsa/View/Heading/Heading'
-import Feedback from '../Feedback/Feedback'
 
 export default async function FeedbackPage() {
   const { title, form_fields: formFields, active_announcements: activeAnnouncements } = await getFeedbackPage()
--- src/app/components/ui/ukhsa/Feedback/Feedback.tsx
+++ src/app/components/ui/ukhsa/Feedback/Feedback.tsx
@@ -6,7 +6,7 @@ import { z } from 'zod'
 
 import { FormField } from '@/api/models/cms/Page/FormFields'
 
-import { handler } from '../utils/handler'
+import { handler } from '../../../cms/utils/handler'
 import CheckboxField from './Fields/Checkbox/CheckboxField'
 import CheckboxesField from './Fields/Checkboxes/CheckboxesField'
 import DateField from './Fields/Date/DateField'
