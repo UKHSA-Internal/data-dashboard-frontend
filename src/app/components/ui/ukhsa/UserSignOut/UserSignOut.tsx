d470c0f7f0093654a0629baf13ce147d616df807
Luke Towell <luke.towell@burendo.com>
2026-01-14 09:50:45 +0000

    CDD-2469: Updated functionality for user logout of federated login from cognito

--- src/app/components/ui/ukhsa/UserSignOut/UserSignOut.tsx
+++ src/app/components/ui/ukhsa/UserSignOut/UserSignOut.tsx
@@ -1,10 +1,19 @@
-import { signOut } from '@/api/requests/auth/signOut'
+import { redirect } from 'next/navigation'
+
 import { getServerTranslation } from '@/app/i18n'
-import { auth } from '@/auth'
-import { authSignOutRedirectionPath } from '@/config/constants'
+import { auth, signOut } from '@/auth'
+import { logger } from '@/lib/logger'
 
 export default async function UserSignOut() {
-  const session = await auth()
+  let session
+  try {
+    session = await auth()
+  } catch (error) {
+    logger.error('Auth error:', error)
+    return <></>
+  }
+
+  const getAuthApiBaseUrl = `${process.env.AUTH_DOMAIN ?? ''}`
 
   if (!session) return <></>
 
@@ -15,7 +24,16 @@ export default async function UserSignOut() {
       className="inline-flex items-center gap-3"
       action={async () => {
         'use server'
-        await signOut({ redirectTo: authSignOutRedirectionPath })
+        try {
+          await signOut({ redirect: false })
+          const cognitoLogoutUrl = new URL(`${getAuthApiBaseUrl}/logout`)
+          cognitoLogoutUrl.searchParams.set('client_id', process.env.AUTH_CLIENT_ID!)
+          cognitoLogoutUrl.searchParams.set('logout_uri', process.env.NEXTAUTH_URL)
+          redirect(cognitoLogoutUrl.toString())
+        } catch (error) {
+          logger.error(error)
+          redirect(process.env.NEXTAUTH_URL)
+        }
       }}
     >
       <button className="govuk-button mb-0" type="submit">
