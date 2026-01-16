39062b52b3bab202281978ed41fd3340e5ff4162
Taiwo Kareem <taiwo.kareem36@gmail.com>
2026-01-16 09:56:37 +0000

    FE: downloads api request update (confidence intervals)

--- src/api/requests/downloads/getDownloads.ts
+++ src/api/requests/downloads/getDownloads.ts
@@ -7,6 +7,7 @@ import { logger } from '@/lib/logger'
 export const requestSchema = z.object({
   file_format: z.enum(['json', 'csv']),
   x_axis: z.enum(['age', 'geography', 'sex', 'stratum', 'date', 'metric']).optional().nullable(),
+  confidence_intervals: z.boolean().default(false),
   plots: z.array(
     z.object({
       topic: Topics,
@@ -27,13 +28,15 @@ export type RequestParams = z.infer<typeof requestSchema>
 export const getDownloads = async (
   plots: RequestParams['plots'],
   format: RequestParams['file_format'] = 'csv',
-  x_axis: RequestParams['x_axis'] = null
+  x_axis: RequestParams['x_axis'] = null,
+  confidence_intervals: RequestParams['confidence_intervals'] = false
 ) => {
   try {
     const body: RequestParams = {
       plots,
       x_axis,
       file_format: format,
+      confidence_intervals,
     }
     const { data } = await client<string>(`downloads/v2`, { body })
 
