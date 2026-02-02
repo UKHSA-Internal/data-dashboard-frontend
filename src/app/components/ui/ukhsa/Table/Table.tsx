26941507e9c41a3fbf17b4f317e25f480e24d0d7
Aidan Skinner <aidan@skinner.me.uk>
2026-02-02 15:08:59 +0000

    Table: remove counter variable and use the available index

--- src/app/components/ui/ukhsa/Table/Table.tsx
+++ src/app/components/ui/ukhsa/Table/Table.tsx
@@ -141,8 +141,6 @@ export async function Table({
 
     const timestamp = chartResponse.success && chartResponse.data.last_updated
 
-    let incrementingColumnId = 0
-
     const hasReportingDelayPeriod = groups.some(({ data }) => data.some((item) => item.inReportingDelay))
 
     return (
@@ -181,7 +179,6 @@ export async function Table({
                       labelIndex = groupIndex * previousColLength + columnIndex - 1
                     }
 
-                    incrementingColumnId += 1
                     const chartLabel = columnIndex === 0 ? '' : (chart[labelIndex]?.value?.label ?? '')
                     const axisTitle = columnIndex === 0 ? (x_axis_title ?? '') : (y_axis_title ?? '')
                     const columnHeader = t('cms.blocks.table.header', {
@@ -192,7 +189,7 @@ export async function Table({
 
                     return (
                       <th
-                        id={`${kebabCase(title)}-col-${incrementingColumnId}`}
+                        id={`${kebabCase(title)}-col-${columnIndex}`}
                         key={columnIndex}
                         headers="blank"
                         className="govuk-table__header js:bg-white"
@@ -211,7 +208,7 @@ export async function Table({
                       aria-label={item.inReportingDelay ? t('reportingLagPeriodKey') : undefined}
                     >
                       {columns.map((column, columnIndex) => {
-                        const incrementingColumnId = columns.length * groupIndex + (columnIndex + 1)
+                        const columnId = columns.length * groupIndex + (columnIndex + 1)
                         const previousItemHasDelay = data[key - 1]?.inReportingDelay ?? false
                         const nextItemHasDelay = data[key + 1]?.inReportingDelay ?? false
 
@@ -236,7 +233,7 @@ export async function Table({
                               </th>
                             ) : (
                               <td
-                                headers={`${kebabCase(title)}-col-${incrementingColumnId}`}
+                                headers={`${kebabCase(title)}-col-${columnId}`}
                                 className={clsx('govuk-table__cell', {
                                   'bg-delay-blue-opaque': item.inReportingDelay,
                                   'border-t-2 border-t-delay-blue': item.inReportingDelay && !previousItemHasDelay,
