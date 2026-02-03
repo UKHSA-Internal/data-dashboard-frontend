42d8c930f3f4543795ce34c35ebceefec3340ada
Manu George <manu.george@ukhsa.gov.uk>
2026-02-03 11:22:04 +0000

    UI issue fix
    UI issue fix

--- src/app/components/ui/ukhsa/ClassificationBanner/ClassificationBanner.tsx
+++ src/app/components/ui/ukhsa/ClassificationBanner/ClassificationBanner.tsx
@@ -10,11 +10,11 @@ const ClassificationBanner: FC<ClassificationBannerProps> = ({ size }) => {
     <div className="bg-[#1D70B8]">
       <p
         className={clsx('govuk-width-container font-sans font-bold uppercase text-white', {
-          'text-[27px] mt-[4px]': size === 'large',
+          'text-[27px] mt-[4px] py-2': size === 'large',
           'text-[18px]': size === 'medium',
         })}
       >
-        Official Sensitive
+        Official-Sensitive
       </p>
     </div>
   )
