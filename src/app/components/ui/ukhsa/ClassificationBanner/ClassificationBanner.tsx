e613cc8b688bde35be6fc9f012368c4b536fd4dd
Kathryn Dale <kathryn.dale@burendo.com>
2026-03-05 09:13:37 +0000

    Update formatting

--- src/app/components/ui/ukhsa/ClassificationBanner/ClassificationBanner.tsx
+++ src/app/components/ui/ukhsa/ClassificationBanner/ClassificationBanner.tsx
@@ -36,13 +36,13 @@ const ClassificationBanner: FC<ClassificationBannerProps> = ({
 }) => {
   return (
     <div
-      className={clsx('govuk-classification-banner', { [customColor || levelColors[level]]: true })}
+      className={clsx('govuk-classification-banner', { [customColor || levelColors[level]]: true, 'my-3':size==='large' })}
       role="note"
       aria-label={`${levelContent[level]} classification`}
     >
       <p
         className={clsx('font-sans font-bold uppercase text-white', {
-          'govuk-width-container text-[27px] py-2': size === 'large',
+          'govuk-width-container text-[27px] py-2 !pl-[12px]': size === 'large',
           'py-1 !pl-[12px] text-[18px]': size === 'medium',
         })}
       >
