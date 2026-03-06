c77697f8c7ace6edf3e75f9f20171675da4b42ff
Rhys <rhys.howard@burendo.com>
2026-03-06 10:54:02 +0000

    Updating source links with comments from stakeholders

--- src/app/components/cms/ChartWithDescriptionCard/ChartWithDescriptionCard.tsx
+++ src/app/components/cms/ChartWithDescriptionCard/ChartWithDescriptionCard.tsx
@@ -45,12 +45,17 @@ export function ChartWithDescriptionCard({ value, cardsCount }: ChartWithDescrip
         {value.description && (
           <p className="govuk-body-s mb-0 mt-3 text-grey-1" data-testid="chart-description">
             {value.description}
-            <Link
-              className="govuk-link govuk-link--no-visited-state ml-1"
-              href={value.source?.page ?? value.source?.external_url ?? ''}
-              prefetch
-            >
-              {value.source?.link_display_text}
+            <Link className="govuk-link govuk-link--no-visited-state ml-1" href={topicPagePath} prefetch>
+              Visit {value.title} to find out more
+            </Link>
+          </p>
+        )}
+
+        {value.source && value.source.external_url && (
+          <p className="govuk-body-s mb-0 mt-3 text-grey-1" data-testid="chart-source">
+            Source:
+            <Link href={value.source.external_url} prefetch>
+              {value.source.link_display_text}
             </Link>
           </p>
         )}
