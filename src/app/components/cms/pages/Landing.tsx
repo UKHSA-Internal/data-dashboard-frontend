e2e53deef033bac7a9eb064c6af2454a08b6017f
Taiwo Kareem <taiwo.kareem36@gmail.com>
2026-03-06 14:09:19 +0000

    FE: Integrate heading section with landing page
    # Conflicts:
    #	src/app/components/cms/pages/Landing.tsx

--- src/app/components/cms/pages/Landing.tsx
+++ src/app/components/cms/pages/Landing.tsx
@@ -8,10 +8,52 @@ export default async function LandingPage({ searchParams: { section } }: PageCom
   const processedSectionParams = processSectionParams(section)
   const { body, active_announcements: activeAnnouncements } = await getLandingPage()
 
+  const {
+    title,
+    body,
+    page_description: pageDescription,
+    related_links: relatedLinks,
+    related_links_layout: relatedLinksLayout,
+    last_published_at: lastPublishedAt,
+    active_announcements: activeAnnouncements,
+  } = await getLandingPage()
+
+  const hasRelatedLinks = Boolean(relatedLinks?.length)
+  const showSidebarRelatedLinks = hasRelatedLinks && relatedLinksLayout === 'Sidebar'
+  const showFooterRelatedLinks = hasRelatedLinks && relatedLinksLayout === 'Footer'
+
   return (
     <View>
-      <Announcements announcements={activeAnnouncements} />
+      <div className="govuk-grid-row">
+        <div
+          className={
+            showSidebarRelatedLinks ? 'govuk-grid-column-three-quarters-from-desktop' : 'govuk-grid-column-full'
+          }
+        >
+          <Heading heading={title} lastUpdated={lastPublishedAt} />
+          <LastUpdated lastUpdated={lastPublishedAt} />
+          <Announcements announcements={activeAnnouncements} />
+          {pageDescription ? <Description description={pageDescription} /> : null}
+        </div>
+
+        {showSidebarRelatedLinks ? (
+          <div className="govuk-grid-column-one-quarter-from-desktop sticky top-2">
+            <RelatedLinksWrapper layout={relatedLinksLayout} links={relatedLinks ?? []} />
+          </div>
+        ) : null}
+      </div>
+
+      <Contents>
+        {body.map(({ id, value }) => (
+          <ContentsLink key={id} href={`#${kebabCase(value.heading)}`}>
+            {value.heading}
+          </ContentsLink>
+        ))}
+      </Contents>
+
       {body.map(renderSection.bind(null, processedSectionParams))}
+
+      {showFooterRelatedLinks ? <RelatedLinksWrapper layout={relatedLinksLayout} links={relatedLinks ?? []} /> : null}
     </View>
   )
 }
