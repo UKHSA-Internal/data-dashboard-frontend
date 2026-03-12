741d3a3b3f9718d3b31ec078fa5adcc31a19c453
Luke Towell <luke.towell@burendo.com>
2026-03-12 14:41:22 +0000

    Updated the error page designs

--- src/app/components/cms/pages/AuthError.tsx
+++ src/app/components/cms/pages/AuthError.tsx
@@ -1,31 +1,53 @@
+import Link from 'next/link'
+
 import { PageType } from '@/api/requests/cms/getPages'
 import { getPageBySlug } from '@/api/requests/getPageBySlug'
 import { Announcements, View } from '@/app/components/ui/ukhsa'
 import { RelatedLinksWrapper } from '@/app/components/ui/ukhsa/RelatedLinks/RelatedLinksWrapper'
 import { Heading } from '@/app/components/ui/ukhsa/View/Heading/Heading'
-import { LastUpdated } from '@/app/components/ui/ukhsa/View/LastUpdated/LastUpdated'
+import { getServerTranslation } from '@/app/i18n'
 import { PageComponentBaseProps } from '@/app/types'
 
-import { RichTextAutoHeadings } from '../../ui/ukhsa/RichTextAutoHeadings/RichTextAutoHeadings'
+import { RichText } from '../RichText/RichText'
 
 export default async function AuthErrorPage({ slug }: PageComponentBaseProps) {
   const {
     title,
-    body,
-    last_updated_at: lastUpdated,
+    error_line: errorLine,
+    error_text: errorText,
+    sub_text: errorSubText,
     related_links: relatedLinks,
     related_links_layout: relatedLinksLayout,
     active_announcements: activeAnnouncements,
   } = await getPageBySlug<PageType.AuthError>(slug)
 
+  const { t } = await getServerTranslation('auth')
+
   return (
     <View>
-      <Heading heading={title} />
-      <LastUpdated lastUpdated={lastUpdated} />
       <Announcements announcements={activeAnnouncements} />
       <div className="govuk-grid-row">
-        <div className="govuk-grid-column-three-quarters-from-desktop">
-          <RichTextAutoHeadings>{body}</RichTextAutoHeadings>
+        <div className="govuk-grid-column-three-quarters-from-desktop pt-5">
+          <div className="border-l-[9px] border-l-red pl-9">
+            <Heading heading={title} />
+            <p className="pb-3 font-bold text-red">{errorLine}</p>
+            <RichText>{errorText}</RichText>
+          </div>
+          <RichText className="mt-6">{errorSubText}</RichText>
+          <Link href="/start">
+            <div className="flex items-center gap-1">
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12.5 15L7.5 10L12.5 5"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <p className="underline">{t('backButtonText')}</p>
+            </div>
+          </Link>
         </div>
 
         {relatedLinksLayout === 'Sidebar' ? (
