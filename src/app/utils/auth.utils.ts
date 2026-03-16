b6a046dc8e703f65c37ea0385a962f0f30afce70
Luke Towell <luke.towell@burendo.com>
2026-03-16 15:41:30 +0000

    CDD-3085: add optional path param to auth utils

--- src/app/utils/auth.utils.ts
+++ src/app/utils/auth.utils.ts
@@ -1,9 +1,11 @@
-export const getCognitoSignoutURL: () => string = () => {
+export const getCognitoSignoutURL = (path?: string): string => {
   const getAuthApiBaseUrl = `${process.env.AUTH_DOMAIN ?? ''}`
+
+  const redirectURL = path ? `${process.env.NEXTAUTH_URL + path}` : process.env.NEXTAUTH_URL
 
   const cognitoLogoutUrl = new URL(`${getAuthApiBaseUrl}/logout`)
   cognitoLogoutUrl.searchParams.set('client_id', process.env.AUTH_CLIENT_ID!)
-  cognitoLogoutUrl.searchParams.set('logout_uri', process.env.NEXTAUTH_URL)
+  cognitoLogoutUrl.searchParams.set('logout_uri', redirectURL)
 
   return cognitoLogoutUrl.toString()
 }
