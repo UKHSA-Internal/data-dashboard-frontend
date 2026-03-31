a81549fcb2c62c6571817b960ea366027569f185
Aidan Skinner <aidan@skinner.me.uk>
2026-03-31 10:41:46 +0100

    remove MegaMenu in favour of a much simpler Menu

---
+++ src/mock-server/handlers/menus/v2/fixtures/menu.ts
@@ -0,0 +1,22 @@
+export const menu = {
+  active_menu: [
+    {
+      type: 'link',
+      value: {
+        title: "What's coming",
+        page: 3,
+        html_url: 'https://my-prefix.dev.ukhsa-dashboard.data.gov.uk/whats-coming/',
+      },
+      id: 'd8e270c7-f3d7-41cf-8d7c-c2bbe62ed71d',
+    },
+    {
+      type: 'link',
+      value: {
+        title: "What's new",
+        page: 14,
+        html_url: 'https://my-prefix.dev.ukhsa-dashboard.data.gov.uk/whats-new/',
+      },
+      id: '021352b9-d606-48ee-b942-1739ccec9e03',
+    },
+  ],
+}
