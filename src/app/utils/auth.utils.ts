e2cd412a009c107318d71aea1693cff2a637ee8a
Dan Dammann <Dan.Dammann@ukhsa.gov.uk>
2026-04-21 00:49:45 +0100

    CDD-3090: add getIsNonPublic()

--- src/app/utils/auth.utils.ts
+++ src/app/utils/auth.utils.ts
@@ -1,3 +1,12 @@
+import { authEnabled } from '@/config/constants'
+
+/**
+ * Synthesize the isNonPublic flag from isPublic (CMS page property)
+ * and the global authEnabled constant (whether this is the public or
+ * non-public frontend application.
+ */
+export const getIsNonPublic = (isPublic: boolean): boolean => authEnabled === true && isPublic === false
+
 export const getCognitoSignoutURL = (path?: string): string => {
   const getAuthApiBaseUrl = `${process.env.AUTH_DOMAIN ?? ''}`
 
