7f192f53ac3b64719798100bff4d998e79df6b06
Manu George <manu.george@ukhsa.gov.uk>
2026-05-18 11:07:54 +0100

    Update auth-setup.fixture.ts
    for passing sonarqube quality gate

--- e2e/fixtures/auth/auth-setup.fixture.ts
+++ e2e/fixtures/auth/auth-setup.fixture.ts
@@ -34,6 +34,7 @@ export const AuthSetupFixtures = base.extend<AuthSetupFixtures>({
     async ({ page, authEnabled, startLoggedOut }, use) => {
       if (!authEnabled || startLoggedOut) {
         await page.context().clearCookies()
+
         return await use()
       }
 
