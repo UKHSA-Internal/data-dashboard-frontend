c8099b46d1031f63e493c86655ae5464f8052b37
Kathryn Dale <kathryn.dale@burendo.com>
2026-05-22 13:27:24 +0100

    fix linting

--- src/app/components/ui/ukhsa/FilterLinkedCards/TimeseriesFilterCard.tsx
+++ src/app/components/ui/ukhsa/FilterLinkedCards/TimeseriesFilterCard.tsx
@@ -13,11 +13,11 @@ import { ClientTable } from '@/app/components/ui/ukhsa/Table/ClientTable'
 import DropdownTab from '@/app/components/ui/ukhsa/Tabs/DropdownTab'
 import { formatDate } from '@/app/utils/date.utils'
 import { FlattenedGeography, getParentGeography } from '@/app/utils/geography.utils'
+import { getDataClassification } from '@/app/utils/table.utils'
 import { getMinMaxYears, MinMaxYear } from '@/app/utils/time-period.utils'
 
 import { Card } from '../Card/Card'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../Tabs/Tabs'
-import { getDataClassification } from '@/app/utils/table.utils'
 
 interface TimeseriesFilterCardProps {
   geography: GeographiesSchemaObject
