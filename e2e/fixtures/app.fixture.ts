0ac2907c9deb809b83e9e0ac246aef0ef2d5092e
Manu George <manu.george@ukhsa.gov.uk>
2026-06-02 12:56:18 +0100

    Update app.fixture.ts

--- e2e/fixtures/app.fixture.ts
+++ e2e/fixtures/app.fixture.ts
@@ -400,14 +400,15 @@ export class App {
   async canDownloadChart(cards: string[], format: 'csv' | 'json', device: 'desktop' | 'mobile' | 'tablet') {
     for (const name of cards) {
       const card = this.page.getByTestId(`chart-row-card-${name}`)
-
+      await card
+        .locator('.chartLoader')
+        .waitFor({ state: 'detached' })
+        .catch(() => {})
       if (device === 'mobile') {
-        await card.locator('.chartLoader').waitFor({ state: 'detached' })
         await card
           .getByRole('combobox', { name: `Choose display option for '${lowerCase(name)}' data` })
           .selectOption('Download')
       } else {
-        await card.locator('.chartLoader').waitFor({ state: 'detached' })
         await card.getByRole('tab', { name: 'Download' }).click()
       }
 
