85d8b707e0d4762e3801117943542d7c19864353
Taiwo Kareem <taiwo.kareem36@gmail.com>
2026-06-17 12:33:18 +0100

    CDD-3273 - Update headline design lost due to merge conflict

--- src/app/components/cms/Headline/Headline.tsx
+++ src/app/components/cms/Headline/Headline.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { z } from 'zod'
 
 import { HeadlineNumber } from '@/api/models/cms/Page/Blocks'
@@ -6,11 +7,18 @@ import { getServerTranslation } from '@/app/i18n'
 
 interface HeadlineProps {
   /* Request metadata from the CMS required to fetch from the headlines api */
-  data: z.infer<typeof HeadlineNumber>['value']
-  datePrefix: string
+  readonly data: z.infer<typeof HeadlineNumber>['value']
+  readonly datePrefix: string
+  readonly headingClassName?: string
+  readonly valueClassName?: string
 }
 
-export async function Headline({ data: { body: heading, ...requestParams }, datePrefix }: HeadlineProps) {
+export async function Headline({
+  data: { body: heading, ...requestParams },
+  datePrefix,
+  headingClassName,
+  valueClassName,
+}: HeadlineProps) {
   const { t } = await getServerTranslation('common')
 
   const headline = await getHeadlines(requestParams)
@@ -21,13 +29,15 @@ export async function Headline({ data: { body: heading, ...requestParams }, date
     } = headline
 
     return (
-      <div>
-        <div>{t('cms.blocks.headline.heading', { heading })}</div>
+      <>
+        <div className={headingClassName}>{t('cms.blocks.headline.heading', { heading })}</div>
         <div className="govuk-body-xs govuk-!-margin-bottom-1 text-dark-grey [.ukhsa-chart-card_&]:hidden">
           {t('cms.blocks.timestamp.value', { prefix: datePrefix, value: date })}
         </div>
-        <div className="govuk-body-l govuk-!-margin-bottom-0">{t('cms.blocks.headline.value', { value })}</div>
-      </div>
+        <div className={clsx(valueClassName ?? 'govuk-body-l', 'govuk-!-margin-bottom-0')}>
+          {t('cms.blocks.headline.value', { value })}
+        </div>
+      </>
     )
   }
 
