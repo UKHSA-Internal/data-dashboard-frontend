1a0efc684935e53d31bdea21164c094db25af217
Daniel Welsh <daniel.welsh@ukhsa.gov.uk>
2026-06-22 10:37:44 +0100

    Removing irrelevant non-public tests

--- e2e/tests/non-public/non-public.e2e.ts
+++ e2e/tests/non-public/non-public.e2e.ts
@@ -2,50 +2,53 @@ import { viewports } from 'e2e/constants/viewports.constants'
 
 import { test } from '../../fixtures/app.fixture.non.public'
 
-test.describe('Non-public tests - desktop @non-public', () => {
-  test.use({ viewport: viewports.desktop })
+// All these tests are navigation to the start page from the nav menu
+// Start page should not appear on menu so commented out
+// File kept incase we need to introduce any other non-public tests
+// test.describe('Non-public tests - desktop @non-public', () => {
+//   test.use({ viewport: viewports.desktop })
 
-  test('Navigates to start page from the navigation menu', async ({ app, authStartPage }) => {
-    await test.step('loads the start page', async () => {
-      await authStartPage.goto()
-      await authStartPage.hasMainHeading()
-    })
+//   test('Navigates to start page from the navigation menu', async ({ app, authStartPage }) => {
+//     await test.step('loads the start page', async () => {
+//       await authStartPage.goto()
+//       await authStartPage.hasMainHeading()
+//     })
 
-    await test.step('loads the "start" page', async () => {
-      await app.clickNav('start')
-      await app.hasHeading('start')
-    })
-  })
-})
+//     await test.step('loads the "start" page', async () => {
+//       await app.clickNav('start')
+//       await app.hasHeading('start')
+//     })
+//   })
+// })
 
-test.describe('Non-public tests - mobile @non-public', () => {
-  test.use({ viewport: viewports.mobile })
+// test.describe('Non-public tests - mobile @non-public', () => {
+//   test.use({ viewport: viewports.mobile })
 
-  test('Navigates to start page from the dropdown mobile navigation menu', async ({ app, authStartPage }) => {
-    await test.step('loads the start page', async () => {
-      await authStartPage.goto()
-      await authStartPage.hasMainHeading()
-    })
+//   test('Navigates to start page from the dropdown mobile navigation menu', async ({ app, authStartPage }) => {
+//     await test.step('loads the start page', async () => {
+//       await authStartPage.goto()
+//       await authStartPage.hasMainHeading()
+//     })
 
-    await test.step('loads the "start" page', async () => {
-      await app.clickNav('start')
-      await app.hasHeading('start')
-    })
-  })
-})
+//     await test.step('loads the "start" page', async () => {
+//       await app.clickNav('start')
+//       await app.hasHeading('start')
+//     })
+//   })
+// })
 
-test.describe('Non-public tests - no JavaScript @non-public', () => {
-  test.use({ javaScriptEnabled: false, viewport: viewports.desktop })
+// test.describe('Non-public tests - no JavaScript @non-public', () => {
+//   test.use({ javaScriptEnabled: false, viewport: viewports.desktop })
 
-  test('Navigates to start page from the side navigation menu', async ({ app, authStartPage }) => {
-    await test.step('loads the start page', async () => {
-      await authStartPage.goto()
-      await authStartPage.hasMainHeading()
-    })
+//   test('Navigates to start page from the side navigation menu', async ({ app, authStartPage }) => {
+//     await test.step('loads the start page', async () => {
+//       await authStartPage.goto()
+//       await authStartPage.hasMainHeading()
+//     })
 
-    await test.step('loads the "start" page', async () => {
-      await app.clickBrowseNav('start')
-      await app.hasHeading('start')
-    })
-  })
-})
+//     await test.step('loads the "start" page', async () => {
+//       await app.clickBrowseNav('start')
+//       await app.hasHeading('start')
+//     })
+//   })
+// })
