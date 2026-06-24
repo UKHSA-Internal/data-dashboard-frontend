21e91f51c37f4cf5c7956ed4d828dc678d597bcf
Daniel Welsh <daniel.welsh@ukhsa.gov.uk>
2026-06-24 10:30:41 +0100

    Fixing the sonarqube issues and retroring main non-public test suite

--- e2e/tests/non-public/non-public.e2e.ts
+++ e2e/tests/non-public/non-public.e2e.ts
@@ -2,50 +2,37 @@ import { viewports } from 'e2e/constants/viewports.constants'
 
 import { test } from '../../fixtures/app.fixture.non.public'
 
-test.describe('Non-public tests - desktop @non-public', () => {
+const respiritoryTopicPages = [
+  {
+    name: 'COVID-19',
+    path: '/respiritory-viruses/covid-19',
+    heading: 'COVID-19',
+  },
+  {
+    name: 'Influenza',
+    path: '/respiritory-viruses/influenza',
+    heading: 'Influenza',
+  },
+  {
+    name: 'Other respiritory viruses',
+    path: '/respiritory-viruses/other-respiritory-viruses',
+    heading: 'Other respiritory viruses',
+  },
+]
+
+test.describe('Respiritory topic pages - non-public @non-public', () => {
   test.use({ viewport: viewports.desktop })
 
-  test('Navigates to start page from the navigation menu', async ({ app, authStartPage }) => {
-    await test.step('loads the start page', async () => {
-      await authStartPage.goto()
-      await authStartPage.hasMainHeading()
+  for (const topicPage of respiritoryTopicPages) {
+    test(`${topicPage.name} shows the classification banner`, async ({ app, authEnabled, page, switchboardPage }) => {Expand annotationCheck failure on line R27
+      test.skip(!authEnabled, 'Skipped: AUTH_ENABLED is false')Expand annotationCheck warning on line R28
+
+      await switchboardPage.setTopicPageIsPublic(false)
+      await page.goto(topicPage.path)
+
+      await app.hasHeading(topicPage.heading)
+      await app.hasClassificationBanner()
+      await app.checkClassificationBannerContent()
     })
-
-    await test.step('loads the "start" page', async () => {
-      await app.clickNav('start')
-      await app.hasHeading('start')
-    })
-  })
-})
-
-test.describe('Non-public tests - mobile @non-public', () => {
-  test.use({ viewport: viewports.mobile })
-
-  test('Navigates to start page from the dropdown mobile navigation menu', async ({ app, authStartPage }) => {
-    await test.step('loads the start page', async () => {
-      await authStartPage.goto()
-      await authStartPage.hasMainHeading()
-    })
-
-    await test.step('loads the "start" page', async () => {
-      await app.clickNav('start')
-      await app.hasHeading('start')
-    })
-  })
-})
-
-test.describe('Non-public tests - no JavaScript @non-public', () => {
-  test.use({ javaScriptEnabled: false, viewport: viewports.desktop })
-
-  test('Navigates to start page from the side navigation menu', async ({ app, authStartPage }) => {
-    await test.step('loads the start page', async () => {
-      await authStartPage.goto()
-      await authStartPage.hasMainHeading()
-    })
-
-    await test.step('loads the "start" page', async () => {
-      await app.clickBrowseNav('start')
-      await app.hasHeading('start')
-    })
-  })
+  }
 })
