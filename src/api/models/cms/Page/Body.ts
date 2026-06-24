335d697e0e606aa4316aa569d9520759b378e736
Megan Bower <megan.bower@burendo.com>
2026-06-24 15:08:37 +0100

    CDD-3441 WIP filter null pages

--- src/api/models/cms/Page/Body.ts
+++ src/api/models/cms/Page/Body.ts
@@ -337,7 +337,7 @@ export const CompositeBody = z.array(
             page: z.string(),
           }),
           id: z.string(),
-        })
+        }).nullable()
       ),
       id: z.string(),
     }),
