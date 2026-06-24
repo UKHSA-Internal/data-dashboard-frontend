c7d3e649110b1d5d86cc56e242c3dd61a802b380
Megan Bower <megan.bower@burendo.com>
2026-06-24 16:16:38 +0100

    CDD-3441 Add test for unauthorised pages

--- src/app/utils/cms.utils.spec.tsx
+++ src/app/utils/cms.utils.spec.tsx
@@ -249,6 +249,31 @@ describe('renderCompositeBlock function', () => {
     expect(screen.getByText('COVID-19 is a respiratory infection caused by the SARS-CoV-2-virus.')).toBeInTheDocument()
   })
 
+  test('does not render unauthorised internal page links', () => {
+    render(
+      renderCompositeBlock({
+        type: 'internal_page_links',
+        value: [
+          {
+            type: 'page_link',
+            value: {
+              title: 'Hidden Page',
+              sub_title: 'Should not show',
+              page: 'http://localhost:3000/hidden/',
+              is_authorised: false,
+            },
+            id: '123',
+          },
+        ],
+        id: 'block-1',
+      })
+    )
+
+    expect(
+      screen.queryByRole('link', { name: 'Hidden Page' })
+    ).not.toBeInTheDocument()
+  })
+
   test('renders code block correctly', () => {
     render(
       renderCompositeBlock({
