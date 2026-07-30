e4ed66010fef62bcac4cf1c647df0441ff486f52
Rhys <rhys.howard@burendo.com>
2026-07-30 22:35:45 +0100

    Customising charts on FE spike

--- src/app/components/cms/ChartInteractive/ChartInteractive.tsx
+++ src/app/components/cms/ChartInteractive/ChartInteractive.tsx
@@ -9,30 +9,68 @@ interface ChartInteractiveProps {
   figure: Figure
 }
 
+const withoutTickMarks = (layout: Figure['layout']): Figure['layout'] => ({
+  ...layout,
+  xaxis: { ...layout?.xaxis, ticks: '', showticklabels: false },
+  yaxis: { ...layout?.yaxis, ticks: '', showticklabels: false },
+})
+
+export const withMoreTickMarks = (layout: Figure['layout']): Figure['layout'] => ({
+  ...layout,
+  xaxis: { ...layout?.xaxis, nticks: 20, ticks: 'outside', showticklabels: true },
+  yaxis: { ...layout?.yaxis, nticks: 20, ticks: 'outside', showticklabels: true },
+})
+
+export const withFewerTickMarks = (layout: Figure['layout']): Figure['layout'] => ({
+  ...layout,
+  xaxis: { ...layout?.xaxis, nticks: 2, ticks: 'outside', showticklabels: true },
+  yaxis: { ...layout?.yaxis, nticks: 2, ticks: 'outside', showticklabels: true },
+})
+
+export const withSwappedBarOrientation = ({ data = [], layout }: Figure): Pick<Figure, 'data' | 'layout'> => ({
+  data: data.map((trace) =>
+    trace.type !== 'bar'
+      ? trace
+      : { ...trace, orientation: trace.orientation === 'h' ? 'v' : 'h', x: trace.y, y: trace.x }
+  ),
+  layout: {
+    ...layout,
+    xaxis: layout?.yaxis,
+    yaxis: layout?.xaxis,
+  },
+})
+
 export default function ChartInteractive({ fallbackUntilLoaded, figure: { data, layout } }: ChartInteractiveProps) {
   const [loaded, setLoaded] = useState(false)
 
-  const { isIntersecting, ref } = useIntersectionObserver({
-    freezeOnceVisible: true,
-  })
+  // Data
+  let dynamicData = data
+
+  // Layout
+  let dynamicLayout = layout
+  // dynamicLayout = withoutTickMarks(layout)
+  dynamicLayout = withMoreTickMarks(layout)
+  // dynamicLayout = withFewerTickMarks(layout)
+  
+  // const swappedBar = withSwappedBarOrientation({ data, layout })
+  // dynamicData = swappedBar.data
+  // dynamicLayout = swappedBar.layout
+
 
   return (
-    <div ref={ref}>
+    <div>
       {!loaded ? fallbackUntilLoaded : null}
-      {isIntersecting ? (
         <Plot
           onInitialized={() => setLoaded(true)}
-          data={data}
-          layout={layout}
+          data={dynamicData}
+          layout={dynamicLayout}
           style={{ width: '100%', height: '100%' }}
           config={{
             displayModeBar: false,
             scrollZoom: false,
             responsive: true,
           }}
-          useResizeHandler
         />
-      ) : null}
     </div>
   )
 }
