cb434199808c00270bc1ff496f5bafe918736b50
Daniel Welsh <daniel.welsh@ukhsa.gov.uk>
2026-09-17 13:15:58 +0100

    adding a longer timeout for test to check if CI is just slower than local

--- e2e/fixtures/app.fixture.ts
+++ e2e/fixtures/app.fixture.ts
@@ -138,7 +138,7 @@ export class App {
   }
 
   async hasDocumentTitle(title: string) {
-    await expect(this.page).toHaveTitle(title)
+    await expect(this.page).toHaveTitle(title, { timeout: 15000 })
   }
 
   async hasNoAccessibilityDefects(additionalDisabledRules: string[] = []) {
